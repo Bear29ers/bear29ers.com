52bc4bd6ccbd1a3f88b0be387cbdf5d7d3e88028
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-23 22:53:18 +0900

    config: :gear: add prettier settings

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+/** @type {import('@typescript-eslint/experimental-utils').TSESLint.Linter.Config} */
+
+const config = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+};
+
+module.exports = config;
---
+++ .prettierrc.cjs
@@ -0,0 +1,33 @@
+/** @type {import('pretteir').Options} */
+
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSameLine: true,
+  trailingComma: 'es5',
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^types$',
+    '^@local/(.*)$',
+    '^@/config/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/components/(.*)$',
+    '^@/styles/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+};
+
+module.exports = config;
