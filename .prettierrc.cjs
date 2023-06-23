7ed241f869911a41d5aedf7de840733d30ea1e58
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-23 23:33:41 +0900

    config: :gear: add eslint settings

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -27,7 +27,6 @@ const config = {
   importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
   importOrderMergeDuplicateImports: true,
   importOrderCombineTypeAndValueImports: true,
-  plugins: ['@ianvs/prettier-plugin-sort-imports'],
 };
 
 module.exports = config;
