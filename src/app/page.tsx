e1ca2e6fed30915210bb22c37207abf42deaebb0
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-24 11:57:06 +0900

    config: :gear: add daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image';
-
-export default function Home() {
+const Home = () => {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main>
       <h2 className='text-2xl font-extrabold'>Next.js Portfolio</h2>
+      <button className='btn btn-primary' type='button'>
+        button
+      </button>
     </main>
   );
-}
+};
+
+export default Home;
