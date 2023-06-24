6e08a5de01af1b21801d8ce79fa417c0ea9ecdfe
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-24 11:42:03 +0900

    config: :gear: add stylelint settings

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -4,14 +4,14 @@ const config = {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
   ignoreFiles: ['/node_modules', '/.next'],
   rules: {
-    'no-empty-source': 'warn',
-    'at-rule-no-unknown': [
+    'no-empty-source': null,
+    'scss/at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: ['tailwind', 'apply', 'layer'],
       },
     ],
-    'function-no-unknown': [
+    'scss/function-no-unknown': [
       true,
       {
         ignoreFunctions: ['theme', 'screen'],
