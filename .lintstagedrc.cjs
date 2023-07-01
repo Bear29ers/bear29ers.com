95b8cf9e107ad7e00f18bb92e7cb7009e9c56d4e
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-01 11:32:48 +0900

    config: :gear: update .lintstagedrc.cjs

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,18 +1,11 @@
-// const config = {
-//   '**/*.{js,jsx,ts,tsx}': 'npx eslint --fix',
-//   '**/*.{css,scss}': 'npx stylelint --fix',
-//   '**/*.{html,jsx,tsx}': 'npx markuplint --fix',
-//   '**/*.{js,jsx,ts,tsx,json}': 'npx prettier --write',
-//   '**/*.{spec,test}.{js,jsx,ts,tsx}': 'npx jest',
-// };
-//
-// export default config;
-
 const path = require('path');
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'eslint --fix', 'prettier --write'],
+  '**/*.{js,jsx,ts,tsx}': [buildEslintCommand, 'eslint --fix'],
+  '**/*.{js,jsx,ts,tsx,json}': 'prettier --write',
+  '**/*.{html,jsx,tsx}': 'markuplint --write',
+  '**/*.{spec,test}.{js,jsx,ts,tsx}': 'jest',
 };
