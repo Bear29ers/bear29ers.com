6b29fd445316a1377e5d744d1db3a649301c798c
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-03 18:53:13 +0900

    feat: :sparkles: set montserrat font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className={`${montserrat.variable}`}>
+      <body className={`${montserrat.className} font-mont bg-light min-h-screen w-full`}>
         <Navbar />
         {children}
       </body>
