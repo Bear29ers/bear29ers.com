9623499be95305bd7725bdf35f8305d6d7ffe325
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-05 08:12:28 +0900

    test: :green_heart: update test files

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import Navbar from './Navbar';
 
 describe('src/components/Navbar/Navbar.test.tsx', () => {
-  it('renders correctly', () => {
+  it('should be [role=banner]', () => {
     const { getByRole } = render(<Navbar />);
     expect(getByRole('banner')).toBeInTheDocument();
   });
