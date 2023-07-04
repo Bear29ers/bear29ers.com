4a997685ea0038762e244574218cf42d89ac4409
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-05 08:12:28 +0900

    test: :green_heart: update test files

--- src/components/Logo/Logo.test.tsx
+++ src/components/Logo/Logo.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import Logo from './Logo';
 
 describe('src/components/Logo/Logo.test.tsx', () => {
-  it('renders correctly', () => {
+  it('should be [role=link] with name BR', () => {
     const { getByRole } = render(<Logo />);
     expect(getByRole('link', { name: 'BR' })).toBeInTheDocument();
   });
--- src/components/NavLink/NavLink.test.tsx
+++ src/components/NavLink/NavLink.test.tsx
@@ -3,8 +3,23 @@ import { render } from '@testing-library/react';
 import NavLink from './NavLink';
 
 describe('src/components/NavLink/NavLink.test.tsx', () => {
-  it('renders correctly', () => {
+  it('should be [role=link] with name Home', () => {
     const { getByRole } = render(<NavLink href="/" title="Home" />);
     expect(getByRole('link', { name: 'Home' })).toBeInTheDocument();
+  });
+
+  it('should be [role=link] with name About', () => {
+    const { getByRole } = render(<NavLink href="/about" title="About" />);
+    expect(getByRole('link', { name: 'About' })).toBeInTheDocument();
+  });
+
+  it('should be [role=link] with name Projects', () => {
+    const { getByRole } = render(<NavLink href="/projects" title="Projects" />);
+    expect(getByRole('link', { name: 'Projects' })).toBeInTheDocument();
+  });
+
+  it('should be [role=link] with name Articles', () => {
+    const { getByRole } = render(<NavLink href="/articles" title="Articles" />);
+    expect(getByRole('link', { name: 'Articles' })).toBeInTheDocument();
   });
 });
