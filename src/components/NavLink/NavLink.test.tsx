d59af37dd67832852fe01b59eac641829bf85f26
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-06 08:05:34 +0900

    test: :green_heart: modify test description

--- src/components/NavLink/NavLink.test.tsx
+++ src/components/NavLink/NavLink.test.tsx
@@ -3,22 +3,24 @@ import { render } from '@testing-library/react';
 import NavLink from './NavLink';
 
 describe('src/components/NavLink/NavLink.test.tsx', () => {
-  it('should be [role=link] with name Home', () => {
+  it('should render NavLink component that is [role=link] with name Home', () => {
     const { getByRole } = render(<NavLink href="/" title="Home" />);
     expect(getByRole('link', { name: 'Home' })).toBeInTheDocument();
   });
 
-  it('should be [role=link] with name About', () => {
+  // TODO: ページ実装後にページ遷移のテストを記述する
+
+  it('should render NavLink component that is [role=link] with name About', () => {
     const { getByRole } = render(<NavLink href="/about" title="About" />);
     expect(getByRole('link', { name: 'About' })).toBeInTheDocument();
   });
 
-  it('should be [role=link] with name Projects', () => {
+  it('should render NavLink compopnent that is [role=link] with name Projects', () => {
     const { getByRole } = render(<NavLink href="/projects" title="Projects" />);
     expect(getByRole('link', { name: 'Projects' })).toBeInTheDocument();
   });
 
-  it('should be [role=link] with name Articles', () => {
+  it('should render NavLink component that is [role=link] with name Articles', () => {
     const { getByRole } = render(<NavLink href="/articles" title="Articles" />);
     expect(getByRole('link', { name: 'Articles' })).toBeInTheDocument();
   });
--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import Navbar from './Navbar';
 
 describe('src/components/Navbar/Navbar.test.tsx', () => {
-  it('should be [role=banner]', () => {
+  it('should render Navbar component that is [role=banner]', () => {
     const { getByRole } = render(<Navbar />);
     expect(getByRole('banner')).toBeInTheDocument();
   });
