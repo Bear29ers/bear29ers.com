a6c2f793a52bb8585c6291cbcc39f3bcca78ce9e
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-09 14:43:24 +0900

    test: :green_heart: update icon components test

--- src/components/Icons/FacebookIcon/FacebookIcon.test.tsx
+++ src/components/Icons/FacebookIcon/FacebookIcon.test.tsx
@@ -4,12 +4,7 @@ import FacebookIcon from './FacebookIcon';
 
 describe('src/components/Icons/FacebookIcon/FacebookIcon.test.tsx', () => {
   it('should render FacebookIcon component', () => {
-    const { getByRole } = render(<FacebookIcon className="" />);
+    const { getByRole } = render(<FacebookIcon />);
     expect(getByRole('img')).toBeInTheDocument();
-  });
-
-  it('should render FacebookIcon component with w-20 class', () => {
-    const { getByRole } = render(<FacebookIcon className="!w-20" />);
-    expect(getByRole('img')).toHaveClass('!w-20');
   });
 });
--- src/components/Icons/GithubIcon/GithubIcon.test.tsx
+++ src/components/Icons/GithubIcon/GithubIcon.test.tsx
@@ -4,12 +4,7 @@ import GithubIcon from './GithubIcon';
 
 describe('src/components/Icons/GithubIcon/GithubIcon.test.tsx', () => {
   it('should render GithubIcon component', () => {
-    const { getByRole } = render(<GithubIcon className="" />);
+    const { getByRole } = render(<GithubIcon />);
     expect(getByRole('img')).toBeInTheDocument();
-  });
-
-  it('should render GithubIcon component with w-20 class', () => {
-    const { getByRole } = render(<GithubIcon className="!w-20" />);
-    expect(getByRole('img')).toHaveClass('!w-20');
   });
 });
