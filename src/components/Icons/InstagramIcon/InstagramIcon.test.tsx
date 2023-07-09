e2f3555a313190776a123ce3f9b94e9d4d0e6c93
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-09 14:43:24 +0900

    test: :green_heart: update icon components test

--- src/components/Icons/InstagramIcon/InstagramIcon.test.tsx
+++ src/components/Icons/InstagramIcon/InstagramIcon.test.tsx
@@ -4,12 +4,7 @@ import InstagramIcon from './InstagramIcon';
 
 describe('src/components/Icons/InstagramIcon/InstagramIcon.test.tsx', () => {
   it('should render InstagramIcon component', () => {
-    const { getByRole } = render(<InstagramIcon className="" />);
+    const { getByRole } = render(<InstagramIcon />);
     expect(getByRole('img')).toBeInTheDocument();
-  });
-
-  it('should render InstagramIcon component with w-20 class', () => {
-    const { getByRole } = render(<InstagramIcon className="!w-20" />);
-    expect(getByRole('img')).toHaveClass('!w-20');
   });
 });
