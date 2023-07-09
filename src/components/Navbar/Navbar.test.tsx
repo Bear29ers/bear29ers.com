54476e2c1ef7d6500e1180d16d43e4f2326935b9
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-09 15:45:02 +0900

    test: :green_heart: update navbar component test

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Navbar from './Navbar';
 
@@ -6,5 +6,16 @@ describe('src/components/Navbar/Navbar.test.tsx', () => {
   it('should render Navbar component that is [role=banner]', () => {
     const { getByRole } = render(<Navbar />);
     expect(getByRole('banner')).toBeInTheDocument();
+  });
+
+  it('should render Navbar component that have two [role=navigation]', () => {
+    const { getAllByRole } = render(<Navbar />);
+    expect(getAllByRole('navigation')).toHaveLength(2);
+  });
+
+  it('should render Navbar component with two nav tags', () => {
+    render(<Navbar />);
+    const navElements = screen.getAllByRole('navigation');
+    navElements.map((element) => expect(element).toBeInTheDocument());
   });
 });
