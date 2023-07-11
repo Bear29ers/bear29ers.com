398293e1ef03bd7dbcb53654ab765ed04c391690
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-11 23:58:31 +0900

    feat: :sparkles: create animated text component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 
+import AnimatedText from '@/components/AnimatedText/AnimatedText';
 import PageLayout from '@/components/PageLayout/PageLayout';
 
 import { HOME_TEXT } from '@/constants';
@@ -16,9 +17,9 @@ const Home: NextPage = () => {
           <div className="w-1/2">
             <Image src={Profile} alt="Bear29ers" className="h-auto w-full" role="img" />
           </div>
-          <div>
-            <h1>{HOME_TEXT.title}</h1>
-            <p>{HOME_TEXT.description}</p>
+          <div className="flex w-1/2 flex-col items-center self-center">
+            <AnimatedText text={HOME_TEXT.title} className="!text-left !text-6xl" />
+            <p className="my-4 text-base font-medium">{HOME_TEXT.description}</p>
           </div>
         </div>
       </PageLayout>
