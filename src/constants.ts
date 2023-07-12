9aeade556cfff09f7ea74bfe94477e9ca1b6118f
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-12 21:39:25 +0900

    refactor: :bulb: modify home page layout and text

--- src/constants.ts
+++ src/constants.ts
@@ -35,6 +35,5 @@ export const SOCIAL_LINK = {
 
 export const HOME_TEXT = {
   title: 'Code and Design make vision a reality',
-  description:
-    'As a front-end developer and designer, I really enjoy giving shape to my own ideas. Explore my personality and latest projests and articles in web development.',
+  description: `As a front-end developer and designer, I really enjoy giving shape to my own ideas.\nExplore my personality and latest projests and articles in web development.`,
 };
