739444060eb5b59bed7bbda30b415abeb4cee93b
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-12 22:24:02 +0900

    feat: :sparkles: add explore button to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import AnimatedText from '@/components/AnimatedText/AnimatedText';
 import PageLayout from '@/components/PageLayout/PageLayout';
@@ -20,6 +21,13 @@ const Home: NextPage = () => {
           <div className="flex w-1/2 flex-col items-center self-center">
             <AnimatedText text={HOME_TEXT.title} className="!text-left !text-6xl" />
             <p className="my-4 whitespace-pre-wrap text-base font-medium">{HOME_TEXT.description}</p>
+            <div className="mt-2 flex items-center self-start">
+              <Link
+                href="/about"
+                className="transition-500 flex items-center rounded-lg border-2 border-solid border-transparent bg-dark px-6 py-2.5 text-lg font-semibold text-light hover:border-dark hover:bg-light hover:text-dark">
+                Explore
+              </Link>
+            </div>
           </div>
         </div>
       </PageLayout>
