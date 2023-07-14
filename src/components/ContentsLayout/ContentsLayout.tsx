b1f21c61b01b5e1f6f27b239d9c94073f6df9223
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-15 01:17:04 +0900

    test: :green_heart: update page test about ContentsLayout

--- src/components/ContentsLayout/ContentsLayout.tsx
+++ src/components/ContentsLayout/ContentsLayout.tsx
@@ -6,7 +6,11 @@ type Props = {
 };
 
 const ContentsLayout: FC<Props> = ({ children, className = '' }) => {
-  return <div className={`z-0 inline-block h-full w-full bg-light p-32 ${className}`}>{children}</div>;
+  return (
+    <div className={`z-0 inline-block h-full w-full bg-light p-32 ${className}`} role="presentation">
+      {children}
+    </div>
+  );
 };
 
 export default ContentsLayout;
