94fc5f2149dadcf36ec7a2b9a0a3c12ff39803b0
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-22 11:26:05 +0900

    wip: :construction: add biography to profile component

--- src/components/Profile/Profile.test.tsx
+++ src/components/Profile/Profile.test.tsx
@@ -1,9 +1,28 @@
 import { render } from '@testing-library/react';
 
+import { ACHIEVEMENTS, PROFILE_TEXT } from '@/constants';
+
 import Profile from './Profile';
 
 describe('src/components/Profile/Profile.test.tsx', () => {
   it('should render Profile component', () => {
     const { getByRole } = render(<Profile />);
+    expect(getByRole('grid')).toBeInTheDocument();
+  });
+
+  it('should render profile picture with alt text', () => {
+    const { getByAltText } = render(<Profile />);
+    expect(getByAltText(PROFILE_TEXT.userName)).toBeInTheDocument();
+  });
+
+  it('should render full name and username', () => {
+    const { getByText } = render(<Profile />);
+    expect(getByText(PROFILE_TEXT.fullName)).toBeInTheDocument();
+    expect(getByText(PROFILE_TEXT.userName)).toBeInTheDocument();
+  });
+
+  it('should render three achievements', () => {
+    const { getAllByRole } = render(<Profile />);
+    expect(getAllByRole('img', { name: /Achievement:/ })).toHaveLength(ACHIEVEMENTS.length);
   });
 });
