2fdb6e77d687eef1e22e74acd23d32da002a2a1f
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-22 12:54:42 +0900

    refactor: :bulb: update social media icon list in navbar

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,14 @@
+import type { FC } from 'react';
+
 import arcticCodeVault from '../public/images/achievements/arctic-code-vault-contributor-default.png';
 import pullShark from '../public/images/achievements/pull-shark-default.png';
 import quickdraw from '../public/images/achievements/quickdraw-default--light.png';
 import yolo from '../public/images/achievements/yolo-default.png';
+
+import FacebookIcon from './components/Icons/FacebookIcon/FacebookIcon';
+import GithubIcon from './components/Icons/GithubIcon/GithubIcon';
+import InstagramIcon from './components/Icons/InstagramIcon/InstagramIcon';
+import ThreadsIcon from './components/Icons/ThreadsIcon/ThreadsIcon';
 
 import type { StaticImageData } from 'next/image';
 
@@ -35,12 +42,45 @@ export const NAV_CONTENTS: NavContents[] = [
   },
 ];
 
-// ナビゲーションのソーシャルメディアリンク
-export const SOCIAL_LINK = {
-  github: 'https://github.com/Bear29ers',
-  facebook: 'https://www.facebook.com/kumakuma1129',
-  instagram: 'https://www.instagram.com/bear_27earl',
+type SocialMedia = {
+  id: number;
+  href: string;
+  describedby: string;
+  text: string;
+  component: FC;
 };
+
+// ソーシャルメディアアイコンのリスト
+export const SOCIAL_MEDIA_LIST: SocialMedia[] = [
+  {
+    id: 1,
+    href: 'https://github.com/Bear29ers',
+    describedby: 'navlink-id-1',
+    text: 'github',
+    component: GithubIcon,
+  },
+  {
+    id: 2,
+    href: 'https://www.instagram.com/bear_27earl',
+    describedby: 'navlink-id-2',
+    text: 'instagram',
+    component: InstagramIcon,
+  },
+  {
+    id: 3,
+    href: 'https://www.threads.net/@bear_27earl',
+    describedby: 'navlink-id-3',
+    text: 'threads',
+    component: ThreadsIcon,
+  },
+  {
+    id: 4,
+    href: 'https://www.facebook.com/kumakuma1129',
+    describedby: 'navlink-id-4',
+    text: 'facebook',
+    component: FacebookIcon,
+  },
+];
 
 // Homeページテキスト
 export const HOME_TEXT = {
