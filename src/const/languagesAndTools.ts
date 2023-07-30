e32c2d86bc6ad2f3a545ef5329007fb39c3814d8
Yuta Okuma <y.dream.nest@icloud.com>
2023-07-30 20:03:28 +0900

    wip: :construction: create languages and tools drag animation

---
+++ src/const/languagesAndTools.ts
@@ -0,0 +1,22 @@
+import html from '../../public/images/languages_tools/html.svg';
+
+
+// ライトモード用アイコン
+export const lightIcons = [
+  [html, html, html, html, html, html, html],
+  [html, html, html, html, html, html, html],
+];
+
+// ダークモード用アイコン
+export const darkIcons = [
+  [0, 1, 2, 3],
+  [4, 5, 6, 7],
+  [8, 9, 10, 11],
+  [12, 13, 14, 15],
+];
+
+// アイコンサイズ
+export const SIZE = 60;
+
+// アイコン間のスペース
+export const GAP = 15;
