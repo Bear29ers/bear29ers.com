723972de412784ae50393d937c6374b663d9e864
Yuta Okuma <y.dream.nest@icloud.com>
2023-08-03 22:18:46 +0900

    feat: :sparkles: add remain skill icon components

--- src/components/Icons/SkillIcons/SkillIcons.test.tsx
+++ src/components/Icons/SkillIcons/SkillIcons.test.tsx
@@ -1,34 +1,62 @@
 import { render } from '@testing-library/react';
 
 import {
+  AfterEffects,
+  Atom,
+  Aws,
   Babel,
+  Bash,
   Bootstrap,
+  Codepen,
   Css,
+  Discord,
+  Docker,
   Emotion,
   Express,
+  Figma,
+  Firebase,
+  Gcp,
+  Git,
+  GitHub,
+  GitHubActions,
   Html,
+  Idea,
+  Illustrator,
   JQuery,
   JavaScript,
   Jest,
   Laravel,
+  Linux,
+  Lua,
+  Markdown,
   MaterialUi,
   MySql,
+  Neovim,
   NextJs,
   NodeJs,
   NuxtJs,
+  Photoshop,
   Php,
+  Postman,
+  PremierePro,
   ReactJs,
   Regex,
   Sass,
+  Sentry,
   Sqlite,
+  StackOverFlow,
   StyledComponents,
+  Svg,
   Tailwind,
   ThreeJs,
   TypeScript,
   Vercel,
+  Vim,
   Vite,
+  Vscode,
   VueJs,
   Webpack,
+  Xd,
 } from './SkillIcons';
 
 describe('src/components/Icons/SkillIcons/SkillIcons.test.tsx', () => {
@@ -172,35 +200,144 @@ describe('src/components/Icons/SkillIcons/SkillIcons.test.tsx', () => {
     expect(getByTestId('babel')).toBeInTheDocument();
   });
 
-  // Docker
-  // Git
-  // GitHub
-  // GitHub Actions
-  // AWS
-  // GCP
-  // Firebase
+  it('should render Docker svg component', () => {
+    const { getByTestId } = render(<Docker />);
+    expect(getByTestId('docker')).toBeInTheDocument();
+  });
 
-  // Vim
-  // Neovim
-  // Linux
-  // Bash
-  // Lua
-  // Markdown
-  // SVG
+  it('should render Git svg component', () => {
+    const { getByTestId } = render(<Git />);
+    expect(getByTestId('git')).toBeInTheDocument();
+  });
 
-  // IDEA
-  // VSCode
-  // Atom
-  // Figma
-  // XD
-  // Photoshop
-  // Illustrator
+  it('should render GitHub svg component', () => {
+    const { getByTestId } = render(<GitHub />);
+    expect(getByTestId('github')).toBeInTheDocument();
+  });
 
-  // Premiere Pro
-  // AfterEffects
-  // Postman
-  // Sentry
-  // Discord
+  it('should render GitHub Actions svg component', () => {
+    const { getByTestId } = render(<GitHubActions />);
+    expect(getByTestId('github-actions')).toBeInTheDocument();
+  });
+
+  it('should render AWS svg component', () => {
+    const { getByTestId } = render(<Aws />);
+    expect(getByTestId('aws')).toBeInTheDocument();
+  });
+
+  it('should render GCP svg component', () => {
+    const { getByTestId } = render(<Gcp />);
+    expect(getByTestId('gcp')).toBeInTheDocument();
+  });
+
+  it('should render Firebase svg component', () => {
+    const { getByTestId } = render(<Firebase />);
+    expect(getByTestId('firebase')).toBeInTheDocument();
+  });
+
+  it('should render Vim svg component', () => {
+    const { getByTestId } = render(<Vim />);
+    expect(getByTestId('vim')).toBeInTheDocument();
+  });
+
+  it('should render Neovim svg component', () => {
+    const { getByTestId } = render(<Neovim />);
+    expect(getByTestId('neovim')).toBeInTheDocument();
+  });
+
+  it('should render Linux svg component', () => {
+    const { getByTestId } = render(<Linux />);
+    expect(getByTestId('linux')).toBeInTheDocument();
+  });
+
+  it('should render Bash svg component', () => {
+    const { getByTestId } = render(<Bash />);
+    expect(getByTestId('bash')).toBeInTheDocument();
+  });
+
+  it('should render Lua svg component', () => {
+    const { getByTestId } = render(<Lua />);
+    expect(getByTestId('lua')).toBeInTheDocument();
+  });
+
+  it('should render Markdown svg component', () => {
+    const { getByTestId } = render(<Markdown />);
+    expect(getByTestId('markdown')).toBeInTheDocument();
+  });
+
+  it('should render SVG svg component', () => {
+    const { getByTestId } = render(<Svg />);
+    expect(getByTestId('svg')).toBeInTheDocument();
+  });
+
+  it('should render IDEA svg component', () => {
+    const { getByTestId } = render(<Idea />);
+    expect(getByTestId('idea')).toBeInTheDocument();
+  });
+
+  it('should render VSCode svg component', () => {
+    const { getByTestId } = render(<Vscode />);
+    expect(getByTestId('vscode')).toBeInTheDocument();
+  });
+
+  it('should render Atom svg component', () => {
+    const { getByTestId } = render(<Atom />);
+    expect(getByTestId('atom')).toBeInTheDocument();
+  });
+
+  it('should render Figma svg component', () => {
+    const { getByTestId } = render(<Figma />);
+    expect(getByTestId('figma')).toBeInTheDocument();
+  });
+
+  it('should render XD svg component', () => {
+    const { getByTestId } = render(<Xd />);
+    expect(getByTestId('xd')).toBeInTheDocument();
+  });
+
+  it('should render Photoshop svg component', () => {
+    const { getByTestId } = render(<Photoshop />);
+    expect(getByTestId('photoshop')).toBeInTheDocument();
+  });
+
+  it('should render Illustrator svg component', () => {
+    const { getByTestId } = render(<Illustrator />);
+    expect(getByTestId('illustrator')).toBeInTheDocument();
+  });
+
+  it('should render Premiere Pro svg component', () => {
+    const { getByTestId } = render(<PremierePro />);
+    expect(getByTestId('premiere-pro')).toBeInTheDocument();
+  });
+
+  it('should render AfterEffects svg component', () => {
+    const { getByTestId } = render(<AfterEffects />);
+    expect(getByTestId('aftereffects')).toBeInTheDocument();
+  });
+
+  it('should render Postman svg component', () => {
+    const { getByTestId } = render(<Postman />);
+    expect(getByTestId('postman')).toBeInTheDocument();
+  });
+
+  it('should render Sentry svg component', () => {
+    const { getByTestId } = render(<Sentry />);
+    expect(getByTestId('sentry')).toBeInTheDocument();
+  });
+
   // Codepen
-  // Stackoverflow
+  it('should render Codepen svg component', () => {
+    const { getByTestId } = render(<Codepen />);
+    expect(getByTestId('codepen')).toBeInTheDocument();
+  });
+
+  it('should render Stack Overflow svg component', () => {
+    const { getByTestId } = render(<StackOverFlow />);
+    expect(getByTestId('stack-overflow')).toBeInTheDocument();
+  });
+
+  it('should render Discord svg component', () => {
+    const { getByTestId } = render(<Discord />);
+    expect(getByTestId('discord')).toBeInTheDocument();
+  });
 });
