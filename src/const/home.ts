5d99b106b962c577439409609faa0b731ec47438
Yuta Okuma <y.dream.nest@icloud.com>
2023-08-06 19:28:41 +0900

    refactor: :bulb: separate constants files

---
+++ src/const/home.ts
@@ -0,0 +1,5 @@
+// Homeページテキスト
+export const HOME_TEXT = {
+  title: 'Code and Design make vision a reality',
+  description: `As a front-end developer and designer, I really enjoy giving shape to my own ideas.\nExplore my personality and latest projests and articles in web development.`,
+};
