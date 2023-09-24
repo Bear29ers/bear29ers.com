1d6c12736dec7c2d83effc103a4b6d9dcade4da1
Yuta Okuma <y.dream.nest@icloud.com>
2023-09-24 22:54:27 +0900

    wip: :construction: implement right side of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Link from 'next/link';
-
 import ProfileCard from '@/components/ProfileCard/ProfileCard';
-import AnimatedText from '@/components/commons/AnimatedText/AnimatedText';
 import HomeContentsLayout from '@/components/layouts/HomeContentsLayout/HomeContentsLayout';
 import PageLayout from '@/components/layouts/PageLayout/PageLayout';
 
-import { HOME_TEXT } from '@/constants/home';
+// import { HOME_TEXT } from '@/constants/home';
 
 import type { NextPage } from 'next';
 
@@ -15,21 +12,12 @@ const Home: NextPage = () => {
       <PageLayout isNotHome={false}>
         <main className="flex min-h-screen w-full items-center" role="main">
           <HomeContentsLayout>
-            <div className="flex-between w-full gap-x-8">
+            <div className="flex w-full items-start justify-between gap-x-8">
               <div className="w-4/12">
                 <ProfileCard />
               </div>
-              <div className="flex w-8/12 flex-col items-center self-center">
-                <AnimatedText text={HOME_TEXT.title} className="!text-left !text-6xl" />
-                <p className="my-4 whitespace-pre-wrap text-base font-medium">{HOME_TEXT.description}</p>
-                <div className="mt-2 flex items-center self-start">
-                  <Link
-                    href="/about"
-                    className="transition-500 flex items-center rounded-lg border-2 border-solid border-transparent bg-dark px-6 py-2.5 text-lg font-semibold text-light hover:border-dark hover:bg-light hover:text-dark"
-                    role="button">
-                    Explore
-                  </Link>
-                </div>
+              <div className="flex h-full w-8/12 flex-col">
+                <h2 className="text-6xl font-extrabold">Persistence pays off</h2>
               </div>
             </div>
           </HomeContentsLayout>
