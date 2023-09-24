2a945c788e0ec0a86d0b33b39fe8dbc695cef376
Yuta Okuma <y.dream.nest@icloud.com>
2023-09-24 16:33:12 +0900

    wip: :construction: create a circle animation object

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -9,13 +9,13 @@ const ProfileCard: FC = () => {
   return (
     <div className="flex flex-col items-end">
       <div className="trapezoid h-12 w-[70%] rounded-tr-2xl bg-green-300" />
-      <div className="flex w-full flex-col items-end rounded-b-2xl rounded-tl-2xl bg-blue-300 px-4 pb-4">
-        <div className="relative">
+      <div className="flex w-full flex-col rounded-b-2xl rounded-tl-2xl bg-blue-300 px-4 pb-4">
+        <div className="relative ml-auto">
           <Image src={Profile} alt="Bear29ers" className="relative z-20 h-auto w-72" role="img" />
           <div className="absolute left-1/2 top-1/2 z-10 h-56 w-56 -translate-x-1/2 -translate-y-1/2 rounded-full bg-pink-300" />
           <div className="absolute left-1/2 top-1/2 z-0 h-64 w-64 -translate-x-1/2 -translate-y-1/2 rounded-full border-[7px] border-white" />
         </div>
-        <div className="-mt-4 mr-auto">
+        <div className="-mt-4 flex items-end justify-between">
           <div>
             <h1 className="text-6xl font-extralight text-white">I&apos;m,</h1>
             <h1 className="text-6xl font-semibold text-white">Yuta</h1>
@@ -26,6 +26,20 @@ const ProfileCard: FC = () => {
               <span>y.earl27.bear@yahoo.ne.jp</span>
               <FiMail className="w-6" />
             </a>
+          </div>
+          <div className="flex-center h-24 w-24 rounded-full bg-stone-600">
+            <svg viewBox="0 0 100 100" className="w-20 overflow-visible">
+              <path
+                id="circle"
+                d="M 10, 50
+                a 40,40 0 1,1 80,0
+                40,40 0 1,1 -80,0"
+                fill="none"
+              />
+              <text fill="#fff" className="text-md font-bold">
+                <textPath xlinkHref="#circle">Frontend Developer & Designer</textPath>
+              </text>
+            </svg>
           </div>
         </div>
       </div>
