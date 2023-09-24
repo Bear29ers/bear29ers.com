70f09298cb5107dd11d5ee76e299ebdb61c67d27
Yuta Okuma <y.dream.nest@icloud.com>
2023-09-24 14:21:50 +0900

    feat: :sparkles: make a profile card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import '@/app/globals.scss';
 import { Montserrat } from 'next/font/google';
 
 const montserrat = Montserrat({
-  weight: ['400', '500', '600', '700', '800', '900'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
   variable: '--font-mont',
 });
