64fef32a71f5525d11c678a9641a0cfbe6ebd93a
Yuta Okuma <y.dream.nest@icloud.com>
2023-09-24 22:54:27 +0900

    wip: :construction: implement right side of home page

--- src/components/ProfileCard/ProfileCard.test.tsx
+++ src/components/ProfileCard/ProfileCard.test.tsx
@@ -1,28 +1,28 @@
 import { render } from '@testing-library/react';
 
-import { TOP_PROFILE_TEXT } from '@/constants/top';
+import { HOME_PROFILE_TEXT } from '@/constants/home';
 
 import ProfileCard from './ProfileCard';
 
 describe('src/components/ProfileCard/ProfileCard.test.tsx', () => {
   it('should render profile picture with alt text', () => {
     const { getByAltText } = render(<ProfileCard />);
-    expect(getByAltText(TOP_PROFILE_TEXT.altText)).toBeInTheDocument();
+    expect(getByAltText(HOME_PROFILE_TEXT.altText)).toBeInTheDocument();
   });
 
   it('should render first and last name as heading', () => {
     const { getByText } = render(<ProfileCard />);
-    expect(getByText(TOP_PROFILE_TEXT.firstName)).toBeInTheDocument();
-    expect(getByText(TOP_PROFILE_TEXT.lastName)).toBeInTheDocument();
+    expect(getByText(HOME_PROFILE_TEXT.firstName)).toBeInTheDocument();
+    expect(getByText(HOME_PROFILE_TEXT.lastName)).toBeInTheDocument();
   });
 
   it('should render email', () => {
     const { getByText } = render(<ProfileCard />);
-    expect(getByText(TOP_PROFILE_TEXT.email)).toBeInTheDocument();
+    expect(getByText(HOME_PROFILE_TEXT.email)).toBeInTheDocument();
   });
 
   it('should render occupation', () => {
     const { getByText } = render(<ProfileCard />);
-    expect(getByText(TOP_PROFILE_TEXT.occupation)).toBeInTheDocument();
+    expect(getByText(HOME_PROFILE_TEXT.occupation)).toBeInTheDocument();
   });
 });
--- src/constants/top.ts
+++
@@ -1,8 +0,0 @@
-// Topページテキスト
-export const TOP_PROFILE_TEXT = {
-  firstName: 'Yuta',
-  lastName: 'Okuma',
-  email: 'y.earl27.bear@yahoo.ne.jp',
-  altText: 'Bear29ers',
-  occupation: 'Frontend Developer & Designer',
-};
