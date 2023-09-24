779e7cc4b9db2c901180cf3218ace69026348c50
Yuta Okuma <y.dream.nest@icloud.com>
2023-09-24 19:08:46 +0900

    test: :green_heart: add ProfileCard component tests

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -3,6 +3,8 @@ import type { FC } from 'react';
 import Image from 'next/image';
 import { FiMail } from 'react-icons/fi';
 
+import { TOP_PROFILE_TEXT } from '@/constants/top';
+
 import Profile from '../../../public/images/memoji.png';
 
 const ProfileCard: FC = () => {
@@ -11,19 +13,19 @@ const ProfileCard: FC = () => {
       <div className="trapezoid h-12 w-[70%] rounded-tr-2xl bg-blue-300" />
       <div className="flex w-full flex-col rounded-b-2xl rounded-tl-2xl bg-blue-300 px-4 pb-4">
         <div className="relative ml-auto">
-          <Image src={Profile} alt="Bear29ers" className="relative z-20 h-auto w-72" role="img" />
+          <Image src={Profile} alt={TOP_PROFILE_TEXT.altText} className="relative z-20 h-auto w-72" role="img" />
           <div className="absolute left-1/2 top-1/2 z-10 h-56 w-56 -translate-x-1/2 -translate-y-1/2 rounded-full bg-pink-300" />
           <div className="absolute left-1/2 top-1/2 z-0 h-64 w-64 -translate-x-1/2 -translate-y-1/2 rounded-full border-[7px] border-white" />
         </div>
         <div className="-mt-4 flex items-end justify-between">
           <div>
             <h1 className="text-6xl font-extralight text-white">I&apos;m,</h1>
-            <h1 className="text-6xl font-semibold text-white">Yuta</h1>
-            <h1 className="text-6xl font-semibold text-white">Okuma</h1>
+            <h1 className="text-6xl font-semibold text-white">{TOP_PROFILE_TEXT.firstName}</h1>
+            <h1 className="text-6xl font-semibold text-white">{TOP_PROFILE_TEXT.lastName}</h1>
             <a
               href="mailto:y.earl27.bear@yahoo.ne.jp"
               className="flex-center mt-5 cursor-pointer gap-x-1.5 border-b border-dashed pb-1.5 text-white">
-              <span>y.earl27.bear@yahoo.ne.jp</span>
+              <span>{TOP_PROFILE_TEXT.email}</span>
               <FiMail className="w-6" />
             </a>
           </div>
@@ -37,7 +39,7 @@ const ProfileCard: FC = () => {
                 fill="none"
               />
               <text fill="#fff" className="text-[15px] font-bold">
-                <textPath xlinkHref="#circle">Frontend Developer & Designer</textPath>
+                <textPath xlinkHref="#circle">{TOP_PROFILE_TEXT.occupation}</textPath>
               </text>
             </svg>
             <span className="absolute h-6 w-6 rounded-full bg-white" />
