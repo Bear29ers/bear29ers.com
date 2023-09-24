98f3d1eb4a304861c02c37722b307f4423eb4e8b
Yuta Okuma <y.dream.nest@icloud.com>
2023-09-24 19:08:46 +0900

    test: :green_heart: add ProfileCard component tests

---
+++ src/constants/top.ts
@@ -0,0 +1,8 @@
+// Topページテキスト
+export const TOP_PROFILE_TEXT = {
+  firstName: 'Yuta',
+  lastName: 'Okuma',
+  email: 'y.earl27.bear@yahoo.ne.jp',
+  altText: 'Bear29ers',
+  occupation: 'Frontend Developer & Designer',
+};
