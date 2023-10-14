fea7b88c0b0ae2eb32baceb4e1cbdd21f9eea551
Yuta Okuma <y.dream.nest@icloud.com>
2023-10-14 18:31:52 +0900

    chore: :paperclip: Define sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,15 +19,15 @@ const Home: NextPage = () => {
               <div className="flex w-[62%] flex-col gap-y-8 bg-sky-100">
                 <h2 className="bg-violet-200 text-6xl font-extrabold">{HOME_TEXT.quote}</h2>
                 <div className="flex h-72 w-full gap-x-8 rounded-2xl bg-green-200">
-                  <div className="h-72 w-9/12 rounded-2xl bg-orange-200">work1</div>
+                  <div className="h-72 w-9/12 rounded-2xl bg-orange-200">Gallery</div>
                   <div className="flex h-72 w-3/12 flex-col rounded-2xl bg-yellow-200">
-                    <div className="h-1/2 rounded-t-2xl bg-rose-200">work2</div>
-                    <div className="h-1/2 rounded-b-2xl bg-fuchsia-200">work3</div>
+                    <div className="h-1/2 rounded-t-2xl bg-rose-200">Time</div>
+                    <div className="h-1/2 rounded-b-2xl bg-fuchsia-200">Weather</div>
                   </div>
                 </div>
                 <div className="flex w-full grow gap-x-8 rounded-2xl bg-purple-200">
-                  <div className="w-5/12 rounded-2xl bg-amber-200">work4</div>
-                  <div className="w-7/12 rounded-2xl bg-lime-200">work4</div>
+                  <div className="w-5/12 rounded-2xl bg-amber-200">work4(TBD)</div>
+                  <div className="w-7/12 rounded-2xl bg-lime-200">work4(TBD)</div>
                 </div>
               </div>
             </div>
