87a8f77b9aec86b23c88e0c6a066d9c5355e03de
Yuta Okuma <y.dream.nest@icloud.com>
2023-10-28 19:56:12 +0900

    refactor: :bulb: make SocialIcons global

--- src/components/SocialIcons/SocialIcons.test.tsx
+++ src/components/SocialIcons/SocialIcons.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 
 import SocialIcons from './SocialIcons';
 
-describe('src/components/common/SocialIcons/SocialIcons.test.tsx', () => {
+describe('src/components/SocialIcons/SocialIcons.test.tsx', () => {
   it('should render SocialIcons component', () => {
     const { getByRole } = render(<SocialIcons />);
   });
