37894eb1f7a4a8651d14d3c175cf5659bc45f850
Yuta Okuma <y.dream.nest@icloud.com>
2023-10-28 21:18:47 +0900

    feat: :sparkles: create a bear

--- src/components/Bear/Bear.tsx
+++ src/components/Bear/Bear.tsx
@@ -1,20 +1,21 @@
+/* eslint-disable tailwindcss/no-custom-classname */
 import type { FC } from 'react';
 
 const Bear: FC = () => {
   return (
     <>
-      <div className="bear">
-        <div className="bear__ears">
-          <div className="bear__ears__left ear" />
-          <div className="bear__ears__right ear" />
+      <div className="w-[60px]">
+        <div className="flex translate-y-[2.5px] justify-between">
+          <div className="h-5 w-[22px] skew-y-[-5deg] rounded-[50%] bg-millbrook" />
+          <div className="h-5 w-[22px] skew-y-[5deg] rounded-[50%] bg-millbrook" />
         </div>
-        <div className="bear__body">
-          <div className="bear__eyes">
-            <div className="bear__eyes--left eye" />
-            <div className="bear__eyes--right eye" />
+        <div className="bear-body-radius h-[57.5px] w-full bg-millbrook">
+          <div className="bear__eyes bear-eyes-transition relative flex translate-y-[15px] justify-around">
+            <div className="bear-eye-transition left-[12.5px] h-[5px] w-[5px] rounded-[50%] bg-coffeeBean" />
+            <div className="bear-eye-transition right-[12.5px] h-[5px] w-[5px] rounded-[50%] bg-coffeeBean" />
           </div>
-          <div className="bear__nose">
-            <div className="bear__nose--inner" />
+          <div className="bear-nose-radius bear-nose-transition relative m-auto h-[25px] w-5 translate-y-[20px] bg-tan">
+            <div className="bear-nose-inner-radius after:bear-nose-inner-after absolute left-1/2 top-1/2 h-[6.5px] w-[12.5px] -translate-x-1/2 -translate-y-1/2 bg-coffeeBean" />
           </div>
         </div>
       </div>
