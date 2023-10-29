3b6bc9fdefa1f48ee16ffad2d88707bb7f427330
Yuta Okuma <y.dream.nest@icloud.com>
2023-10-29 11:41:53 +0900

    feat: :sparkles: set the bear animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,52 @@
 
 import { useState } from 'react';
 
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 
 import Bear from '@/components/Bear/Bear';
 import SocialIcons from '@/components/SocialIcons/SocialIcons';
 
-// import { motion } from 'framer-motion';
-
 // import { HOME_TEXT } from '@/constants/home';
 
 import type { NextPage } from 'next';
 
-// const variants = {};
+const variants = {
+  default: { x: '0%', y: '0%' },
+  clicked: { x: '40%', y: '32%' },
+};
 
 const Home: NextPage = () => {
-  const [_, setIsClicked] = useState<boolean>(false);
+  const [isClicked, setIsClicked] = useState<boolean>(false);
 
   return (
     <main role="main" className="relative h-screen w-screen bg-iron">
       {/* Bear */}
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <motion.div
+        className="flex-center absolute inset-0"
+        animate={isClicked ? 'clicked' : 'default'}
+        variants={variants}
+        transition={{
+          duration: 1.0,
+          ease: 'easeInOut',
+        }}>
         <Bear setIsClicked={setIsClicked} />
-      </div>
+      </motion.div>
       {/* Social Icons */}
       <SocialIcons />
       {/* Page Navigation */}
-      <div className="absolute right-0 top-1/2 -translate-y-1/2 rotate-90 text-lg line-through">Works</div>
-      <Link href="/about" className="absolute bottom-2 left-1/2 -translate-x-1/2 text-lg">
-        About
-      </Link>
-      <div className="absolute left-0 top-1/2 -translate-y-1/2 -rotate-90 text-lg line-through">Gallery</div>
+      <div className="absolute right-0 top-1/2 -translate-y-1/2 rotate-90 text-2xl font-semibold line-through">
+        Works
+      </div>
+      <div className="absolute bottom-3 flex w-screen justify-around">
+        <Link href="/about" className="text-2xl font-semibold">
+          About
+        </Link>
+        <div className="text-2xl font-semibold line-through">Experience</div>
+      </div>
+      <div className="absolute left-0 top-1/2 -translate-y-1/2 -rotate-90 text-2xl font-semibold line-through">
+        Gallery
+      </div>
     </main>
   );
 };
