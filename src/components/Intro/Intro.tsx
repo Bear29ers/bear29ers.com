a184ca8dc4f5d432c87562b0c0b2ffad88f85657
Yuta Okuma <y.dream.nest@icloud.com>
2023-12-17 00:22:35 +0900

    ui: :lipstick: complete Intro component

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import type { FC } from 'react';
 
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 
 import { HOME_PROFILE_TEXT } from '@/constants/home';
@@ -9,21 +10,35 @@ import Profile from '../../../public/images/memoji.png';
 
 const Intro: FC = () => {
   return (
-    <div className="intro-dark-bg flex w-[60vw]">
-      <div className="relative flex w-1/2 flex-col gap-y-10">
-        <h1 className="text-6xl font-bold text-iron">Hi,</h1>
-        <h1 className="text-5xl font-semibold text-iron">I&apos;m, {HOME_PROFILE_TEXT.userName}.</h1>
-        <h2 className="text-2xl font-semibold text-iron">I&apos;m, {HOME_PROFILE_TEXT.occupation}.</h2>
+    <motion.div
+      className="intro-dark-bg flex h-[35vh] w-[60vw]"
+      initial={{ height: 0 }}
+      animate={{ height: '35vh' }}
+      transition={{ type: 'spring', duration: 2, delay: 1 }}>
+      <div className="relative flex w-1/2 flex-col justify-evenly p-5">
+        <h1 className="text-7xl font-bold text-iron">Hi,</h1>
+        <h1 className="text-6xl font-semibold text-iron">I&apos;m, {HOME_PROFILE_TEXT.userName}.</h1>
+        <motion.h2
+          className="text-4xl font-semibold text-hitGray"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 1.5 }}>
+          I&apos;m, {HOME_PROFILE_TEXT.occupation}.
+        </motion.h2>
       </div>
-      <div className="relative flex w-1/2 text-white">
+      <motion.div
+        className="relative flex w-1/2 text-white"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 2 }}>
         <Image
           src={Profile}
           alt="bear29ers"
           role="img"
           className="absolute bottom-0 left-1/2 h-auto w-full -translate-x-1/2"
         />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
