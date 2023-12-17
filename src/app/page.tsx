3451f21f9de3dd1e80ff33fe1ffdd6ea84bcab69
Yuta Okuma <y.dream.nest@icloud.com>
2023-12-17 21:43:55 +0900

    :dizzy: anm: add initial animation in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,26 +41,40 @@ const Home: NextPage = () => {
       {/* Social Icons */}
       <SocialIcons isClicked={isClicked} />
       {/* Page Navigation TODO: コンポーネント化したい */}
-      <div className="absolute right-0 top-1/2 -translate-y-1/2 rotate-90 text-2xl font-semibold text-dark line-through">
+      <motion.div
+        className="absolute right-0 top-1/2 -translate-y-1/2 rotate-90 text-2xl font-semibold text-dark line-through"
+        initial={{ x: 200, rotate: 90 }}
+        animate={{ x: 0, rotate: 90 }}
+        transition={{ type: 'spring', duration: 1.5, delay: 1.0 }}>
         Works
-      </div>
+      </motion.div>
       <div className="absolute bottom-3 flex w-screen justify-around">
         <MotionLink
           href="/about"
           className={`text-2xl font-semibold ${isClicked ? 'text-iron' : 'text-dark'}`}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          transition={{ type: 'spring', stiffness: 400, damping: 10 }}>
+          initial={{ y: 200 }}
+          animate={{ y: 0, transition: { type: 'spring', duration: 1.5, delay: 1.0 } }}
+          whileHover={{ scale: 1.1, transition: { type: 'spring', stiffness: 400, damping: 10 } }}
+          whileTap={{ scale: 0.9, transition: { type: 'spring', stiffness: 400, damping: 10 } }}>
           About
         </MotionLink>
-        <div className="text-2xl font-semibold text-dark line-through">Experience</div>
+        <motion.div
+          className="text-2xl font-semibold text-dark line-through"
+          initial={{ y: 200 }}
+          animate={{ y: 0 }}
+          transition={{ type: 'spring', duration: 1.5, delay: 1.0 }}>
+          Experience
+        </motion.div>
       </div>
-      <div
+      <motion.div
         className={`absolute left-0 top-1/2 -translate-y-1/2 -rotate-90 text-2xl font-semibold line-through ${
           isClicked ? 'text-iron' : 'text-dark'
-        }`}>
+        }`}
+        initial={{ x: -200, rotate: -90 }}
+        animate={{ x: 0, rotate: -90 }}
+        transition={{ type: 'spring', duration: 1.5, delay: 1.0 }}>
         Gallery
-      </div>
+      </motion.div>
     </main>
   );
 };
