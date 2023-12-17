0a5f5a9e577c478215ddc2f4933d90c3baed19ea
Yuta Okuma <y.dream.nest@icloud.com>
2023-12-17 21:43:03 +0900

    :wrench: conf: update tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   mode: 'jit',
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
