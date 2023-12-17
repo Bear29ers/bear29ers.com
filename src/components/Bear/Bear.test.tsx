6e505fb17395562d590fd6b8ab67af750782f4b3
Yuta Okuma <y.dream.nest@icloud.com>
2023-12-17 20:28:21 +0900

    :bulb: cmt: add some TODO comments

--- src/components/Bear/Bear.test.tsx
+++ src/components/Bear/Bear.test.tsx
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import Bear from './Bear';
 
 describe('src/components/Bear/Bear.test.tsx', () => {
+  // TODO: テストを書く → クリックテストも必要
   it('should render Bear component', () => {
     const { getByRole } = render(<Bear />);
   });
