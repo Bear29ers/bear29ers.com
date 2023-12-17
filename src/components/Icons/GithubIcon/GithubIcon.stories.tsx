43872b2184a020ca63df01907e8e57dab09af6b7
Yuta Okuma <y.dream.nest@icloud.com>
2023-12-17 19:55:35 +0900

    :white_check_mark: test: update some Icons storybook

--- src/components/Icons/GithubIcon/GithubIcon.stories.tsx
+++ src/components/Icons/GithubIcon/GithubIcon.stories.tsx
@@ -10,14 +10,26 @@ export default meta;
 
 type Story = StoryObj<typeof GithubIcon>;
 
-export const Standard: Story = {
-  render: () => <GithubIcon />,
+export const IsClickedTrue: Story = {
+  render: () => <GithubIcon isClicked />,
 };
 
-export const W20: Story = {
+export const IsClickedFalse: Story = {
+  render: () => <GithubIcon isClicked={false} />,
+};
+
+export const W20IsClickedTrue: Story = {
   render: () => (
     <div className="w-20">
-      <GithubIcon />
+      <GithubIcon isClicked />
+    </div>
+  ),
+};
+
+export const W20IsClickedFalse: Story = {
+  render: () => (
+    <div className="w-20">
+      <GithubIcon isClicked={false} />
     </div>
   ),
 };
