1f4492d7d6da198f19799146e6dc322dfdc64ffc
Yuta Okuma <y.dream.nest@icloud.com>
2023-12-17 18:54:05 +0900

    :white_check_mark: test: update the Bear component storybook

--- src/components/Bear/Bear.stories.tsx
+++ src/components/Bear/Bear.stories.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Bear from './Bear';
 
 import type { Meta, StoryObj } from '@storybook/react';
@@ -11,5 +13,12 @@ export default meta;
 type Story = StoryObj<typeof Bear>;
 
 export const Standard: Story = {
-  render: () => <Bear />,
+  render: function Compo() {
+    const [_, setValue] = useState<boolean>(false);
+    return (
+      <div className="relative h-screen w-screen">
+        <Bear setIsClicked={setValue} />
+      </div>
+    );
+  },
 };
