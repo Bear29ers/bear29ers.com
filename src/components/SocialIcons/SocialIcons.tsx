62cd22bd277f32a092fb4e668b8a987f0ed8f3b1
Yuta Okuma <y.dream.nest@icloud.com>
2023-12-18 00:41:54 +0900

    :dizzy: anm: add socialicon animation

--- src/components/SocialIcons/SocialIcons.tsx
+++ src/components/SocialIcons/SocialIcons.tsx
@@ -21,13 +21,22 @@ const SocialIcons: FC<Props> = ({ isClicked }) => {
           className="w-6"
           role="link"
           aria-describedby={socialMedia.describedby}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          transition={{ type: 'spring', stiffness: 400, damping: 10 }}>
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1.0,
+            transition: { type: 'spring', duration: 1.0, delay: 1.0 + 0.3 * socialMedia.id },
+          }}
+          whileHover={{ scale: 1.1, transition: { type: 'spring', stiffness: 400, damping: 10 } }}
+          whileTap={{ scale: 0.9, transition: { type: 'spring', stiffness: 400, damping: 10 } }}>
           <Icon iconName={socialMedia.text} isClicked={isClicked} />
         </motion.a>
       ))}
-      <span className={`h-32 w-0.5 ${isClicked ? 'bg-iron' : 'bg-dark'}`} />
+      <motion.span
+        className={`h-32 w-0.5 ${isClicked ? 'bg-iron' : 'bg-dark'}`}
+        initial={{ height: '0' }}
+        animate={{ height: '128px' }}
+        transition={{ type: 'spring', duration: 1.0, delay: 0.8 }}
+      />
     </div>
   );
 };
