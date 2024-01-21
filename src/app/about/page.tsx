7c9e8b27b3e3095573377b9e852157695e65dd71
Yuta Okuma <y.dream.nest@icloud.com>
2024-01-21 18:24:21 +0900

    :sparkles: feat: apply PageTransitionEffect to pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,16 +2,20 @@ import Profile from '@/components/Profile/Profile';
 import AnimatedText from '@/components/commons/AnimatedText/AnimatedText';
 import ContentsLayout from '@/components/layouts/ContentsLayout/ContentsLayout';
 
+import PageTransitionEffect from '../PageTransitionEffect';
+
 import type { NextPage } from 'next';
 
 const About: NextPage = () => {
   return (
-    <main className="flex-center w-full flex-col bg-light" role="main">
-      <ContentsLayout className="mt-40">
-        <AnimatedText text="Persistence pays off!" className="mb-16" />
-        <Profile />
-      </ContentsLayout>
-    </main>
+    <PageTransitionEffect>
+      <main className="flex-center w-full flex-col bg-light" role="main">
+        <ContentsLayout className="mt-40">
+          <AnimatedText text="Persistence pays off!" className="mb-16" />
+          <Profile />
+        </ContentsLayout>
+      </main>
+    </PageTransitionEffect>
   );
 };
 
