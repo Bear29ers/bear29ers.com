b6642ecedf48c94aa0c1fc662a9e2a4a9dc46d62
Yuta Okuma <y.dream.nest@icloud.com>
2024-01-28 18:39:27 +0900

    :lipstick: ui: change opacity when pathname and href is same

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,5 +1,8 @@
 import type { FC } from 'react';
 
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 import { MENU_ITEMS } from '@/constants/menu';
 
 interface Props {
@@ -7,23 +10,24 @@ interface Props {
 }
 
 const Menu: FC<Props> = ({ menuIsActive }) => {
+  const pathname = usePathname();
+
   return (
-    // TODO: usePathnameでパス名を取得して、現在のページのメニューのopacityを100にする
     // TODO: 表示する際にピクセルバックグラウンドに合わせて、少しdelayをつける + opacityでアニメーションする
     <div className={`relative z-40 h-screen w-screen ${menuIsActive ? 'block' : 'hidden'}`}>
       <div className="flex-center absolute inset-0 -left-1/4">
         <ul className="flex flex-col gap-y-4">
-          {/* TODO: constantsからデータだけを引っ張ってきて、ループで作成する */}
           {MENU_ITEMS.map((menuItem) => (
+            // TODO: framer-motionでホバーリンクをアニメーションさせる
             <li key={menuItem.id}>
-              {/* TODO: Linkコンポーネントに変更する */}
-              <a
+              <Link
                 className={`text-9xl font-black text-dark opacity-30 transition-opacity hover:opacity-100 ${
-                  !menuItem.isValid ? 'line-through opacity-10 hover:opacity-10' : ''
-                }`}
-                href={menuItem.href}>
+                  !menuItem.isValid ? 'pointer-events-none line-through opacity-10 hover:opacity-10' : ''
+                } ${menuItem.href === pathname ? 'opacity-100' : ''}`}
+                href={menuItem.href}
+                aria-disabled={!menuItem.isValid}>
                 {menuItem.title}
-              </a>
+              </Link>
             </li>
           ))}
         </ul>
