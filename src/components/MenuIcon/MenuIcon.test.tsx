70c167b87c1c5ffbbed500eb42389ecdb33c799b
Yuta Okuma <y.dream.nest@icloud.com>
2024-01-28 13:57:32 +0900

    :white_check_mark: test: add test for MenuIcon

--- src/components/MenuIcon/MenuIcon.test.tsx
+++ src/components/MenuIcon/MenuIcon.test.tsx
@@ -1,9 +1,34 @@
-import { render } from '@testing-library/react';
+/* eslint-disable @typescript-eslint/no-empty-function */
+
+import React from 'react';
+
+import { fireEvent, render } from '@testing-library/react';
 
 import MenuIcon from './MenuIcon';
 
 describe('src/components/MenuIcon/MenuIcon.test.tsx', () => {
-  it('should render the MenuIcon component', () => {
-    const { getByRole } = render(<MenuIcon />);
+  it('should render the MenuIcon component with the button element', () => {
+    const { getByRole } = render(<MenuIcon menuIsActive={false} setMenuIsActive={() => {}} />);
+    expect(getByRole('button')).toBeInTheDocument();
   });
+
+  it('should calls setMenuIsActive when the button is clicked', () => {
+    const setMenuIsActiveMock = jest.fn();
+    const { getByRole } = render(<MenuIcon menuIsActive={false} setMenuIsActive={setMenuIsActiveMock} />);
+    const buttonElement = getByRole('button');
+
+    fireEvent.click(buttonElement);
+    expect(setMenuIsActiveMock).toHaveBeenCalled();
+  });
+
+  it('should toggle menuIsActive state when the button is clicked', () => {
+    const { getAllByTestId } = render(<MenuIcon menuIsActive setMenuIsActive={() => {}} />);
+
+    const lineElements = getAllByTestId('menu-icon-line');
+    lineElements.forEach((lineElement) => {
+      expect(lineElement).toHaveClass('translate-x-10');
+    });
+  });
+
+  // TODO: useStateのテスト
 });
--- src/components/MenuIcon/MenuIcon.tsx
+++ src/components/MenuIcon/MenuIcon.tsx
@@ -1,15 +1,50 @@
-import type { FC } from 'react';
+import type { Dispatch, FC, SetStateAction } from 'react';
 
-const MenuIcon: FC = () => {
+interface Props {
+  menuIsActive: boolean;
+  setMenuIsActive: Dispatch<SetStateAction<boolean>>;
+}
+
+const MenuIcon: FC<Props> = ({ menuIsActive, setMenuIsActive }) => {
+  const handleClick = () => {
+    setMenuIsActive((prevMenuIsActive) => !prevMenuIsActive);
+  };
+
   return (
-    <button className="group relative overflow-hidden" type="button">
+    <button className="group relative overflow-hidden" type="button" onClick={handleClick}>
       <div className="flex h-7 w-8 origin-center flex-col justify-between transition-all duration-300">
-        <div className="h-1 w-8 origin-left rounded bg-dark transition-all duration-300 group-focus:translate-x-10" />
-        <div className="h-1 w-8 origin-left rounded bg-dark transition-all delay-75 duration-300 group-focus:translate-x-10" />
-        <div className="h-1 w-8 origin-left rounded bg-dark transition-all delay-150 duration-300 group-focus:translate-x-10" />
-        <div className="absolute inset-0 flex w-0 -translate-x-10 items-center justify-between transition-all duration-500 group-focus:w-12 group-focus:translate-x-0">
-          <div className="absolute h-1 w-7 rotate-0 rounded bg-dark transition-all delay-300 duration-500 group-focus:rotate-45" />
-          <div className="absolute h-1 w-7 -rotate-0 rounded bg-dark transition-all delay-300 duration-500 group-focus:-rotate-45" />
+        <div
+          className={`h-1 w-8 origin-left rounded bg-dark transition-all duration-300 ${
+            menuIsActive ? 'translate-x-10' : ''
+          }`}
+          data-testid="menu-icon-line"
+        />
+        <div
+          className={`h-1 w-8 origin-left rounded bg-dark transition-all delay-75 duration-300 ${
+            menuIsActive ? 'translate-x-10' : ''
+          }`}
+          data-testid="menu-icon-line"
+        />
+        <div
+          className={`h-1 w-8 origin-left rounded bg-dark transition-all delay-150 duration-300 ${
+            menuIsActive ? 'translate-x-10' : ''
+          }`}
+          data-testid="menu-icon-line"
+        />
+        <div
+          className={`absolute inset-0 flex w-0 -translate-x-10 items-center justify-between transition-all duration-500 group-focus:w-12 ${
+            menuIsActive ? 'translate-x-0' : ''
+          }`}>
+          <div
+            className={`absolute h-1 w-7 rotate-0 rounded bg-dark transition-all delay-300 duration-500 ${
+              menuIsActive ? 'rotate-45' : ''
+            }`}
+          />
+          <div
+            className={`absolute h-1 w-7 -rotate-0 rounded bg-dark transition-all delay-300 duration-500 ${
+              menuIsActive ? '-rotate-45' : ''
+            }`}
+          />
         </div>
       </div>
     </button>
