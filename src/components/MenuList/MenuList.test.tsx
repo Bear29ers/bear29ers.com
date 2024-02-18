a1bf39214fcb520a24e1dc75739c144164bdbd0d
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-18 11:24:05 +0900

    :fire: rem: remove all files of the project

--- src/components/MenuList/MenuList.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-
-import MenuList from './MenuList';
-
-describe('src/components/MenuList/MenuList.test.tsx', () => {
-  it('should render the MenuList component', () => {
-    const { getByRole } = render(<MenuList />);
-  });
-});
