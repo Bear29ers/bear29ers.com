1cb228d869f14b441d12b913a2fd956192e12e0a
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-19 16:28:48 +0900

    :sparkles: feat: restore the Intro component

---
+++ src/common/constants/home.ts
@@ -0,0 +1,10 @@
+// プロフィールテキスト
+export const HOME_PROFILE_TEXT = {
+  firstName: 'Yuta',
+  lastName: 'Okuma',
+  fullName: 'Yuta Okuma',
+  userName: 'Bear29ers',
+  email: 'y.earl27.bear@yahoo.ne.jp',
+  occupation: 'Frontend Developer & Designer',
+  profileAlt: 'bear29ers',
+};
