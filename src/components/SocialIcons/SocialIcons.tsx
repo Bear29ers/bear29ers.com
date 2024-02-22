7f3f9d200a49bccc695a7a8987caa55822ccf72d
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-22 11:18:58 +0900

    :sparkles: feat: create a SocialIcons component

---
+++ src/components/SocialIcons/SocialIcons.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+const SocialIcons: FC<Props> = ({ isClicked }) => {
+  return (
+    <div>
+      <h1>SocialIcons</h1>
+    </div>
+  );
+};
+
+export default SocialIcons;
