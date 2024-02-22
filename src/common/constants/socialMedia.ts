4b249d77b6fe6b1af8bb409dc7753c1b4765f375
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-22 11:41:25 +0900

    :construction: wip: updating the SocialIcon component

---
+++ src/common/constants/socialMedia.ts
@@ -0,0 +1,24 @@
+import type { SocialMedia } from '../types/socialMedia';
+
+export const SOCIAL_MEDIA_LIST: SocialMedia[] = [
+  {
+    id: 1,
+    href: 'https://github.com/Bear29ers',
+    text: 'github',
+  },
+  {
+    id: 2,
+    href: 'https://www.instagram.com/bear_27earl',
+    text: 'instagram',
+  },
+  {
+    id: 3,
+    href: 'https://www.threads.net/@bear_27earl',
+    text: 'threads',
+  },
+  {
+    id: 4,
+    href: 'https://www.facebook.com/kumakuma1129',
+    text: 'facebook',
+  },
+];
--- src/components/SocialIcons/SocialIcons.tsx
+++ src/components/SocialIcons/SocialIcons.tsx
@@ -1,6 +1,11 @@
 import type { FC } from 'react';
 
+interface Props {
+  isClicked: boolean;
+}
+
 const SocialIcons: FC<Props> = ({ isClicked }) => {
+  console.log('isClicked: ', isClicked);
   return (
     <div>
       <h1>SocialIcons</h1>
