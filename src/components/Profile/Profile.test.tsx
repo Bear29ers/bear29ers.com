2f372952fe9d63c873df8c072772cc10f3f727bb
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-23 21:11:06 +0900

    :sparkles: feat: complete the Biography component

--- src/components/Profile/Profile.test.tsx
+++ src/components/Profile/Profile.test.tsx
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Profile from './Profile';
 
 describe('src/components/Profile/Profile.test.tsx', () => {
   it('should render the Profile component', () => {
-    const { getByRole } = render(<Profile />);
+    render(<Profile />);
+    expect(screen.getByRole('presentation')).toBeInTheDocument();
   });
 });
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,15 @@
 import type { FC } from 'react';
 
+import Biography from '@/components/profiles/Biography/Biography';
+
 const Profile: FC = () => {
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="mx-auto flex w-full max-w-screen-xl  pt-24 font-sans text-dark" role="presentation">
+      <div className="col-span-3 w-full max-w-xs bg-light">
+        {/* Biography */}
+        <Biography />
+      </div>
+      <div className="col-span-6 w-full bg-lime-300" />
     </div>
   );
 };
