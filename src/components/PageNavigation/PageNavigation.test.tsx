5825aca1018a94b464008726043fd64bfb070230
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-23 18:20:01 +0900

    :white_check_mark: test: complete the PageNavigation test

--- src/components/PageNavigation/PageNavigation.test.tsx
+++ src/components/PageNavigation/PageNavigation.test.tsx
@@ -1,10 +1,80 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import PageNavigation from './PageNavigation';
 
+import type { RenderResult } from '@testing-library/react';
+
 describe('src/components/PageNavigation/PageNavigation.test.tsx', () => {
-  it('should render the PageNavigation component', () => {
-    const { getByRole } = render(<PageNavigation />);
+  describe('when isClicked props is false', () => {
+    let renderResult: RenderResult;
+
+    beforeEach(() => {
+      renderResult = render(<PageNavigation isClicked={false} />);
+    });
+
+    afterEach(() => {
+      renderResult.unmount();
+    });
+
+    it('should render the Works text', () => {
+      expect(screen.getByText('Works')).toBeInTheDocument();
+    });
+
+    it('should render the About link', () => {
+      expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+    });
+
+    it('should have text-dark class with the About link', () => {
+      expect(screen.getByRole('link', { name: 'About' })).toHaveClass('text-dark');
+    });
+
+    it('should render the Experience text', () => {
+      expect(screen.getByText('Experience')).toBeInTheDocument();
+    });
+
+    it('should render the Gallery text', () => {
+      expect(screen.getByText('Gallery')).toBeInTheDocument();
+    });
+
+    it('should have text-dark class with the Gallery text', () => {
+      expect(screen.getByText('Gallery')).toHaveClass('text-dark');
+    });
+  });
+
+  describe('when isClicked props is true', () => {
+    let renderResult: RenderResult;
+
+    beforeEach(() => {
+      renderResult = render(<PageNavigation isClicked />);
+    });
+
+    afterEach(() => {
+      renderResult.unmount();
+    });
+
+    it('should render the Works text', () => {
+      expect(screen.getByText('Works')).toBeInTheDocument();
+    });
+
+    it('should render the About link', () => {
+      expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+    });
+
+    it('should have text-dark class with the About link', () => {
+      expect(screen.getByRole('link', { name: 'About' })).toHaveClass('text-iron');
+    });
+
+    it('should render the Experience text', () => {
+      expect(screen.getByText('Experience')).toBeInTheDocument();
+    });
+
+    it('should render the Gallery text', () => {
+      expect(screen.getByText('Gallery')).toBeInTheDocument();
+    });
+
+    it('should have text-dark class with the Gallery text', () => {
+      expect(screen.getByText('Gallery')).toHaveClass('text-iron');
+    });
   });
 });
