44455d379b28e893d9642a2b462c637d41099b9e
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-23 23:29:44 +0900

    :construction: wip: creating the SkillAndActivity test

--- src/components/profiles/SkillAndActivity/SkillAndActivity.tsx
+++ src/components/profiles/SkillAndActivity/SkillAndActivity.tsx
@@ -9,18 +9,27 @@ const SkillAndActivity: FC = () => {
     <div className="rounded-md border border-geyser p-6">
       <div className="mb-5 flex items-center justify-between">
         <p className="font-mono text-xs">
-          Bear29ers<span className="px-px text-hitGray">/</span>README<span className="text-hitGray">.md</span>
+          {PROFILE_TEXT.userName}
+          <span className="px-px text-hitGray">/</span>README<span className="text-hitGray">.md</span>
         </p>
         <div className="size-4">
           <PencilIcon />
         </div>
       </div>
       <div className="flex flex-col gap-y-16">
-        <div className="mb-6 flex items-end gap-x-3">
-          <h3 className="text-xl font-bold">{PROFILE_TEXT.shortGreeting}</h3>
-          <WavingHand />
+        <div>
+          <div className="mb-6 flex items-end gap-x-3">
+            <h3 className="text-xl font-bold">{PROFILE_TEXT.shortGreeting}</h3>
+            <WavingHand />
+          </div>
+          <h2 className="text-2xl font-bold">{PROFILE_TEXT.title}</h2>
+          <hr className="mb-6 mt-2 bg-geyser" />
+          <div className="flex flex-col gap-y-4">
+            {PROFILE_TEXT.biographyList.map((biography) => (
+              <p key={biography.id}>{biography.text}</p>
+            ))}
+          </div>
         </div>
-        <h2>{PROFILE_TEXT.title}</h2>
       </div>
     </div>
   );
