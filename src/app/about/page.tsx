627a1946db33921be479f1ef502138805f4b756a
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-23 23:56:09 +0900

    :sparkles: feat: create an Edit component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,21 @@
-import Profile from '@/components/Profile/Profile';
+import Edit from '@/components/profiles/Edit/Edit';
+import Status from '@/components/profiles/Status/Status';
 
 import type { NextPage } from 'next';
 
 const About: NextPage = () => {
   return (
     <main className="flex-center w-full flex-col bg-iron text-dark">
-      <Profile />
+      <div className="mx-auto flex w-full max-w-screen-xl pt-24 font-sans">
+        <div className="col-span-3 w-full max-w-xs bg-light">
+          {/* Status */}
+          <Status />
+        </div>
+        <div className="grow rounded-lg border border-geyser bg-lime-100 p-6">
+          {/* Edit */}
+          <Edit />
+        </div>
+      </div>
     </main>
   );
 };
