b08a13d9a71c0adc98767c03e60c45d3ba317c85
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-23 19:05:54 +0900

    :sparkles: feat: create a Biography component by template

---
+++ src/components/profiles/Biography/Biography.stories.tsx
@@ -0,0 +1,15 @@
+import Biography from './Biography';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Biography> = {
+  component: Biography,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Biography>;
+
+export const Default: Story = {
+  render: () => <Biography />,
+};
