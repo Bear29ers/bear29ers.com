caa398a8ad8314847312932646222a610532c3ed
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-23 20:21:49 +0900

    :white_check_mark: test: complete the ProfileIcons test

--- src/components/icons/ProfileIcons/ProfileIcons.test.tsx
+++ src/components/icons/ProfileIcons/ProfileIcons.test.tsx
@@ -1,10 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import ProfileIcons from './ProfileIcons';
+import { LocationIcon, OrganizationIcon, WebsiteIcon } from './ProfileIcons';
 
 describe('src/components/icons/ProfileIcons/ProfileIcons.test.tsx', () => {
-  it('should render the ProfileIcons component', () => {
-    const { getByRole } = render(<ProfileIcons />);
+  it('should render the OrganizationIcon svg', () => {
+    render(<OrganizationIcon />);
+    expect(screen.getByRole('img', { name: 'OrganizationIcon' })).toBeInTheDocument();
+  });
+
+  it('should render the LocationIcon svg', () => {
+    render(<LocationIcon />);
+    expect(screen.getByRole('img', { name: 'LocationIcon' })).toBeInTheDocument();
+  });
+
+  it('should render the WebsiteIcon svg', () => {
+    render(<WebsiteIcon />);
+    expect(screen.getByRole('img', { name: 'WebsiteIcon' })).toBeInTheDocument();
   });
 });
