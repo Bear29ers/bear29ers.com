e3bee6abfb9ba4bdb2491dc8293f94a5a470f7d3
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-23 17:10:29 +0900

    :sparkles: feat: implement the SocialIcon component

--- src/components/SocialIcons/SocialIcons.tsx
+++ src/components/SocialIcons/SocialIcons.tsx
@@ -1,14 +1,57 @@
 import type { FC } from 'react';
 
+import { motion } from 'framer-motion';
+
+import FacebookIcon from '@/components/icons/FacebookIcon/FacebookIcon';
+import GithubIcon from '@/components/icons/GithubIcon/GithubIcon';
+import InstagramIcon from '@/components/icons/InstagramIcon/InstagramIcon';
+import ThreadsIcon from '@/components/icons/ThreadsIcon/ThreadsIcon';
+
+import { SOCIAL_MEDIA_LIST } from '@/common/constants/socialMedia';
+
 interface Props {
   isClicked: boolean;
 }
 
-const SocialIcons: FC<Props> = ({ isClicked }) => {
-  console.log('isClicked: ', isClicked);
+const getIconComponent = (iconName: string, isClicked: boolean) => {
   return (
-    <div>
-      <h1>SocialIcons</h1>
+    <>
+      {iconName === 'github' && <GithubIcon isClicked={isClicked} />}
+      {iconName === 'instagram' && <InstagramIcon />}
+      {iconName === 'threads' && <ThreadsIcon isClicked={isClicked} />}
+      {iconName === 'facebook' && <FacebookIcon />}
+    </>
+  );
+};
+
+const SocialIcons: FC<Props> = ({ isClicked }) => {
+  return (
+    <div className="fixed bottom-0 left-8 flex flex-col items-center gap-y-4">
+      {SOCIAL_MEDIA_LIST.map((socialMedia) => (
+        <motion.a
+          key={socialMedia.id}
+          href={socialMedia.href}
+          target="_blank"
+          className="w-6"
+          role="link"
+          aria-describedby={socialMedia.text}
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1.0,
+            transition: { type: 'spring', duration: 1.0, delay: 1.0 + 0.3 * socialMedia.id },
+          }}
+          whileHover={{ scale: 1.1, transition: { type: 'spring', stiffness: 400, damping: 10 } }}
+          whileTap={{ scale: 0.9, transition: { type: 'spring', stiffness: 400, damping: 10 } }}>
+          {getIconComponent(socialMedia.text, isClicked)}
+        </motion.a>
+      ))}
+      <motion.span
+        className={`h-32 w-0.5 ${isClicked ? 'bg-iron' : 'bg-dark'}`}
+        initial={{ height: '0' }}
+        animate={{ height: '128px' }}
+        transition={{ type: 'spring', duration: 1.0, delay: 0.8 }}
+        data-testid="icons-line"
+      />
     </div>
   );
 };
