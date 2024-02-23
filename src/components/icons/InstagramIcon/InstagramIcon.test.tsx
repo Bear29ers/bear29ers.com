3c59e6a91b39a8b369c53eaa684f2451e405d790
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-23 16:33:23 +0900

    :white_check_mark: test: complete the InstagramIcon test

--- src/components/icons/InstagramIcon/InstagramIcon.test.tsx
+++ src/components/icons/InstagramIcon/InstagramIcon.test.tsx
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import InstagramIcon from './InstagramIcon';
 
 describe('src/components/icons/InstagramIcon/InstagramIcon.test.tsx', () => {
   it('should render the InstagramIcon component', () => {
-    const { getByRole } = render(<InstagramIcon />);
+    render(<InstagramIcon />);
+    expect(screen.getByRole('img', { name: 'InstagramIcon' })).toBeInTheDocument();
   });
 });
