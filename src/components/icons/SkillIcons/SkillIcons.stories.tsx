36a792b5e82a1d1f27871228275f7033312c5b35
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-24 18:31:38 +0900

    :sparkles: feat: implement first line of skill icons

--- src/components/icons/SkillIcons/SkillIcons.stories.tsx
+++ src/components/icons/SkillIcons/SkillIcons.stories.tsx
@@ -1,15 +1,63 @@
-import SkillIcons from './SkillIcons';
+import {
+  BootstrapIcon,
+  CSSIcon,
+  HTMLIcon,
+  JQueryIcon,
+  JavaScriptIcon,
+  ReactIcon,
+  SassIcon,
+  TailwindCSSIcon,
+  ThreeJsIcon,
+  TypeScriptIcon,
+} from './SkillIcons';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof SkillIcons> = {
-  component: SkillIcons,
+const meta: Meta<typeof HTMLIcon> = {
+  component: HTMLIcon,
 };
 
 export default meta;
 
-type Story = StoryObj<typeof SkillIcons>;
+type Story = StoryObj<typeof HTMLIcon>;
 
 export const Default: Story = {
-  render: () => <SkillIcons />,
+  render: () => {
+    return (
+      <div className="flex flex-col gap-y-4">
+        <div className="flex gap-x-4">
+          <div className="w-24">
+            <HTMLIcon />
+          </div>
+          <div className="w-24">
+            <CSSIcon />
+          </div>
+          <div className="w-24">
+            <SassIcon />
+          </div>
+          <div className="w-24">
+            <BootstrapIcon />
+          </div>
+          <div className="w-24">
+            <TailwindCSSIcon />
+          </div>
+          <div className="w-24">
+            <JavaScriptIcon />
+          </div>
+          <div className="w-24">
+            <TypeScriptIcon />
+          </div>
+          <div className="w-24">
+            <JQueryIcon />
+          </div>
+          <div className="w-24">
+            <ThreeJsIcon />
+          </div>
+          <div className="w-24">
+            <ReactIcon />
+          </div>
+        </div>
+      </div>
+    );
+  },
 };
