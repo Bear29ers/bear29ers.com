c46678582207ea4a86da0bbfdfa2c103cd039d78
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-25 23:39:17 +0900

    :sparkles: feat: implement the MenuIcon component

--- src/components/menus/MenuIcon/MenuIcon.tsx
+++ src/components/menus/MenuIcon/MenuIcon.tsx
@@ -1,10 +1,56 @@
-import type { FC } from 'react';
+import type { Dispatch, FC, SetStateAction } from 'react';
 
-const MenuIcon: FC = () => {
+interface Props {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const MenuIcon: FC<Props> = ({ isOpen, setIsOpen }) => {
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      <h1>MenuIcon</h1>
-    </div>
+    <button className="group relative overflow-hidden" type="button" onClick={handleClick}>
+      <div className="flex h-7 w-8 origin-center flex-col justify-between transition-all duration-300">
+        <div
+          className={`h-1 w-8 origin-left rounded bg-dark transition-all duration-300 ${
+            isOpen ? 'translate-x-10' : ''
+          }`}
+          data-testid="menu-icon-top-line"
+        />
+        <div
+          className={`h-1 w-8 origin-left rounded bg-dark transition-all delay-75 duration-300 ${
+            isOpen ? 'translate-x-10' : ''
+          }`}
+          data-testid="menu-icon-middle-line"
+        />
+        <div
+          className={`h-1 w-8 origin-left rounded bg-dark transition-all delay-150 duration-300 ${
+            isOpen ? 'translate-x-10' : ''
+          }`}
+          data-testid="menu-icon-bottom-line"
+        />
+        <div
+          className={`absolute inset-0 flex w-0 -translate-x-10 items-center justify-between transition-all duration-500 group-focus:w-12 ${
+            isOpen ? 'translate-x-0' : ''
+          }`}
+          data-testid="menu-icon-close">
+          <div
+            className={`absolute h-1 w-7 rotate-0 rounded bg-dark transition-all delay-300 duration-500 ${
+              isOpen ? 'rotate-45' : ''
+            }`}
+            data-testid="menu-icon-close-top"
+          />
+          <div
+            className={`absolute h-1 w-7 -rotate-0 rounded bg-dark transition-all delay-300 duration-500 ${
+              isOpen ? '-rotate-45' : ''
+            }`}
+            data-testid="menu-icon-close-bottom"
+          />
+        </div>
+      </div>
+    </button>
   );
 };
 
