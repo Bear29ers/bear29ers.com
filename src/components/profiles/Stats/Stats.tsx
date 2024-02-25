38cd2d06eeca3516a7d2176a8bb2d31f223b3d2f
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-25 18:15:00 +0900

    :sparkles: feat: implement the Stats component

--- src/components/profiles/Stats/Stats.tsx
+++ src/components/profiles/Stats/Stats.tsx
@@ -3,7 +3,13 @@ import type { FC } from 'react';
 const Stats: FC = () => {
   return (
     <div>
-      <h1>Stats</h1>
+      <h3 className="mb-4 text-xl font-bold">📊 GitHub Stats</h3>
+      <img
+        src="https://github-readme-stats-bear29ers.vercel.app/api?username=Bear29ers&show_icons=true&theme=bear"
+        alt="GitHub Stats"
+        className="w-full max-w-xl"
+        aria-label="GitHubStats"
+      />
     </div>
   );
 };
