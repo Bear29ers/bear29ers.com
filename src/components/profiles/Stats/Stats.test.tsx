b3b6ee60b7f14ca17d16698fa4b9c6ecad78d6c0
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-25 18:33:06 +0900

    :white_check_mark: test: complete the Stats test

--- src/components/profiles/Stats/Stats.test.tsx
+++ src/components/profiles/Stats/Stats.test.tsx
@@ -1,10 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Stats from './Stats';
 
+import type { RenderResult } from '@testing-library/react';
+
 describe('src/components/profiles/Stats/Stats.test.tsx', () => {
-  it('should render the Stats component', () => {
-    const { getByRole } = render(<Stats />);
+  let renderResult: RenderResult;
+
+  beforeEach(() => {
+    renderResult = render(<Stats />);
+  });
+
+  afterEach(() => {
+    renderResult.unmount();
+  });
+
+  it('should render the title', () => {
+    expect(screen.getByRole('heading', { level: 3, name: /GitHub Stats/ })).toBeInTheDocument();
+  });
+
+  it('should render the GitHub Stats image', () => {
+    expect(screen.getByRole('img', { name: 'GitHubStats' })).toBeInTheDocument();
   });
 });
