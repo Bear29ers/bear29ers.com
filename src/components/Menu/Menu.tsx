bbf7ea07e418d6979ffc13cb1710cb00548453dd
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-25 23:39:17 +0900

    :sparkles: feat: implement the MenuIcon component

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,15 @@
-import type { FC } from 'react';
+'use client';
+
+import { useState, type FC } from 'react';
+
+import MenuIcon from '@/components/menus/MenuIcon/MenuIcon';
 
 const Menu: FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className="fixed right-8 top-3 z-50">
+      <MenuIcon isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
   );
 };
