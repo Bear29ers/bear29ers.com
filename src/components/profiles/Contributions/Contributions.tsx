8ba3f062a44edf700f52f7f144dce4b0f2caf38d
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-25 18:47:31 +0900

    :sparkles: feat: implement the Contributions

--- src/components/profiles/Contributions/Contributions.tsx
+++ src/components/profiles/Contributions/Contributions.tsx
@@ -3,7 +3,13 @@ import type { FC } from 'react';
 const Contributions: FC = () => {
   return (
     <div>
-      <h1>Contributions</h1>
+      <h3 className="mb-4 text-xl font-bold">🍀 Contributions</h3>
+      <img
+        src="https://raw.githubusercontent.com/Bear29ers/Bear29ers/master/profile-3d-contrib/profile-green-animate.svg"
+        alt="contributions"
+        className="w-full"
+        data-testid="contributions"
+      />
     </div>
   );
 };
