5a28a7c9187af617c63e319b6f11403ad65f1776
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-26 23:16:16 +0900

    :sparkles: feat: add the Menu to the layout

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,14 +3,18 @@
 import { useState, type FC } from 'react';
 
 import MenuIcon from '@/components/menus/MenuIcon/MenuIcon';
+import PixelBackground from '@/components/menus/PixelBackground/PixelBackground';
 
 const Menu: FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <div className="fixed right-8 top-3 z-50">
-      <MenuIcon isOpen={isOpen} setIsOpen={setIsOpen} />
-    </div>
+    <>
+      <div className="fixed right-8 top-3 z-50">
+        <MenuIcon isOpen={isOpen} setIsOpen={setIsOpen} />
+      </div>
+      <PixelBackground isDisplay={isOpen} />
+    </>
   );
 };
 
