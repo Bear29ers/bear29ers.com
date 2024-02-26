b41b4b834da06a47daba23cd7453ddceb8cefcd1
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-27 00:03:09 +0900

    :sparkles: feat: create a MenuList component by template

---
+++ src/components/menus/MenuList/MenuList.stories.tsx
@@ -0,0 +1,15 @@
+import MenuList from './MenuList';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof MenuList> = {
+  component: MenuList,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MenuList>;
+
+export const Default: Story = {
+  render: () => <MenuList />,
+};
---
+++ src/components/menus/MenuList/MenuList.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+const MenuList: FC = () => {
+  return (
+    <div>
+      <h1>MenuList</h1>
+    </div>
+  );
+};
+
+export default MenuList;
