b5ee42496ca14aa0b4f766353bb0321cc5d76fcd
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-27 21:18:30 +0900

    :lipstick: ui: display Menu component except Home

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,19 +2,26 @@
 
 import { useState, type FC } from 'react';
 
+import { usePathname } from 'next/navigation';
+
 import MenuIcon from '@/components/menus/MenuIcon/MenuIcon';
 import PixelBackground from '@/components/menus/PixelBackground/PixelBackground';
 
 const Menu: FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const pathname = usePathname();
 
   return (
-    <>
-      <div className="fixed right-8 top-3 z-50">
-        <MenuIcon isOpen={isOpen} setIsOpen={setIsOpen} />
-      </div>
-      <PixelBackground isDisplay={isOpen} />
-    </>
+    <div>
+      {pathname !== '/' && (
+        <div>
+          <div className="fixed right-8 top-3 z-50">
+            <MenuIcon isOpen={isOpen} setIsOpen={setIsOpen} />
+          </div>
+          <PixelBackground isDisplay={isOpen} />
+        </div>
+      )}
+    </div>
   );
 };
 
