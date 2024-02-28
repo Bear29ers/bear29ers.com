7654a588f064d210597bff338d103eb21e37446b
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-28 22:03:45 +0900

    :fire: rem: remove PixelBackground

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -5,7 +5,6 @@ import { useState, type FC } from 'react';
 import { usePathname } from 'next/navigation';
 
 import MenuIcon from '@/components/menus/MenuIcon/MenuIcon';
-import PixelBackground from '@/components/menus/PixelBackground/PixelBackground';
 
 const Menu: FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -18,7 +17,6 @@ const Menu: FC = () => {
           <div className="fixed right-8 top-3 z-50">
             <MenuIcon isOpen={isOpen} setIsOpen={setIsOpen} />
           </div>
-          <PixelBackground isDisplay={isOpen} />
         </div>
       )}
     </div>
