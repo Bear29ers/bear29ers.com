31767aac93edd252b6c809760f3288fe1841d312
Yuta Okuma <y.dream.nest@icloud.com>
2024-02-28 23:49:09 +0900

    :lipstick: ui: add button teritory

--- src/components/menus/MenuIcon/MenuIcon.tsx
+++ src/components/menus/MenuIcon/MenuIcon.tsx
@@ -11,43 +11,45 @@ const MenuIcon: FC<Props> = ({ isOpen, setIsOpen }) => {
   };
 
   return (
-    <button className="group relative overflow-hidden" type="button" onClick={handleClick}>
-      <div className="flex h-7 w-8 origin-center flex-col justify-between transition-all duration-300">
-        <div
-          className={`h-1 w-8 origin-left rounded bg-dark transition-all duration-300 ${
-            isOpen ? 'translate-x-10' : ''
-          }`}
-          data-testid="menu-icon-top-line"
-        />
-        <div
-          className={`h-1 w-8 origin-left rounded bg-dark transition-all delay-75 duration-300 ${
-            isOpen ? 'translate-x-10' : ''
-          }`}
-          data-testid="menu-icon-middle-line"
-        />
-        <div
-          className={`h-1 w-8 origin-left rounded bg-dark transition-all delay-150 duration-300 ${
-            isOpen ? 'translate-x-10' : ''
-          }`}
-          data-testid="menu-icon-bottom-line"
-        />
-        <div
-          className={`absolute inset-0 flex w-0 -translate-x-10 items-center justify-between transition-all duration-500 group-focus:w-12 ${
-            isOpen ? 'translate-x-0' : ''
-          }`}
-          data-testid="menu-icon-close">
+    <button className="flex-center relative rounded-full bg-persianPink p-3" type="button" onClick={handleClick}>
+      <div className="group relative overflow-hidden">
+        <div className="flex h-7 w-8 origin-center flex-col justify-between transition-all duration-300">
           <div
-            className={`absolute h-1 w-7 rotate-0 rounded bg-dark transition-all delay-300 duration-500 ${
-              isOpen ? 'rotate-45' : ''
+            className={`h-1 w-8 origin-left rounded bg-dark transition-all duration-300 ${
+              isOpen ? 'translate-x-10' : ''
             }`}
-            data-testid="menu-icon-close-top"
+            data-testid="menu-icon-top-line"
           />
           <div
-            className={`absolute h-1 w-7 -rotate-0 rounded bg-dark transition-all delay-300 duration-500 ${
-              isOpen ? '-rotate-45' : ''
+            className={`h-1 w-8 origin-left rounded bg-dark transition-all delay-75 duration-300 ${
+              isOpen ? 'translate-x-10' : ''
             }`}
-            data-testid="menu-icon-close-bottom"
+            data-testid="menu-icon-middle-line"
           />
+          <div
+            className={`h-1 w-8 origin-left rounded bg-dark transition-all delay-150 duration-300 ${
+              isOpen ? 'translate-x-10' : ''
+            }`}
+            data-testid="menu-icon-bottom-line"
+          />
+          <div
+            className={`absolute inset-0 flex w-0 -translate-x-10 items-center justify-between transition-all duration-500 group-focus:w-12 ${
+              isOpen ? 'translate-x-0' : ''
+            }`}
+            data-testid="menu-icon-close">
+            <div
+              className={`absolute h-1 w-8 rotate-0 rounded bg-dark transition-all delay-300 duration-500 ${
+                isOpen ? 'rotate-45' : ''
+              }`}
+              data-testid="menu-icon-close-top"
+            />
+            <div
+              className={`absolute h-1 w-8 -rotate-0 rounded bg-dark transition-all delay-300 duration-500 ${
+                isOpen ? '-rotate-45' : ''
+              }`}
+              data-testid="menu-icon-close-bottom"
+            />
+          </div>
         </div>
       </div>
     </button>
