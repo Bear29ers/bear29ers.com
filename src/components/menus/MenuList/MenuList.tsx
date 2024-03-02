5f463b9e572a50c2314b7a46963e361b8576ee28
Yuta Okuma <y.dream.nest@icloud.com>
2024-03-02 21:37:33 +0900

    :dizzy: anm: add stagger animation to the social icons in the MenuList

--- src/components/menus/MenuList/MenuList.tsx
+++ src/components/menus/MenuList/MenuList.tsx
@@ -8,7 +8,7 @@ import getIconComponent from '@/common/functions/getIconComponent';
 import type { MenuItem } from '@/common/types/menuItems';
 import type { SocialMedia } from '@/common/types/socialMedia';
 
-const variants = {
+const menuVariants = {
   initial: {
     opacity: 0,
     translateY: 80,
@@ -22,13 +22,29 @@ const variants = {
       duration: 0.65,
       delay: 0.5 + i * 0.1,
       ease: [0.215, 0.61, 0.355, 1],
-      // opacity: { duration: 0.65 },
     },
   }),
-
   exit: {
     opacity: 0,
     transition: { duration: 0.5, type: 'linear', ease: [0.76, 0, 0.24, 1] },
+  },
+};
+
+const socialVariants = {
+  initial: {
+    opacity: 0,
+  },
+  enter: (i: number) => ({
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: 0.95 + i * 0.2,
+      ease: [0.215, 0.61, 0.355, 1],
+    },
+  }),
+  exit: {
+    opacity: 0,
+    transition: { duration: 0.5, type: 'tween', ease: 'easeInOut' },
   },
 };
 
@@ -36,12 +52,12 @@ const MenuList: FC = () => {
   return (
     <div className="flex-center relative h-full flex-col">
       <div className="flex w-fit flex-col items-start gap-y-5">
-        {MENU_ITEMS.map((menuItem: MenuItem, index: number) => (
+        {MENU_ITEMS.map((menuItem: MenuItem, i: number) => (
           <div key={menuItem.id}>
             <motion.div
-              custom={index}
-              variants={variants}
+              variants={menuVariants}
               initial="initial"
+              custom={i}
               animate="enter"
               exit="exit"
               className="text-5xl font-bold">
@@ -51,11 +67,16 @@ const MenuList: FC = () => {
         ))}
       </div>
       <div className="flex items-center gap-x-6 pt-20">
-        {SOCIAL_MEDIA_LIST.map((socialMedia: SocialMedia) => (
+        {SOCIAL_MEDIA_LIST.map((socialMedia: SocialMedia, i: number) => (
           <motion.a
             href={socialMedia.href}
             target="_blank"
             className="size-10"
+            variants={socialVariants}
+            initial="initial"
+            custom={i}
+            animate="enter"
+            exit="exit"
             whileHover={{ scale: 1.2 }}
             whileTap={{ scale: 0.9 }}
             transition={{ type: 'spring', stiffness: 400, damping: 10 }}
