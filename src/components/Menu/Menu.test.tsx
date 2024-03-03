417a1452b2d7a470cc8612670892965d04b58a17
Yuta Okuma <y.dream.nest@icloud.com>
2024-03-03 20:27:36 +0900

    :white_check_mark: test: complete the Menu test

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -1,10 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Menu from './Menu';
 
+import type { RenderResult } from '@testing-library/react';
+
 describe('src/components/Menu/Menu.test.tsx', () => {
-  it('should render the Menu component', () => {
-    const { getByRole } = render(<Menu />);
+  let renderResult: RenderResult;
+
+  beforeEach(() => {
+    renderResult = render(<Menu />);
+  });
+
+  afterEach(() => {
+    renderResult.unmount();
+  });
+
+  it('should render the button element', () => {
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  it('should render the MenuWindow component', () => {
+    expect(screen.getByTestId('menu-window')).toBeInTheDocument();
   });
 });
