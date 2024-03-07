0edf3c94568dc639ebdd859fa8f77a330e8c9ff7
Yuta Okuma <y.dream.nest@icloud.com>
2024-03-07 22:40:11 +0900

    :construction: wip: implementing transition provider

--- src/app/TransitionProvider.tsx
+++ src/app/TransitionProvider.tsx
@@ -1,7 +1,24 @@
-import { AnimatePresence } from 'framer-motion';
+'use client';
+
+import { AnimatePresence, motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
 
 const TransitionProvider = ({ children }: { children: React.ReactNode }) => {
-  return <AnimatePresence mode="wait">{children}</AnimatePresence>;
+  const pathname = usePathname();
+
+  return (
+    <AnimatePresence mode="wait">
+      <div key={pathname} className="h-screen w-screen bg-gradient-to-b from-blue-50 to-red-100">
+        <motion.div
+          className="z-40 h-screen w-screen rounded-b-[100px] bg-black"
+          animate={{ height: '0vh' }}
+          exit={{ height: '100vh' }}
+          transition={{ duration: 0.6, ease: 'easeOut' }}
+        />
+        {children}
+      </div>
+    </AnimatePresence>
+  );
 };
 
 export default TransitionProvider;
