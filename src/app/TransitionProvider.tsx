325e5ee7f474bf31dd6d9e3f21a326767b58c9a1
Yuta Okuma <y.dream.nest@icloud.com>
2024-03-13 22:24:58 +0900

    :construction: wip: add variants

--- src/app/TransitionProvider.tsx
+++ src/app/TransitionProvider.tsx
@@ -3,12 +3,60 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import { usePathname } from 'next/navigation';
 
+// const textVariants = {
+//   initial: { opacity: 1 },
+//   enter: {
+//     opacity: 0,
+//     top: -100,
+//     transition: { duration: 0.75, delay: 0.35, ease: [0.76, 0, 0.24, 1] },
+//     transitionEnd: { top: '47.5%' },
+//   },
+//   exit: {
+//     opacity: 1,
+//     top: '40%',
+//     transition: { duration: 0.5, delay: 0.4, ease: [0.33, 1, 0.68, 1] },
+//   },
+// };
+//
+// const curveVariants = (initialPath, targetPath) => {
+//   return {
+//     initial: {
+//       d: initialPath,
+//     },
+//     enter: {
+//       d: targetPath,
+//       transition: { duration: 0.75, delay: 0.35, ease: [0.76, 0, 0.24, 1] },
+//     },
+//     exit: {
+//       d: initialPath,
+//       transition: { duration: 0.75, ease: [0.76, 0, 0.24, 1] },
+//     },
+//   };
+// };
+//
+// const translateVariants = {
+//   initial: {
+//     top: '-300px',
+//   },
+//   enter: {
+//     top: '-100vh',
+//     transition: { duration: 0.75, delay: 0.35, ease: [0.76, 0, 0.24, 1] },
+//     transitionEnd: {
+//       top: '100vh',
+//     },
+//   },
+//   exit: {
+//     top: '-300px',
+//     transition: { duration: 0.75, ease: [0.76, 0, 0.24, 1] },
+//   },
+// };
+
 const TransitionProvider = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
   return (
     <AnimatePresence mode="wait">
-      <div key={pathname} className="h-screen w-screen bg-gradient-to-b from-blue-50 to-red-100">
+      <div key={pathname} className="h-screen w-screen">
         <motion.div
           className="fixed z-40 h-screen w-screen rounded-b-[100px] bg-black"
           animate={{ height: '0vh' }}
