d0c98a18440b44a9f1e7c868fc61b5501569ef44
Yuta Okuma <y.dream.nest@icloud.com>
2024-04-20 14:31:14 +0900

    :white_check_mark: test: update tests to match home page implementation

--- src/components/icons/ThreadsIcon/ThreadsIcon.test.tsx
+++ src/components/icons/ThreadsIcon/ThreadsIcon.test.tsx
@@ -10,31 +10,7 @@ describe('src/components/icons/ThreadsIcon/ThreadsIcon.test.tsx', () => {
     let renderResult: RenderResult;
 
     beforeEach(() => {
-      renderResult = render(<ThreadsIcon darkMode={false} />);
-    });
-
-    afterEach(() => {
-      renderResult.unmount();
-    });
-
-    it('should render the ThreadsIcon component', () => {
-      expect(screen.getByRole('img', { name: 'ThreadsIcon' })).toBeInTheDocument();
-    });
-
-    it('should have style that icon background fill is #000', () => {
-      expect(screen.getByTestId('threads-background')).toHaveStyle('fill: #000');
-    });
-
-    it('should have style that icon logo fill is #D2D8D9', () => {
-      expect(screen.getByTestId('threads-logo')).toHaveStyle('fill: #D2D8D9');
-    });
-  });
-
-  describe('when isClicked props is true', () => {
-    let renderResult: RenderResult;
-
-    beforeEach(() => {
-      renderResult = render(<ThreadsIcon darkMode />);
+      renderResult = render(<ThreadsIcon lightMode={false} />);
     });
 
     afterEach(() => {
@@ -51,6 +27,30 @@ describe('src/components/icons/ThreadsIcon/ThreadsIcon.test.tsx', () => {
 
     it('should have style that icon logo fill is #000', () => {
       expect(screen.getByTestId('threads-logo')).toHaveStyle('fill: #000');
+    });
+  });
+
+  describe('when isClicked props is true', () => {
+    let renderResult: RenderResult;
+
+    beforeEach(() => {
+      renderResult = render(<ThreadsIcon lightMode />);
+    });
+
+    afterEach(() => {
+      renderResult.unmount();
+    });
+
+    it('should render the ThreadsIcon component', () => {
+      expect(screen.getByRole('img', { name: 'ThreadsIcon' })).toBeInTheDocument();
+    });
+
+    it('should have style that icon background fill is #000', () => {
+      expect(screen.getByTestId('threads-background')).toHaveStyle('fill: #000');
+    });
+
+    it('should have style that icon logo fill is #D2D8D9', () => {
+      expect(screen.getByTestId('threads-logo')).toHaveStyle('fill: #D2D8D9');
     });
   });
 });
