ffcfe9aab10c8a60dfd207e91d2cb2b08d8936e0
Yuta Okuma <y.dream.nest@icloud.com>
2024-04-21 22:37:07 +0900

    :construction: wip: creating the experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
 
 const Experience: NextPage = () => {
-  return <h1>Experience page</h1>;
+  return (
+    <div className="h-screen w-full bg-dark text-white">
+      <div className="mx-auto flex w-full max-w-screen-xl pt-24">
+        <h1>Experience page</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
