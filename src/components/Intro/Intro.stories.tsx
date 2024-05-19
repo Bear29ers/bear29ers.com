54c7e003f7d4b0a67e622d199c3772a6714f49a5
Yuta Okuma <y.dream.nest@icloud.com>
2024-05-19 13:22:57 +0900

    :white_check_mark: test: update the Intro component test and story

--- src/components/Intro/Intro.stories.tsx
+++ src/components/Intro/Intro.stories.tsx
@@ -1,16 +1,24 @@
+import { HOME_PROFILE_TEXT } from '@/common/constants/home';
+
 import Intro from './Intro';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof Intro> = {
   component: Intro,
-  args: {
-    isClicked: false,
-  },
+  tags: ['autodocs'],
 };
 
 export default meta;
 
 type Story = StoryObj<typeof Intro>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    isClicked: false,
+    homeProfileText: HOME_PROFILE_TEXT,
+  },
+  render: ({ isClicked, homeProfileText }) => {
+    return <Intro isClicked={isClicked} homeProfileText={homeProfileText} />;
+  },
+};
