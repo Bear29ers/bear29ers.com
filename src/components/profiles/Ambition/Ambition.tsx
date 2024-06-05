89a543f79bd7489cdca5e38e9c2a44a6f9fa2000
Yuta Okuma <y.dream.nest@icloud.com>
2024-06-05 20:45:17 +0900

    :recycle: refa: fix profiles storybooks

--- src/components/profiles/Ambition/Ambition.tsx
+++ src/components/profiles/Ambition/Ambition.tsx
@@ -1,12 +1,12 @@
 import type { FC } from 'react';
 
 import { AMBITIONS } from '@/common/constants/about';
-import { Ambition } from '@/common/types/about';
+import type { Ambition as AmbitionType } from '@/common/types/about';
 
 const Ambition: FC = () => {
   return (
     <ul className="list-inside list-disc space-y-3">
-      {AMBITIONS.map((ambition: Ambition) => (
+      {AMBITIONS.map((ambition: AmbitionType) => (
         <li key={ambition.id} className="text-xl">
           {ambition.text}
         </li>
