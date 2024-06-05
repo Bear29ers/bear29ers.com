6934dc8f875b15249c84f833d673b96c2066ee8d
Yuta Okuma <y.dream.nest@icloud.com>
2024-06-05 20:55:11 +0900

    :white_check_mark: test: complete the SectionTitle test

--- src/components/profiles/SectionTitle/SectionTitle.test.tsx
+++ src/components/profiles/SectionTitle/SectionTitle.test.tsx
@@ -1,10 +1,27 @@
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 
+import '@testing-library/jest-dom';
 import SectionTitle from './SectionTitle';
 
+import type { RenderResult } from '@testing-library/react';
+
 describe('src/components/profiles/SectionTitle/SectionTitle.test.tsx', () => {
-  it('should render the SectionTitle component', () => {
-    const { getByRole } = render(<SectionTitle />);
+  const title = 'Biography';
+  let renderResult: RenderResult;
+
+  beforeEach(() => {
+    renderResult = render(<SectionTitle title={title} />);
+  });
+
+  afterEach(() => {
+    renderResult.unmount();
+  });
+
+  it('should render the section title with h2 tag', () => {
+    expect(screen.getByRole('heading', { level: 2, name: title })).toBeInTheDocument();
+  });
+
+  it('should render the section title with h4 tag', () => {
+    expect(screen.getByRole('heading', { level: 4, name: title })).toBeInTheDocument();
   });
 });
