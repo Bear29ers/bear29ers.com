90a7a28476b0809323e38676b1546350681cc0db
Yuta Okuma <y.dream.nest@icloud.com>
2024-06-08 12:58:53 +0900

    :adhesive_bandage: ish: fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,27 +2,22 @@ import { NextResponse } from 'next/server';
 
 import type { NextRequest } from 'next/server';
 
-export const config = {
-  matcher: '/:path*',
-};
-
-const middleware = (req: NextRequest): NextResponse => {
+export const middleware = (req: NextRequest): NextResponse => {
   if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
     const basicAuth = req.headers.get('authorization');
     const url = req.nextUrl;
     if (basicAuth) {
-      const authValue = basicAuth.split(' ')[1];
-      const [user, pwd] = atob(String(authValue)).split(':');
+      const authValue = basicAuth.split(' ')[1] ?? '';
+      const [user, pwd] = atob(authValue).split(':');
 
       if (user === process.env.BASIC_USERNAME && pwd === process.env.BASIC_PASSWORD) {
         return NextResponse.next();
       }
     }
+
     url.pathname = '/api/auth';
     return NextResponse.rewrite(url);
   }
 
   return NextResponse.next();
 };
-
-export default middleware;
