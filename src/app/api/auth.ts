056d9fa900bb31b6c17453efc705c6490d2d6d69
Yuta Okuma <y.dream.nest@icloud.com>
2024-06-08 12:50:38 +0900

    :adhesive_bandage: ish: fix api/auth

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -1,9 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 
-const handler = (_: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader('WWW-authenticate', 'Basic realm="Secure Area"');
-  res.statusCode = 401;
-  res.end(`Auth Required.`);
+const GET = (): NextResponse => {
+  return NextResponse.json(
+    { error: 'Basic Auth Required' },
+    {
+      status: 401,
+      headers: { 'WWW-Authenticate': "Basic realm='secure_area'" },
+    }
+  );
 };
 
-export default handler;
+export { GET };
