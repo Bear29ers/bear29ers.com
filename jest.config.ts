035b7f756f4bfe385e3d4aa53da1efcb7397de32
Yuta Okuma <y.dream.nest@icloud.com>
2024-06-16 10:42:40 +0900

    :wrench: conf: update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
-/** @types {import('@jest/types').Config.InitialOptions} */
+import nextJest from 'next/jest';
 
-const nextJest = require('next/jest');
+import type { Config } from 'jest';
 
 const createJestConfig = nextJest({
   // next.config.cjsとテスト環境用の.envファイルが配置されたディレクトリを設定
@@ -9,11 +9,11 @@ const createJestConfig = nextJest({
 
 // Jestのカスタム設定
 /** @type {import('jest').Config} */
-const config = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+const config: Config = {
+  coverageProvider: 'v8',
   testEnvironment: 'jest-environment-jsdom',
-  preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
 // createJestConfigを定義することで、このファイルで定義された設定がNext.jsの設定に反映される
-module.exports = createJestConfig(config);
+export default createJestConfig(config);
