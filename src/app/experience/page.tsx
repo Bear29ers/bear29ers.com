d4bba7ba876598d4ccd95abe776bbef83e5095e2
Yuta Okuma <y.dream.nest@icloud.com>
2024-06-17 21:09:53 +0900

    :iphone: resp: make the experience page responsive until xsm

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -17,11 +17,13 @@ const Experience: NextPage = () => {
       </div>
       <div className="mx-auto flex w-full flex-col gap-y-48 lg:max-w-screen-lg">
         {EXPERIENCE_INFO.map((experience: Experience) => (
-          <div className="flex items-center justify-between md:justify-evenly lg:justify-between" key={experience.id}>
-            <div className="writing-mode-vertical rotate-180 text-3xl font-black text-hitGray md:text-4xl">
+          <div
+            className="flex flex-col items-center justify-between gap-y-8 xsm:flex-row md:justify-evenly lg:justify-between"
+            key={experience.id}>
+            <div className="xsm:writing-mode-vertical text-3xl font-black text-hitGray xsm:rotate-180 md:text-4xl">
               <Period startAt={experience.startAt} endAt={experience.endAt} />
             </div>
-            <div className="flex max-w-md flex-col gap-y-12 md:max-w-xl lg:max-w-3xl">
+            <div className="flex flex-col gap-y-12 xsm:max-w-md md:max-w-xl lg:max-w-3xl">
               {experience.projects.map((project: ProjectType) => (
                 <Project project={project} key={project.id} />
               ))}
