1ccccc6991c4222e671ada88eaf45147b83b83ea
Yuta Okuma <y.dream.nest@icloud.com>
2024-06-17 20:24:50 +0900

    :iphone: resp: make the experience page responsive until md

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -11,17 +11,17 @@ import type { NextPage } from 'next';
 
 const Experience: NextPage = () => {
   return (
-    <div className="flex w-full flex-col items-center bg-dark text-white">
+    <div className="flex w-full flex-col items-center bg-dark px-5 text-white lg:px-0">
       <div className="my-24">
         <AnimatedText text="Experience" />
       </div>
-      <div className="mx-auto flex w-full max-w-screen-lg flex-col gap-y-48">
+      <div className="mx-auto flex w-full flex-col gap-y-48 lg:max-w-screen-lg">
         {EXPERIENCE_INFO.map((experience: Experience) => (
-          <div className="flex items-center justify-between" key={experience.id}>
+          <div className="flex items-center justify-evenly lg:justify-between" key={experience.id}>
             <div className="writing-mode-vertical rotate-180 text-4xl font-black text-hitGray">
               <Period startAt={experience.startAt} endAt={experience.endAt} />
             </div>
-            <div className="flex max-w-3xl flex-col gap-y-12">
+            <div className="flex max-w-xl flex-col gap-y-12 lg:max-w-3xl">
               {experience.projects.map((project: ProjectType) => (
                 <Project project={project} key={project.id} />
               ))}
