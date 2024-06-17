7fea50c8974e0f3c18c038875dc96bca37ec3a69
Yuta Okuma <y.dream.nest@icloud.com>
2024-06-17 20:56:33 +0900

    :iphone: resp: make the experience responsive until xsm

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -11,17 +11,17 @@ import type { NextPage } from 'next';
 
 const Experience: NextPage = () => {
   return (
-    <div className="flex w-full flex-col items-center bg-dark px-5 text-white lg:px-0">
+    <div className="flex w-full flex-col items-center bg-dark px-2.5 text-white xs:px-5 lg:px-0">
       <div className="my-24">
         <AnimatedText text="Experience" />
       </div>
       <div className="mx-auto flex w-full flex-col gap-y-48 lg:max-w-screen-lg">
         {EXPERIENCE_INFO.map((experience: Experience) => (
-          <div className="flex items-center justify-evenly lg:justify-between" key={experience.id}>
-            <div className="writing-mode-vertical rotate-180 text-4xl font-black text-hitGray">
+          <div className="flex items-center justify-between md:justify-evenly lg:justify-between" key={experience.id}>
+            <div className="writing-mode-vertical rotate-180 text-3xl font-black text-hitGray md:text-4xl">
               <Period startAt={experience.startAt} endAt={experience.endAt} />
             </div>
-            <div className="flex max-w-xl flex-col gap-y-12 lg:max-w-3xl">
+            <div className="flex max-w-md flex-col gap-y-12 md:max-w-xl lg:max-w-3xl">
               {experience.projects.map((project: ProjectType) => (
                 <Project project={project} key={project.id} />
               ))}
--- src/components/projects/ProjectHighlight/ProjectHighlight.tsx
+++ src/components/projects/ProjectHighlight/ProjectHighlight.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const ProjectHighlight: FC<Props> = ({ highlight }) => {
   return (
-    <span className="flex w-fit cursor-default items-center gap-x-2 rounded-xl bg-pink-900 px-4 py-2">
+    <span className="flex w-fit cursor-default items-center gap-x-2 rounded-xl bg-pink-900 px-3 py-1.5 text-sm md:px-4 md:py-2 md:text-base">
       <span>&#127775;</span>
       <span className="font-medium">{highlight}</span>
     </span>
