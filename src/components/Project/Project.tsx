cfc8b4a214df4c015acc31bf04fb1c8bcbdfe3a9
Yuta Okuma <y.dream.nest@icloud.com>
2024-06-18 21:13:39 +0900

    :iphone: resp: complete the experience page responsive

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -13,7 +13,7 @@ import ProjectTag from '../projects/ProjectTag/ProjectTag';
 
 const bgVariants = {
   opened: {
-    backgroundColor: '#29313D',
+    backgroundColor: '#2C3641',
     transition: { duration: 0.75, type: 'tween', ease: [0.76, 0, 0.24, 1] },
   },
   closed: {
@@ -83,7 +83,7 @@ const Project: FC<Props> = ({ project }) => {
         </div>
         <button
           type="button"
-          className="transition-500 inline w-fit rounded-xl bg-pink-500 px-4 py-1 text-sm font-medium uppercase hover:bg-pink-400"
+          className="transition-500 inline w-fit rounded-xl bg-pink-500 px-4 py-1 text-xs font-medium uppercase hover:bg-pink-400 xs:text-sm"
           onClick={handleClick}>
           {isShow ? 'Show Less' : 'Show More'}
         </button>
@@ -99,7 +99,7 @@ const Project: FC<Props> = ({ project }) => {
           (isShow &&
           <div className="flex flex-col gap-y-5">
             {/* Description */}
-            <div className="mt-6 flex flex-col gap-y-3">
+            <div className="mt-6 flex flex-col gap-y-3 text-sm xs:text-base">
               {project.description.map((paragraph: string, index: number) => (
                 <p key={index}>{paragraph}</p>
               ))}
