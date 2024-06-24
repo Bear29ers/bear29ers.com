7302e14fd7b8ec79c91f4e4b7fc5afc6494d03eb
Yuta Okuma <y.dream.nest@icloud.com>
2024-06-25 08:38:02 +0900

    :iphone: resp: make the not found page responsive until sm

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,11 +7,11 @@ import type { NextPage } from 'next';
 const NotFoundPage: NextPage = () => {
   return (
     <div className="h-screen w-screen flex-col bg-dark text-white flex-center">
-      <div className="gap-x-36 flex-center">
-        <AnimatedText text="4" classes="text-[150px]" />
-        <AnimatedText text="4" classes="text-[150px]" />
+      <div className="gap-x-28 flex-center sm:gap-x-36">
+        <AnimatedText text="4" classes="text-[120px] sm:text-[150px]" />
+        <AnimatedText text="4" classes="text-[120px] sm:text-[150px]" />
       </div>
-      <div className="relative mr-44 h-28 w-[350px]">
+      <div className="relative mr-32 h-20 w-[350px] scale-75 sm:mr-44 sm:h-28 sm:scale-100">
         <svg className="absolute bottom-0 left-0 animate-hit" viewBox="0 0 337.4 42.6">
           <path
             d="M95.2 12.7c-36.4 1.4-74.9-.2-85-.7C8.9 7.1 6 7.3 6 7.3s-6 1-6 13.8 4.7 14.8 6.5 14.8c2.5 0 3.4-4.2 3.6-5.2 10.1-.5 48.6-2.1 85-.7 15.5.6 42.9 2 72 3.6V9.1c-29.1 1.6-56.5 3-71.9 3.6zM323 .1s-33.5 1.7-43.1 2.5c-5.3.5-47.8 3-92 5.4v26.6c44.2 2.4 86.7 4.9 92 5.4 9.6.8 43.1 2.5 43.1 2.5s14.4 2.5 14.4-21v-.4c0-23.5-14.4-21-14.4-21z"
@@ -28,14 +28,14 @@ const NotFoundPage: NextPage = () => {
         </svg>
       </div>
       <div className="mt-24 flex-col gap-y-8 flex-center">
-        <h2 className="text-5xl font-semibold">Page Not Found</h2>
+        <h2 className="text-3xl font-semibold sm:text-5xl">Page Not Found</h2>
         <div className="flex-col flex-center">
-          <p className="text-xl">I&apos;m sorry, the page you requested could not be found.</p>
-          <p className="text-xl">Please go back to the homepage.</p>
+          <p className="sm:text-xl">I&apos;m sorry, the page you requested could not be found.</p>
+          <p className="sm:text-xl">Please go back to the homepage.</p>
         </div>
         <Link
           href="/"
-          className="w-fit rounded-3xl bg-pink-500 px-8 py-2 text-lg font-medium uppercase text-white transition-colors duration-500 ease-in-out hover:bg-pink-400">
+          className="w-fit rounded-3xl bg-pink-500 px-8 py-2 font-medium uppercase text-white transition-colors duration-500 ease-in-out hover:bg-pink-400 sm:text-lg">
           Go Home
         </Link>
       </div>
