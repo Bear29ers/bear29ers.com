0e293398c82263421c05684eb00ef36e314a7131
Yuta Okuma <y.dream.nest@icloud.com>
2024-06-30 19:50:54 +0900

    :alien: api: get instagram media data from its api

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,13 +1,64 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable no-shadow */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import AnimatedText from '@/components/common/AnimatedText/AnimatedText';
 import Footer from '@/components/layout/Footer/Footer';
 
 import type { NextPage } from 'next';
 
 const Gallery: NextPage = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch('/api/instagram')
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError('Error fetching data');
+        setLoading(false);
+        console.error(err);
+      });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+  if (!data) return <div>No data</div>;
+
   return (
     <div className="flex w-full flex-col items-center px-2.5 text-white xs:px-5 lg:px-0">
       <div className="my-24">
         <AnimatedText text="Gallery" classes="text-[48px] xs:text-[60px] xsm:text-[80px]" />
+      </div>
+      <div>
+        {data.media.data.map((post, index) => (
+          <div key={index}>
+            <img src={post.media_url} alt={post.caption} />
+            <p>{post.caption}</p>
+            <p>Likes: {post.like_count}</p>
+            <p>Posted by: {post.username}</p>
+            <a href={post.permalink} target="_blank" rel="noopener noreferrer">
+              View on Instagram
+            </a>
+          </div>
+        ))}
       </div>
       {/* Footer */}
       <Footer />
