b951aa801495ee7aa240ebdf2ab3fcb6aad2c5c7
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-08 23:02:19 +0900

    :construction: wip: stagger animation of images

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -91,35 +91,45 @@ const Gallery: NextPage = () => {
             />
             <motion.div>
               <motion.img
-                animate={{ x: '-30%', y: -90, rotate: 5 }}
+                initial={{ x: '-50%', y: 220, rotate: 0 }}
+                animate={open ? { x: '-50%', y: 220, rotate: 0 } : { x: '-30%', y: -90, rotate: 5 }}
+                transition={{ delay: 0.65, duration: 1.2, ease: [0.16, 1, 0.3, 1] }}
                 src="/images/polaroid/image-2.jpg"
                 className="absolute bottom-0 left-1/2 w-full max-w-[400px] "
               />
               <motion.img
-                animate={{ x: '-105%', y: 120, rotate: -20 }}
+                initial={{ x: '-50%', y: 220, rotate: 0 }}
+                animate={open ? { x: '-50%', y: 220, rotate: 0 } : { x: '-105%', y: 120, rotate: -20 }}
+                transition={{ delay: 0.5, duration: 1.2, ease: [0.16, 1, 0.3, 1] }}
                 src="/images/polaroid/image-1.jpg"
                 className="absolute bottom-0 left-1/2 w-full max-w-[400px] "
               />
               <motion.img
-                animate={{ x: '-5%', y: 40, rotate: 12 }}
+                initial={{ x: '-50%', y: 220, rotate: 0 }}
+                animate={open ? { x: '-50%', y: 220, rotate: 0 } : { x: '-5%', y: 40, rotate: 12 }}
+                transition={{ delay: 0.8, duration: 1.2, ease: [0.16, 1, 0.3, 1] }}
                 src="/images/polaroid/image-3.jpg"
                 className="absolute bottom-0 left-1/2 w-full max-w-[400px] "
               />
-              <motion.img
-                animate={{ x: '5%', y: 160, rotate: 15 }}
-                src="/images/polaroid/image-6.jpg"
-                className="absolute bottom-0 left-1/2 w-full max-w-[400px] "
-              />
-              <motion.img
-                animate={{ x: '-90%', y: 0, rotate: -17 }}
-                src="/images/polaroid/image-4.jpg"
-                className="absolute bottom-0 left-1/2 w-full max-w-[400px] "
-              />
-              <motion.img
-                animate={{ x: '-70%', y: 20, rotate: -10 }}
-                src="/images/polaroid/image-5.jpg"
-                className="absolute bottom-0 left-1/2 w-full max-w-[400px] "
-              />
+              {/* <motion.img */}
+              {/*   animate={{ x: '5%', y: 160, rotate: 15 }} */}
+              {/*   src="/images/polaroid/image-6.jpg" */}
+              {/*   className="absolute bottom-0 left-1/2 w-full max-w-[400px] " */}
+              {/* /> */}
+              {/* <motion.img */}
+              {/*   initial={{ x: '-50%', y: 220, rotate: 0 }} */}
+              {/*   animate={open ? { x: '-50%', y: 220, rotate: 0 } : { x: '-90%', y: 0, rotate: -17 }} */}
+              {/*   transition={{ duration: 1.2, ease: [0.16, 1, 0.3, 1] }} */}
+              {/*   src="/images/polaroid/image-4.jpg" */}
+              {/*   className="absolute bottom-0 left-1/2 w-full max-w-[400px] " */}
+              {/* /> */}
+              {/* <motion.img */}
+              {/*   initial={{ x: '-50%', y: 500, rotate: 0 }} */}
+              {/*   animate={open ? { x: '-50%', y: 500, rotate: 0 } : { x: '-70%', y: 20, rotate: -10 }} */}
+              {/*   transition={{ duration: 1.2, ease: [0.16, 1, 0.3, 1] }} */}
+              {/*   src="/images/polaroid/image-5.jpg" */}
+              {/*   className="absolute bottom-0 left-1/2 w-full max-w-[400px] " */}
+              {/* /> */}
             </motion.div>
           </div>
         </div>
