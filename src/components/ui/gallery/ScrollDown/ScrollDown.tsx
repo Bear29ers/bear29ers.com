4960407acee86d5763d9949d4e38be9bcd31d446
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-10 23:51:53 +0900

    :dizzy: anm: add main visual exit animation

--- src/components/ui/gallery/ScrollDown/ScrollDown.tsx
+++ src/components/ui/gallery/ScrollDown/ScrollDown.tsx
@@ -14,27 +14,36 @@ const variants = {
       duration: 2.5,
     },
   },
+  exit: {
+    opacity: 0,
+    transition: {
+      type: 'spring',
+      duration: 0.2,
+    },
+  },
 };
 
 interface Props {
+  state: boolean;
   setState: (state: boolean) => void;
 }
 
-const ScrollDown: FC<Props> = ({ setState }) => {
+const ScrollDown: FC<Props> = ({ state, setState }) => {
   return (
     <motion.div
       variants={variants}
       initial="initial"
-      animate="animate"
+      animate={state ? 'exit' : 'animate'}
       className="absolute bottom-0 left-1/2 z-40 h-40 w-full max-w-[337px] -translate-x-1/2 bg-gradient-to-transparent">
       <button
         type="button"
-        className="absolute inset-x-0 bottom-10 mx-auto w-12 animate-down"
+        className="absolute inset-x-0 bottom-10 mx-auto w-12 animate-down fill-dark transition-colors duration-400 hover:fill-pink-600"
         aria-label="scroll-down"
         onClick={() => setState(true)}>
         <svg width="100%" height="100%" viewBox="0 0 310 177">
           <path
-            fill="#000"
+            className="fill-inherit"
+            fill="none"
             d="M159.875,174.481L306.945,27.41c2.93-2.929,2.93-7.678,0-10.606L292.803,2.661c-1.406-1.407-3.314-2.197-5.303-2.197c-1.989,0-3.896,0.79-5.303,2.197L154.572,130.287L26.946,2.661c-1.406-1.407-3.314-2.197-5.303-2.197c-1.989,0-3.897,0.79-5.303,2.197L2.197,16.804C0.733,18.269,0,20.188,0,22.107s0.732,3.839,2.197,5.303l147.071,147.071C152.197,177.411,156.945,177.411,159.875,174.481L159.875,174.481z"
           />
         </svg>
