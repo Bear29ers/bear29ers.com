c12d38304f1928dd45c309348f5576161f283be2
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-12 23:57:12 +0900

    :sparkles: feat: complete the FanningItem
    Closes

--- src/components/ui/gallery/FanningItem/FanningItem.tsx
+++ src/components/ui/gallery/FanningItem/FanningItem.tsx
@@ -1,10 +1,43 @@
 import type { FC } from 'react';
 
-const FanningItem: FC = () => {
+import { motion } from 'framer-motion';
+
+const variants = (x: string, y: number, rotate: number) => ({
+  initial: {
+    x: '-50%',
+    y: 220,
+    rotate: 0,
+  },
+  animate: {
+    x,
+    y,
+    rotate,
+    transition: {
+      duration: 1.2,
+      ease: [0.16, 1, 0.3, 1],
+    },
+  },
+});
+
+interface Props {
+  imageSrc: string;
+  layoutId: string;
+  styles: {
+    zIndex: string;
+    x: string;
+    y: number;
+    rotate: number;
+  };
+}
+
+const FanningItem: FC<Props> = ({ imageSrc, layoutId, styles }) => {
   return (
-    <div>
-      <h1>FanningItem</h1>
-    </div>
+    <motion.img
+      layoutId={layoutId}
+      variants={variants(styles.x, styles.y, styles.rotate)}
+      src={imageSrc}
+      className={`absolute bottom-0 left-1/2 w-full max-w-[400px] ${styles.zIndex}`}
+    />
   );
 };
 
