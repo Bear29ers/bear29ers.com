86deee4ca3ca44a85b962e7cef464de99c4e39d6
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-13 00:24:18 +0900

    :sparkles: feat: complete the FanningImages

--- src/components/ui/gallery/FanningImages/FanningImages.tsx
+++ src/components/ui/gallery/FanningImages/FanningImages.tsx
@@ -2,6 +2,10 @@ import type { Dispatch, FC, SetStateAction } from 'react';
 
 import { motion } from 'framer-motion';
 
+import type { MediaData } from '@/types/media';
+
+import FanningItem from '../FanningItem/FanningItem';
+
 const variants = {
   initial: {},
   animate: {
@@ -12,70 +16,35 @@ const variants = {
   },
 };
 
-const polaroidItemVariants = (x: string, y: number, rotate: number) => ({
-  initial: {
-    x: '-50%',
-    y: 220,
-    rotate: 0,
-  },
-  animate: {
-    x,
-    y,
-    rotate,
-    transition: {
-      duration: 1.2,
-      ease: [0.16, 1, 0.3, 1],
-    },
-  },
-});
+const itemStyleList = [
+  { zIndex: 'z-[2]', x: '-105%', y: 120, rotate: -20 },
+  { zIndex: 'z-[1]', x: '-30%', y: -90, rotate: 5 },
+  { zIndex: 'z-[5]', x: '-90%', y: 0, rotate: -17 },
+  { zIndex: 'z-[3]', x: '-5%', y: 40, rotate: 12 },
+  { zIndex: 'z-[6]', x: '-70%', y: 20, rotate: -10 },
+  { zIndex: 'z-[4]', x: '5%', y: 160, rotate: 15 },
+];
 
 interface Props {
+  mediaList: MediaData[];
   setState: Dispatch<SetStateAction<boolean>>;
 }
 
-const FanningImages: FC<Props> = ({ setState }) => {
+const FanningImages: FC<Props> = ({ mediaList, setState }) => {
   return (
     <motion.div
       variants={variants}
       initial="initial"
       animate="animate"
       onAnimationComplete={() => setState((prevState: boolean): boolean => !prevState)}>
-      <motion.img
-        layoutId="image-1"
-        variants={polaroidItemVariants('-105%', 120, -20)}
-        src="/images/polaroid/image-1.jpg"
-        className="absolute bottom-0 left-1/2 z-[2] w-full max-w-[400px]"
-      />
-      <motion.img
-        layoutId="image-2"
-        variants={polaroidItemVariants('-30%', -90, 5)}
-        src="/images/polaroid/image-2.jpg"
-        className="absolute bottom-0 left-1/2 z-[1] w-full max-w-[400px]"
-      />
-      <motion.img
-        layoutId="image-3"
-        variants={polaroidItemVariants('-90%', 0, -17)}
-        src="/images/polaroid/image-3.jpg"
-        className="absolute bottom-0 left-1/2 z-[5] w-full max-w-[400px]"
-      />
-      <motion.img
-        layoutId="image-4"
-        variants={polaroidItemVariants('-5%', 40, 12)}
-        src="/images/polaroid/image-4.jpg"
-        className="absolute bottom-0 left-1/2 z-[3] w-full max-w-[400px]"
-      />
-      <motion.img
-        layoutId="image-5"
-        variants={polaroidItemVariants('-70%', 20, -10)}
-        src="/images/polaroid/image-5.jpg"
-        className="absolute bottom-0 left-1/2 z-[6] w-full max-w-[400px]"
-      />
-      <motion.img
-        layoutId="image-6"
-        variants={polaroidItemVariants('5%', 160, 15)}
-        src="/images/polaroid/image-6.jpg"
-        className="absolute bottom-0 left-1/2 z-[4] w-full max-w-[400px]"
-      />
+      {mediaList.map((media: MediaData, index: number) => (
+        <FanningItem
+          imageSrc={media.mediaUrl}
+          layoutId={media.id}
+          styles={itemStyleList[index]!}
+          key={media.timestamp}
+        />
+      ))}
     </motion.div>
   );
 };
