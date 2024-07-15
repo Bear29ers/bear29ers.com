9268272eb28f879c111c6352384dcd61a2b3bbf8
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-15 16:13:39 +0900

    :white_check_mark: test: complete GalleryIntro stroybook

---
+++ src/components/ui/gallery/GalleryIntro/GalleryIntro.stories.tsx
@@ -0,0 +1,253 @@
+import GalleryIntro from './GalleryIntro';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof GalleryIntro> = {
+  component: GalleryIntro,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof GalleryIntro>;
+
+export const Default: Story = {
+  args: {
+    mainVisualImageSrc:
+      'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/441230592_383105057910639_7763458365855703155_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=18de74&_nc_ohc=Aadzmd8MMpwQ7kNvgGpcTwG&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYC8BLo1S5Nz34Q5RhqjbLcjDynLX08hQFjO_I6bhZ5NWw&oe=669A99FF',
+    layoutId: '17963211944740661',
+    mediaList: [
+      {
+        caption:
+          '.\n…\nメリーゴーランド。\n静寂の夜、光の帳が織りなす回転木馬の幻想。\n…\nMerry-go-round.\nOn a silent night, a curtain of light creates the illusion of a carousel.\n——\n📷 Ricoh GRⅢ HDF\n🎨 Lightroom Mobile\n——\n\n#photograghy #landscape \n#streetphotography #streetgrammer #streetsnap\n#ricoh #ricohgr3 #ricoh_gr_photography \n#gr3 #griii #gr3hdf #griiihdf\n#grsnaps #grist #shootgr #gr_meet_japan\n#lightroommobile #lightroom\n#instagramjapan #reco_jp #igersjp',
+        children: {
+          data: [
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/451166443_3808549426099080_1051796540578757172_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=22N_GcDEHNUQ7kNvgHy1CEW&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBawbjA6Dj69kvaPcSE-yGzXh5yTx_D9L-Nfug_KovZkA&oe=669A9924',
+              id: '18274358392235025',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450929385_8086156728102182_1321659624761938017_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=18de74&_nc_ohc=3Lffb96TmoUQ7kNvgFhYWF3&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYDXow5373Bf8YJAzAlO4bj5nOfr4S38pGkmm9yDEIDKWQ&oe=669A83DF',
+              id: '18064002031582691',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450906740_1028024795605948_7566414893122301672_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=18de74&_nc_ohc=og30EUFXPGQQ7kNvgEdnxdQ&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYCPkb2IG1nqSK9J-reXfFXd-N6IZtZZQ3DA_pnvD1u1ew&oe=669A84F0',
+              id: '18070253050532704',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450936891_773503261522842_3044257970544693698_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=18de74&_nc_ohc=77iw_wqRHTYQ7kNvgGwq0i1&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBVsr41fDnn38YCsWI-L5hoiOBDaDWueefKhVDQ0UIe8w&oe=669A8208',
+              id: '17860275786194145',
+            },
+          ],
+        },
+        mediaUrl:
+          'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/451166443_3808549426099080_1051796540578757172_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=22N_GcDEHNUQ7kNvgHy1CEW&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBawbjA6Dj69kvaPcSE-yGzXh5yTx_D9L-Nfug_KovZkA&oe=669A9924',
+        permalink: 'https://www.instagram.com/p/C9bfg4vyXNx/',
+        likeCount: 0,
+        timestamp: '2024-07-15T04:15:00+0000',
+        username: 'bear29ers',
+        id: '18033720356079349',
+      },
+      {
+        caption:
+          '.\n…\nバラのライトアップ。\n2000品種100万本のバラがライトアップによって幻想的になっていた。\n…\nIlluminated roses.\nThe 1 million roses of 2,000 varieties were lit up, creating a magical atmosphere.\n——\n📷 Ricoh GRⅢ HDF\n🎨 Lightroom Mobile\n——\n\n#photograghy #landscape \n#streetphotography #streetgrammer #streetsnap\n#ricoh #ricohgr3 #ricoh_gr_photography \n#gr3 #griii #gr3hdf #griiihdf\n#grsnaps #grist #shootgr #gr_meet_japan\n#lightroommobile #lightroom\n#instagramjapan #reco_jp #igersjp',
+        children: {
+          data: [
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450845581_2744944942337393_4641788592330974644_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=18de74&_nc_ohc=i7DLyun3MtwQ7kNvgHGZeBl&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYCP7wCwVcgmz7jPaF0ChU0JO5clLV9fqEEyPRugJ-CO6A&oe=669AA615',
+              id: '18069568834552984',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450840423_789938013334821_7164000312034726755_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=18de74&_nc_ohc=JSVS6b-K3ucQ7kNvgF_4LiJ&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYAfeRh8uwwkXq9rGiWJpBKgG-OHjpcPGu8ifhgWcjzprQ&oe=669A8876',
+              id: '18012085892527361',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450809089_2158906321141393_3394214892354933635_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=18de74&_nc_ohc=7K8h0vGeKcsQ7kNvgHbfnz7&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBfwelQUGRI7lBPVG2Ok2cYr_cHGM7YicM1UibnVXs17A&oe=669A91D2',
+              id: '18129119692358733',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450811021_1203296790798425_4226608009432175062_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=18de74&_nc_ohc=92A0PJsWkZQQ7kNvgEEdXHO&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYCHXWYUNvGO7CYLh6DZT_1heigZI2P2bGtzkoqVL0KZLg&oe=669AB04E',
+              id: '18307023823092352',
+            },
+          ],
+        },
+        mediaUrl:
+          'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450845581_2744944942337393_4641788592330974644_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=18de74&_nc_ohc=i7DLyun3MtwQ7kNvgHGZeBl&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYCP7wCwVcgmz7jPaF0ChU0JO5clLV9fqEEyPRugJ-CO6A&oe=669AA615',
+        permalink: 'https://www.instagram.com/p/C9WsRqbyk4Y/',
+        likeCount: 3,
+        timestamp: '2024-07-13T07:30:00+0000',
+        username: 'bear29ers',
+        id: '18019811789347112',
+      },
+      {
+        caption:
+          '.\n…\nフラワーファンタジア。\n光のファンタジアのエリアの中で一番印象的で綺麗だった。\n…\nFlower Fantasia.\nThis was the most impressive and beautiful area of ​​the Light Fantasia.\n——\n📷 Ricoh GRⅢ HDF\n🎨 Lightroom Mobile\n——\n\n#photograghy #landscape \n#streetphotography #streetgrammer #streetsnap\n#ricoh #ricohgr3 #ricoh_gr_photography \n#gr3 #griii #gr3hdf #griiihdf\n#grsnaps #grist #shootgr #gr_meet_japan\n#lightroommobile #lightroom\n#instagramjapan #reco_jp #igersjp is',
+        children: {
+          data: [
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450466888_464268302888038_7037169780474550501_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=jCYMX_sgPskQ7kNvgHPjl1B&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBZNuBsHN4rqQL3KmN0TlCHjxVW9nIKQLIQkGKFCcjuVQ&oe=669AB78D',
+              id: '18040312204934599',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450339936_356014034194371_8778254040339541392_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=18de74&_nc_ohc=MCJOHiXYlqQQ7kNvgGIcoH4&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYDIaL-iM0CymOsDJs38ToH7Wo3Am7CsrlATaSoZzAOuRQ&oe=669A9C3E',
+              id: '17936961152763680',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450574928_796731625862884_6669774851577929325_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=18de74&_nc_ohc=n5Nlug-rlqQQ7kNvgG6kDeV&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYDnbuN3zS-Y8XhU9VGx_26rarTwu_XhwCbn5thdugaLQw&oe=669AA2A6',
+              id: '18012548945243082',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450591497_453088540974987_8882674946050541948_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=18de74&_nc_ohc=EECHlckJ25wQ7kNvgE0pzqZ&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYCBeAgdpU9Wte8r_gsGJ2VEDDRaaaU6Iaod9Hxxq4KSNQ&oe=669A92D9',
+              id: '18099581791424712',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450322703_465799572734990_5616161456270977174_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=18de74&_nc_ohc=j5YriNbhImwQ7kNvgH3d0If&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYALyRRpYWct4n39XDsQgahdF9zfuytEryYSDkg4bMghZw&oe=669A9A1D',
+              id: '18036661051981703',
+            },
+          ],
+        },
+        mediaUrl:
+          'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/450466888_464268302888038_7037169780474550501_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=jCYMX_sgPskQ7kNvgHPjl1B&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBZNuBsHN4rqQL3KmN0TlCHjxVW9nIKQLIQkGKFCcjuVQ&oe=669AB78D',
+        permalink: 'https://www.instagram.com/p/C9Mn-Vkh8aS/',
+        likeCount: 4,
+        timestamp: '2024-07-09T09:40:00+0000',
+        username: 'bear29ers',
+        id: '18011856767527511',
+      },
+      {
+        caption:
+          '.\n…\n海のファンタジア。\nとてもインタラクティブで、海の美しさに触れるような体験だった。\n…\nSea Fantasia.\nIt was a very interactive experience that really brought us into contact with the beauty of the ocean.\n——\n📷 Ricoh GRⅢ HDF\n🎨 Lightroom Mobile\n——\n\n#photograghy #landscape \n#streetphotography #streetgrammer #streetsnap\n#ricoh #ricohgr3 #ricoh_gr_photography \n#gr3 #griii #gr3hdf #griiihdf\n#grsnaps #grist #shootgr #gr_meet_japan\n#lightroommobile #lightroom\n#instagramjapan #reco_jp #igersjp',
+        children: {
+          data: [
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/449861704_3746099808998397_1011346847735010601_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=18de74&_nc_ohc=uNGozMeVN5AQ7kNvgEEzyba&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBxB8j0yx4bm1ztK9UEFm6fBeAob2S3pxaZPGIjGd0WYQ&oe=669AB273',
+              id: '18035048750304006',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/449870741_1692470878225660_5384580749593806124_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=18de74&_nc_ohc=5ZkfnctWF68Q7kNvgGUsG5C&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYCm0nYkK_OYcQX7IfO2EL5KlqpsyYePMVbJiHy2IpOElA&oe=669A8D71',
+              id: '18040648963897947',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/449863935_477345411712692_7439023042783998945_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=18de74&_nc_ohc=_1mRlRGWskAQ7kNvgF98GOT&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYAj5k6pQort3FMKnTkwHO6HEu1dbCZ9ggx0IiN8ifPFIA&oe=669AB5F5',
+              id: '17958262172667604',
+            },
+          ],
+        },
+        mediaUrl:
+          'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/449861704_3746099808998397_1011346847735010601_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=18de74&_nc_ohc=uNGozMeVN5AQ7kNvgEEzyba&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBxB8j0yx4bm1ztK9UEFm6fBeAob2S3pxaZPGIjGd0WYQ&oe=669AB273',
+        permalink: 'https://www.instagram.com/p/C9G0AX1ypZH/',
+        likeCount: 4,
+        timestamp: '2024-07-07T03:30:00+0000',
+        username: 'bear29ers',
+        id: '17924112776916166',
+      },
+      {
+        caption:
+          '.\n…\n夜のすがた。\n街が全体的にライトアップされ、どこを切り取っても画になる。\n…\nNight view.\nThe whole city is lit up, making any shot a beautiful sight.\n——\n📷 Ricoh GRⅢ HDF\n🎨 Lightroom Mobile\n——\n\n#photograghy #landscape \n#streetphotography #streetgrammer #streetsnap\n#ricoh #ricohgr3 #ricoh_gr_photography \n#gr3 #griii #gr3hdf #griiihdf\n#grsnaps #grist #shootgr #gr_meet_japan\n#lightroommobile #lightroom\n#instagramjapan #reco_jp #igersjp',
+        children: {
+          data: [
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/449137722_871539818116303_5297367111864990242_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=18de74&_nc_ohc=Sh6ZCta3oWoQ7kNvgGE2POK&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYDJgGEBCQt5iwxisYQM0T8VParm8Oq-TsjksZv7Oj2NiQ&oe=669A805B',
+              id: '17997062600466484',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/449102949_1843684612780864_7251221434741061094_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=18de74&_nc_ohc=_PPjMIFjMdcQ7kNvgEHMhGV&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYAWA5mYe6CiOP8Td-w0FA2EqKQbproQipw2nBOz8hOW6g&oe=669A8BF4',
+              id: '18050837683670719',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/449065369_1124462695524926_962542830013154560_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=18de74&_nc_ohc=1DvQzE7sCxUQ7kNvgE9VwD7&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYAC2iLht6nGSe49yl617j48PClsP579xQpWt-yz_00n8g&oe=669A85C2',
+              id: '17922412328916439',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/449045437_789352296716114_3300102491923052793_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=18de74&_nc_ohc=te9KIJyo0RoQ7kNvgGyW7Dv&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYATTANjeNRukDA6iVHRILRn7NYwpRr_N6_8HLGdjGAoVQ&oe=669AA827',
+              id: '18022562630470349',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/448959149_1150922659468440_3134666621652361741_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=18de74&_nc_ohc=BlITASyI850Q7kNvgGzizMJ&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYDuwIOv_vMeuCzWWZGd36wjyVl3W7MPaMnqSoc5gldZGQ&oe=669AA3B9',
+              id: '18052922938676630',
+            },
+          ],
+        },
+        mediaUrl:
+          'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/449137722_871539818116303_5297367111864990242_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=18de74&_nc_ohc=Sh6ZCta3oWoQ7kNvgGE2POK&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYDJgGEBCQt5iwxisYQM0T8VParm8Oq-TsjksZv7Oj2NiQ&oe=669A805B',
+        permalink: 'https://www.instagram.com/p/C8r1JRzhqCI/',
+        likeCount: 1,
+        timestamp: '2024-06-26T16:00:00+0000',
+        username: 'bear29ers',
+        id: '18105552064398637',
+      },
+      {
+        caption:
+          ".\n…\nオランダ風車。\n生まれ故郷の関係で見慣れている。\n…\nDutch windmills.\nI'm used to seeing them because that's where I was born.\n——\n📷 Ricoh GRⅢ HDF\n🎨 Lightroom Mobile\n——\n\n#photograghy #landscape \n#streetphotography #streetgrammer #streetsnap\n#ricoh #ricohgr3 #ricoh_gr_photography \n#gr3 #griii #gr3hdf #griiihdf\n#grsnaps #grist #shootgr #gr_meet_japan\n#lightroommobile #lightroom\n#instagramjapan #reco_jp #igersjp",
+        children: {
+          data: [
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/448869856_1656785671751251_5335989703262142847_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=mEJiLPk1kjMQ7kNvgFyxkVK&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYAYIv3sg50k-1e3_jPqOf-T9XsKKTf6dqAbBakdJtx56w&oe=669AAA47',
+              id: '17893979523020142',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/448670938_1175758053574017_4549583692451227676_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=18de74&_nc_ohc=krbwi3leFaEQ7kNvgFiwPoZ&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYB-o0fmr67098z4UuNiz1e10BsIoMnuNQfsf0jpCgAQYQ&oe=669AA64A',
+              id: '18053957395723794',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/448819697_1118299029238023_8233767868103366832_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=18de74&_nc_ohc=nBU92WNRvtQQ7kNvgFsWnG-&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYCH0iNfP6kCxH20PrSpIDswXzcL6G9tdr36mhzHOxtO_g&oe=669A9BC7',
+              id: '17928435893899214',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/448641383_1421843795194652_7834099846141379928_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=18de74&_nc_ohc=3Qq6jNISwm0Q7kNvgFmd1N9&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBrX0I7qtAQf3oviVNfqO5R0Oa5TxXI5H77E2oJ-nMHrg&oe=669A96F1',
+              id: '17849482578202360',
+            },
+            {
+              mediaUrl:
+                'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/448640375_1469656357277009_7527534705433783770_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=18de74&_nc_ohc=nnZOCmTy_AIQ7kNvgF6S6ke&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBUZduUnK0w_kN7lnc0XiIrlJirTMyC0nK3YYLNC6HO_Q&oe=669A9D97',
+              id: '18439046926014329',
+            },
+          ],
+        },
+        mediaUrl:
+          'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/448869856_1656785671751251_5335989703262142847_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=mEJiLPk1kjMQ7kNvgFyxkVK&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYAYIv3sg50k-1e3_jPqOf-T9XsKKTf6dqAbBakdJtx56w&oe=669AAA47',
+        permalink: 'https://www.instagram.com/p/C8eQhAABUR4/',
+        likeCount: 5,
+        timestamp: '2024-06-21T09:30:00+0000',
+        username: 'bear29ers',
+        id: '17870788908109221',
+      },
+    ],
+    setState: () => {},
+  },
+  render: ({ mainVisualImageSrc, layoutId, mediaList, setState }) => {
+    return (
+      <GalleryIntro
+        mainVisualImageSrc={mainVisualImageSrc}
+        layoutId={layoutId}
+        mediaList={mediaList}
+        setState={setState}
+      />
+    );
+  },
+};
