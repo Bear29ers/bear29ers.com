f62cebf0cf898ef722a2c658e0255bf0c5988ced
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-15 22:53:04 +0900

    :white_check_mark: test: complete the AnimatedGallery storybook

--- src/components/ui/gallery/AnimatedGallery/AnimatedGallery.stories.tsx
+++ src/components/ui/gallery/AnimatedGallery/AnimatedGallery.stories.tsx
@@ -4,6 +4,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof AnimatedGallery> = {
   component: AnimatedGallery,
+  tags: ['autodocs'],
 };
 
 export default meta;
@@ -11,5 +12,23 @@ export default meta;
 type Story = StoryObj<typeof AnimatedGallery>;
 
 export const Default: Story = {
-  render: () => <AnimatedGallery />,
+  args: {
+    layoutId: '18033720356079349',
+    imageSrc:
+      'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/451166443_3808549426099080_1051796540578757172_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=22N_GcDEHNUQ7kNvgHy1CEW&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBawbjA6Dj69kvaPcSE-yGzXh5yTx_D9L-Nfug_KovZkA&oe=669A9924',
+    zIndex: 'z-[1]',
+    maxWidth: 'max-w-72',
+    setState: () => {},
+  },
+  render: ({ layoutId, imageSrc, zIndex, maxWidth, setState }) => {
+    return (
+      <AnimatedGallery
+        layoutId={layoutId}
+        imageSrc={imageSrc}
+        zIndex={zIndex}
+        maxWidth={maxWidth}
+        setState={setState}
+      />
+    );
+  },
 };
