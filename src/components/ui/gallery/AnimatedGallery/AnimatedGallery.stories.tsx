5cec819ad93ff961f6697d35c6254a3cd905a9af
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-20 11:39:17 +0900

    :white_check_mark: test: update storybook images paths

--- src/components/ui/gallery/AnimatedGallery/AnimatedGallery.stories.tsx
+++ src/components/ui/gallery/AnimatedGallery/AnimatedGallery.stories.tsx
@@ -14,8 +14,7 @@ type Story = StoryObj<typeof AnimatedGallery>;
 export const Default: Story = {
   args: {
     layoutId: '18033720356079349',
-    imageSrc:
-      'https://scontent-nrt1-2.cdninstagram.com/v/t51.29350-15/451166443_3808549426099080_1051796540578757172_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=22N_GcDEHNUQ7kNvgHy1CEW&_nc_ht=scontent-nrt1-2.cdninstagram.com&edm=AL-3X8kEAAAA&oh=00_AYBawbjA6Dj69kvaPcSE-yGzXh5yTx_D9L-Nfug_KovZkA&oe=669A9924',
+    imageSrc: '/images/example/image-1.jpg',
     zIndex: 'z-[1]',
     maxWidth: 'max-w-72',
     setState: () => {},
