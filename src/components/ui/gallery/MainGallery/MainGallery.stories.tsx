2a690b0d3a370e96bf20e4c2aad3241e64c333fa
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-20 12:57:14 +0900

    :white_check_mark: test: complete the MainGallery storybook

--- src/components/ui/gallery/MainGallery/MainGallery.stories.tsx
+++ src/components/ui/gallery/MainGallery/MainGallery.stories.tsx
@@ -4,6 +4,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof MainGallery> = {
   component: MainGallery,
+  tags: ['autodocs'],
 };
 
 export default meta;
@@ -11,5 +12,8 @@ export default meta;
 type Story = StoryObj<typeof MainGallery>;
 
 export const Default: Story = {
-  render: () => <MainGallery />,
+  args: { imageSrc: '/images/example/image-4.jpg', id: '17924112776916166', maxWidth: 'max-w-72' },
+  render: ({ imageSrc, id, maxWidth }) => {
+    return <MainGallery imageSrc={imageSrc} id={id} maxWidth={maxWidth} />;
+  },
 };
