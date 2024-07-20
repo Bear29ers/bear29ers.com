79e254581fa2aa63f8d10133e2f4a2ae48e975d1
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-20 12:15:35 +0900

    :white_check_mark: test: complete the FanningItem storybook

--- src/components/ui/gallery/FanningItem/FanningItem.stories.tsx
+++ src/components/ui/gallery/FanningItem/FanningItem.stories.tsx
@@ -4,6 +4,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof FanningItem> = {
   component: FanningItem,
+  tags: ['autodocs'],
 };
 
 export default meta;
@@ -11,5 +12,28 @@ export default meta;
 type Story = StoryObj<typeof FanningItem>;
 
 export const Default: Story = {
-  render: () => <FanningItem />,
+  args: {
+    imageSrc: '/images/example/image-3.jpg',
+    layoutId: '17924112776916166',
+    styles: {
+      x: '-60%',
+      y: 0,
+      rotate: -5,
+    },
+    zIndex: 'z-[3]',
+    maxWidth: 'max-w-72',
+    initialY: 220,
+  },
+  render: ({ imageSrc, layoutId, styles, zIndex, maxWidth, initialY }) => {
+    return (
+      <FanningItem
+        imageSrc={imageSrc}
+        layoutId={layoutId}
+        styles={styles}
+        zIndex={zIndex}
+        maxWidth={maxWidth}
+        initialY={initialY}
+      />
+    );
+  },
 };
