280494b74ae34b918dcbd6cb3dc05a102b8c9bd2
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-20 22:26:52 +0900

    :white_check_mark: test: complete the ScrollDown test

--- src/components/ui/gallery/ScrollDown/ScrollDown.test.tsx
+++ src/components/ui/gallery/ScrollDown/ScrollDown.test.tsx
@@ -1,10 +1,33 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import ScrollDown from './ScrollDown';
 
 describe('src/components/ui/gallery/ScrollDown/ScrollDown.test.tsx', () => {
-  it('should render the ScrollDown component', () => {
-    const { getByRole } = render(<ScrollDown />);
+  const mockSetState = jest.fn();
+
+  describe('when state is false', () => {
+    it('should render the scroll down button', () => {
+      render(<ScrollDown state={false} setState={mockSetState} />);
+      expect(screen.getByRole('button')).toBeInTheDocument();
+    });
+
+    it('should calls setState when the button is clicked', async () => {
+      render(<ScrollDown state={false} setState={mockSetState} />);
+      const button = screen.getByRole('button');
+      fireEvent.click(button);
+      await waitFor(() => {
+        expect(mockSetState).toHaveBeenCalledWith(true);
+      });
+    });
+  });
+
+  describe('when state is true', () => {
+    it('should have opacity 0 with the scroll down button', async () => {
+      render(<ScrollDown state setState={jest.fn()} />);
+      await waitFor(() => {
+        expect(screen.getByRole('button')).toHaveStyle('opacity: 0');
+      });
+    });
   });
 });
