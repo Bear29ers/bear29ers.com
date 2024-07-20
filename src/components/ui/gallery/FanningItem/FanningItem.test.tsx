c5dc438af81dd63dd9d51be0d498de42dd002fe4
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-20 12:26:07 +0900

    :white_check_mark: test: complete the FanningItem test

--- src/components/ui/gallery/FanningItem/FanningItem.test.tsx
+++ src/components/ui/gallery/FanningItem/FanningItem.test.tsx
@@ -1,10 +1,34 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import FanningItem from './FanningItem';
 
+import type { RenderResult } from '@testing-library/react';
+
 describe('src/components/ui/gallery/FanningItem/FanningItem.test.tsx', () => {
+  let renderResult: RenderResult;
+  const args = {
+    imageSrc: '/images/example/image-3.jpg',
+    layoutId: '17924112776916166',
+    styles: {
+      x: '-60%',
+      y: 0,
+      rotate: -5,
+    },
+    zIndex: 'z-[3]',
+    maxWidth: 'max-w-72',
+    initialY: 220,
+  };
+
+  beforeEach(() => {
+    renderResult = render(<FanningItem {...args} />);
+  });
+
+  afterEach(() => {
+    renderResult.unmount();
+  });
+
   it('should render the FanningItem component', () => {
-    const { getByRole } = render(<FanningItem />);
+    expect(screen.getByRole('img')).toBeInTheDocument();
   });
 });
