4b6a510239269bc09d9a429f11b80b9cb3c2b349
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-20 22:08:03 +0900

    :white_check_mark: test: complete the ScrollDown storybook

--- src/components/ui/gallery/ScrollDown/ScrollDown.stories.tsx
+++ src/components/ui/gallery/ScrollDown/ScrollDown.stories.tsx
@@ -4,6 +4,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof ScrollDown> = {
   component: ScrollDown,
+  tags: ['autodocs'],
 };
 
 export default meta;
@@ -11,5 +12,15 @@ export default meta;
 type Story = StoryObj<typeof ScrollDown>;
 
 export const Default: Story = {
-  render: () => <ScrollDown />,
+  args: {
+    state: false,
+    setState: () => {},
+  },
+  render: ({ state, setState }) => {
+    return (
+      <div className="h-screen w-full bg-dark">
+        <ScrollDown state={state} setState={setState} />
+      </div>
+    );
+  },
 };
