fabb1ac9c8d83b2efd1be49550aed8334471fa7a
Yuta Okuma <y.dream.nest@icloud.com>
2024-07-21 09:56:48 +0900

    :white_check_mark: test: complete the Subhead test

--- src/components/ui/gallery/Subhead/Subhead.test.tsx
+++ src/components/ui/gallery/Subhead/Subhead.test.tsx
@@ -1,10 +1,13 @@
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Subhead from './Subhead';
 
 describe('src/components/ui/gallery/Subhead/Subhead.test.tsx', () => {
-  it('should render the Subhead component', () => {
-    const { getByRole } = render(<Subhead />);
+  it('should render the Subhead text', async () => {
+    render(<Subhead text="subhead text" />);
+    await waitFor(() => {
+      expect(screen.getByRole('heading', { level: 2, name: 'subhead text' })).toBeInTheDocument();
+    });
   });
 });
