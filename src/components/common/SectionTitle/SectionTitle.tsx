91eb5c605a66abf03bd8642e994358c0420125b7
Yuta Okuma <y.dream.nest@icloud.com>
2024-08-24 11:44:29 +0900

    :lipstick: ui: change the SectionTitle color

--- src/components/common/SectionTitle/SectionTitle.tsx
+++ src/components/common/SectionTitle/SectionTitle.tsx
@@ -9,7 +9,7 @@ interface Props {
 const SectionTitle: FC<Props> = ({ title }) => {
   return (
     <div className={`${damion.variable} relative w-fit`}>
-      <h2 className="font-damion text-[20vw] text-darkerGray lg:text-[200px]">{title}</h2>
+      <h2 className="font-damion text-[20vw] text-darkGray lg:text-[200px]">{title}</h2>
       <h4 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-damion text-[10vw] text-hitGray lg:text-8xl">
         {title}
       </h4>
