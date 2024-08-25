f36d0096b5ceb7838edd5666743ea75ee200828e
Yuta Okuma <y.dream.nest@icloud.com>
2024-08-25 10:48:25 +0900

    :dizzy: anm: add framer motion

--- src/components/ui/gallery/ScrollToTop/ScrollToTop.tsx
+++ src/components/ui/gallery/ScrollToTop/ScrollToTop.tsx
@@ -1,5 +1,7 @@
 import type { FC } from 'react';
 
+import { motion } from 'framer-motion';
+
 const ScrollToTop: FC = () => {
   const scrollToTop = () => {
     document.documentElement.scrollTo({
@@ -9,7 +11,15 @@ const ScrollToTop: FC = () => {
   };
 
   return (
-    <button type="button" className="size-14 rounded-full bg-[#007acc] flex-center" onClick={scrollToTop}>
+    <motion.button
+      type="button"
+      className="size-14 rounded-full bg-customRed-500 flex-center"
+      onClick={scrollToTop}
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1, transition: { duration: 0.6 } }}
+      exit={{ y: 100, opacity: 0, transition: { duration: 0.6 } }}
+      whileHover={{ scale: 1.1, transition: { type: 'spring', stiffness: 400, damping: 10 } }}
+      whileTap={{ scale: 0.9, transition: { type: 'spring', stiffness: 400, damping: 10 } }}>
       <svg
         className="size-8"
         aria-hidden="true"
@@ -24,7 +34,7 @@ const ScrollToTop: FC = () => {
           d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"
         />
       </svg>
-    </button>
+    </motion.button>
   );
 };
 
