7cce6da31860526aaade1ceea4e30d98922f8f24
Yuta Okuma <y.dream.nest@icloud.com>
2024-08-25 11:21:28 +0900

    :sparkles: feat: add display conditional

--- src/components/ui/gallery/ScrollToTop/ScrollToTop.tsx
+++ src/components/ui/gallery/ScrollToTop/ScrollToTop.tsx
@@ -1,8 +1,20 @@
-import type { FC } from 'react';
+import { useState, type FC, useEffect } from 'react';
 
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const ScrollToTop: FC = () => {
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+  useEffect(() => {
+    const updatePosition = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener('scroll', updatePosition);
+
+    return () => window.removeEventListener('scroll', updatePosition);
+  }, []);
+
   const scrollToTop = () => {
     document.documentElement.scrollTo({
       top: 0,
@@ -11,30 +23,34 @@ const ScrollToTop: FC = () => {
   };
 
   return (
-    <motion.button
-      type="button"
-      className="size-14 rounded-full bg-customRed-500 flex-center"
-      onClick={scrollToTop}
-      initial={{ y: 100, opacity: 0 }}
-      animate={{ y: 0, opacity: 1, transition: { duration: 0.6 } }}
-      exit={{ y: 100, opacity: 0, transition: { duration: 0.6 } }}
-      whileHover={{ scale: 1.1, transition: { type: 'spring', stiffness: 400, damping: 10 } }}
-      whileTap={{ scale: 0.9, transition: { type: 'spring', stiffness: 400, damping: 10 } }}>
-      <svg
-        className="size-8"
-        aria-hidden="true"
-        focusable="false"
-        data-prefix="fas"
-        data-icon="chevron-up"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512">
-        <path
-          fill="currentColor"
-          d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"
-        />
-      </svg>
-    </motion.button>
+    <AnimatePresence>
+      {scrollPosition > 100 && (
+        <motion.button
+          type="button"
+          className="size-14 rounded-full bg-customRed-500 flex-center"
+          onClick={scrollToTop}
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { duration: 0.6 } }}
+          exit={{ opacity: 0, transition: { duration: 0.4 } }}
+          whileHover={{ scale: 1.1, transition: { type: 'spring', stiffness: 400, damping: 10 } }}
+          whileTap={{ scale: 0.9, transition: { type: 'spring', stiffness: 400, damping: 10 } }}>
+          <svg
+            className="size-8"
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="chevron-up"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512">
+            <path
+              fill="currentColor"
+              d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"
+            />
+          </svg>
+        </motion.button>
+      )}
+    </AnimatePresence>
   );
 };
 
