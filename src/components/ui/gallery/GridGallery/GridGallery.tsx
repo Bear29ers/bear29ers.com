76c9b137401078287b5c8544cc45bb62517c9477
Yuta Okuma <y.dream.nest@icloud.com>
2024-08-31 21:22:23 +0900

    :lipstick: ui: replace bg colors to images

--- src/components/ui/gallery/GridGallery/GridGallery.tsx
+++ src/components/ui/gallery/GridGallery/GridGallery.tsx
@@ -1,5 +1,7 @@
 import type { FC } from 'react';
 
+import { motion } from 'framer-motion';
+
 const GridGallery: FC = () => {
   const colorBlocks = [
     { color: 'bg-red-500', area: 'img-1' },
@@ -13,17 +15,25 @@ const GridGallery: FC = () => {
   ];
 
   return (
-    <div className="mt-40 grid h-[700px] w-full max-w-[1120px] grid-cols-4 gap-4">
+    <div className="mt-56 grid h-[700px] w-full max-w-[1120px] grid-cols-4 gap-4">
       {colorBlocks.map((block, index) => (
         <div
           key={index}
-          className={`${block.color} flex items-center justify-center rounded-md text-3xl font-bold text-white shadow-md
+          className={`${block.color} flex size-full items-center justify-center overflow-hidden rounded-md
               ${index === 0 ? 'col-span-1 row-span-2' : ''}
               ${index === 2 ? 'col-span-2' : ''}
               ${index === 5 ? 'row-span-2' : ''}
               ${index === 6 ? 'col-span-2' : ''}
               `}>
-          {block.area}
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a href="#" className="size-full">
+            <motion.img
+              src={`/images/gallery/gallery-0${index + 1}.jpg`}
+              alt={block.area}
+              className="size-full object-cover"
+              whileHover={{ scale: 1.1, transition: { type: 'spring' } }}
+            />
+          </a>
         </div>
       ))}
     </div>
