af98004151d1230b8449207366fafdb53cf9af1b
Yuta Okuma <y.dream.nest@icloud.com>
2024-08-31 12:59:43 +0900

    :alien: api: fetch media repeatedly until nextUrl is null

--- src/app/api/instagram/route.ts
+++ src/app/api/instagram/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 import fetchJson from '@/libs/fetchJson';
 
-import type { Media, MediaData } from '@/types/media';
+import type { Media } from '@/types/media';
 
 import type { NextRequest } from 'next/server';
 
@@ -12,19 +12,11 @@ export const GET = async (request: NextRequest) => {
   const accessToken = process.env.GRAPH_API_ACCESS_TOKEN;
   const since = request.nextUrl.searchParams.get('since');
   const queries = 'media{caption,children{media_url},media_url,media_type,permalink,like_count,timestamp,username}';
-  let url = `${baseUrl}/${instagramId}?access_token=${accessToken}&fields=${queries}&since=${since}`;
-  let allMedia: MediaData[] = [];
+  const url = `${baseUrl}/${instagramId}?access_token=${accessToken}&fields=${queries}&since=${since}`;
 
   try {
     const data = await fetchJson<Media>(url);
-
-    if (data.media && Array.isArray(data.media.data)) {
-      allMedia = allMedia.concat(data.media.data);
-    }
-
-    url = data.media.paging?.next ? `${data.media.paging?.next}&since=${since}` : '';
-
-    return NextResponse.json({ media: { data: allMedia } });
+    return NextResponse.json(data);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Error fetching Instagram media data: ', error);
