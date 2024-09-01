7f2792bc6dde3fc1f997fa809b52684af58f230e
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-01 14:32:48 +0900

    :sparkles: feat: move link list to constants

--- src/constants/gallery.ts
+++ src/constants/gallery.ts
@@ -1,1 +1,12 @@
 export const zIndexList = ['z-[2]', 'z-[1]', 'z-[5]', 'z-[3]', 'z-[6]', 'z-[4]'];
+
+export const imageLinkList = [
+  { src: 'https://www.instagram.com/p/BTNoXdplt68/' },
+  { src: 'https://www.instagram.com/p/BWFraFrFsh-/' },
+  { src: 'https://www.instagram.com/p/BS_NXzYFZbl/' },
+  { src: 'https://www.instagram.com/p/BVDcF-dliQR/' },
+  { src: 'https://www.instagram.com/p/BWJKs3blCJN/' },
+  { src: 'https://www.instagram.com/p/BUh46wWFV4Y/' },
+  { src: 'https://www.instagram.com/p/BXzJw-zFnNO/' },
+  { src: 'https://www.instagram.com/p/BeiqY7HBYaN/' },
+];
