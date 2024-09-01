88088e9475a4c357d682ba1ab5b59b1dd2371569
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-01 14:52:43 +0900

    :sparkles: feat: move grid classes to the constants

--- src/components/ui/gallery/GridGallery/GridGallery.tsx
+++ src/components/ui/gallery/GridGallery/GridGallery.tsx
@@ -2,24 +2,19 @@ import type { FC } from 'react';
 
 import { motion } from 'framer-motion';
 
-import { imageLinkList } from '@/constants/gallery';
+import type { ImageInfo } from '@/types/gallery';
 
-const GridGallery: FC = () => {
+interface Props {
+  imageInfoList: ImageInfo[];
+}
+
+const GridGallery: FC<Props> = ({ imageInfoList }) => {
   return (
     <div className="grid h-[900px] w-full max-w-[1120px] grid-cols-2 gap-4 xs:h-[800px] xsm:grid-cols-3 md:h-[700px] md:grid-cols-4">
-      {imageLinkList.map((item, index) => (
+      {imageInfoList.map((item, index) => (
         <div
           key={index}
-          className={`flex size-full items-center justify-center overflow-hidden rounded-md
-            ${index === 0 && 'col-span-2 xs:col-span-1 xs:row-span-2'}
-            ${index === 1 && 'xsm:col-span-2 md:col-span-1'}
-            ${index === 2 && 'md:col-span-2'}
-            ${index === 3 && 'col-span-2 xsm:col-span-1'}
-            ${index === 4 && 'col-span-2 xsm:col-span-1'}
-            ${index === 5 && 'md:row-span-2'}
-            ${index === 6 && 'xs:row-span-2 md:col-span-2 md:row-span-1'}
-            ${index === 7 && 'col-span-2 xs:col-span-1 xsm:col-span-2 md:col-span-1'}
-          `}>
+          className={`flex size-full items-center justify-center overflow-hidden rounded-md ${item.class}`}>
           <a href={item.src} target="_blank" className="size-full" rel="noreferrer">
             <motion.img
               src={`/images/gallery/gallery-0${index + 1}.jpg`}
--- src/constants/gallery.ts
+++ src/constants/gallery.ts
@@ -1,12 +1,14 @@
+import type { ImageInfo } from '@/types/gallery';
+
 export const zIndexList = ['z-[2]', 'z-[1]', 'z-[5]', 'z-[3]', 'z-[6]', 'z-[4]'];
 
-export const imageLinkList = [
-  { src: 'https://www.instagram.com/p/BTNoXdplt68/' },
-  { src: 'https://www.instagram.com/p/BWFraFrFsh-/' },
-  { src: 'https://www.instagram.com/p/BS_NXzYFZbl/' },
-  { src: 'https://www.instagram.com/p/BVDcF-dliQR/' },
-  { src: 'https://www.instagram.com/p/BWJKs3blCJN/' },
-  { src: 'https://www.instagram.com/p/BUh46wWFV4Y/' },
-  { src: 'https://www.instagram.com/p/BXzJw-zFnNO/' },
-  { src: 'https://www.instagram.com/p/BeiqY7HBYaN/' },
+export const imageInfoList: ImageInfo[] = [
+  { class: 'col-span-2 xs:col-span-1 xs:row-span-2', src: 'https://www.instagram.com/p/BTNoXdplt68/' },
+  { class: 'xsm:col-span-2 md:col-span-1', src: 'https://www.instagram.com/p/BWFraFrFsh-/' },
+  { class: 'md:col-span-2', src: 'https://www.instagram.com/p/BS_NXzYFZbl/' },
+  { class: 'col-span-2 xsm:col-span-1', src: 'https://www.instagram.com/p/BVDcF-dliQR/' },
+  { class: 'col-span-2 xsm:col-span-1', src: 'https://www.instagram.com/p/BWJKs3blCJN/' },
+  { class: 'md:row-span-2', src: 'https://www.instagram.com/p/BUh46wWFV4Y/' },
+  { class: 'xs:row-span-2 md:col-span-2 md:row-span-1', src: 'https://www.instagram.com/p/BXzJw-zFnNO/' },
+  { class: 'col-span-2 xs:col-span-1 xsm:col-span-2 md:col-span-1', src: 'https://www.instagram.com/p/BeiqY7HBYaN/' },
 ];
