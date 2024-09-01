5b627a6bdccfab8ea349dabebd839424a7df256c
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-01 14:32:48 +0900

    :sparkles: feat: move link list to constants

--- src/components/ui/gallery/GridGallery/GridGallery.tsx
+++ src/components/ui/gallery/GridGallery/GridGallery.tsx
@@ -2,21 +2,12 @@ import type { FC } from 'react';
 
 import { motion } from 'framer-motion';
 
-const GridGallery: FC = () => {
-  const colorBlocks = [
-    { area: 'grid-gallery-img-1', src: 'https://www.instagram.com/p/BTNoXdplt68/' },
-    { area: 'grid-gallery-img-2', src: 'https://www.instagram.com/p/BWFraFrFsh-/' },
-    { area: 'grid-gallery-img-3', src: 'https://www.instagram.com/p/BS_NXzYFZbl/' },
-    { area: 'grid-gallery-img-4', src: 'https://www.instagram.com/p/BVDcF-dliQR/' },
-    { area: 'grid-gallery-img-5', src: 'https://www.instagram.com/p/BWJKs3blCJN/' },
-    { area: 'grid-gallery-img-6', src: 'https://www.instagram.com/p/BUh46wWFV4Y/' },
-    { area: 'grid-gallery-img-7', src: 'https://www.instagram.com/p/BXzJw-zFnNO/' },
-    { area: 'grid-gallery-img-8', src: 'https://www.instagram.com/p/BeiqY7HBYaN/' },
-  ];
+import { imageLinkList } from '@/constants/gallery';
 
+const GridGallery: FC = () => {
   return (
     <div className="grid h-[900px] w-full max-w-[1120px] grid-cols-2 gap-4 xs:h-[800px] xsm:grid-cols-3 md:h-[700px] md:grid-cols-4">
-      {colorBlocks.map((block, index) => (
+      {imageLinkList.map((item, index) => (
         <div
           key={index}
           className={`flex size-full items-center justify-center overflow-hidden rounded-md
@@ -29,10 +20,10 @@ const GridGallery: FC = () => {
             ${index === 6 && 'xs:row-span-2 md:col-span-2 md:row-span-1'}
             ${index === 7 && 'col-span-2 xs:col-span-1 xsm:col-span-2 md:col-span-1'}
           `}>
-          <a href={block.src} target="_blank" className="size-full" rel="noreferrer">
+          <a href={item.src} target="_blank" className="size-full" rel="noreferrer">
             <motion.img
               src={`/images/gallery/gallery-0${index + 1}.jpg`}
-              alt={block.area}
+              alt={`grid-gallery-img-0${index + 1}`}
               className="size-full object-cover"
               whileHover={{ scale: 1.1, transition: { type: 'spring' } }}
             />
