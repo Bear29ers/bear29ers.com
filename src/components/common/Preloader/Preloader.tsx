ffc6fff2a7d718824dfe0a1817155fd2ae32fd1b
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-05 18:03:53 +0900

    :sparkles: feat: complete implementation of the Preloader

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -2,8 +2,47 @@ import type { FC } from 'react';
 
 const Preloader: FC = () => {
   return (
-    <div>
-      <h1>Preloader</h1>
+    <div className="flex w-fit">
+      <div className="relative mx-4 inline-block size-11 before:absolute before:left-[19px] before:top-[37px] before:size-1.5 before:translate-x-[-18px] before:translate-y-[-18px] before:animate-dotRect before:rounded-full before:bg-[#5628EE] before:content-['']">
+        <svg viewBox="0 0 80 80" className="size-full">
+          <circle
+            cx="40"
+            cy="40"
+            r="32"
+            className="animate-pathCircle fill-none stroke-[#2F3545] stroke-[10]"
+            style={{ strokeDasharray: '150 50 150 50', strokeDashoffset: 75 }}
+            strokeLinejoin="round"
+            strokeLinecap="round"
+          />
+        </svg>
+      </div>
+
+      <div className="relative mx-4 inline-block h-11 w-12 before:absolute before:left-[21px] before:top-[37px] before:size-1.5 before:translate-x-[-10px] before:translate-y-[-18px] before:animate-dotTriangle before:rounded-full before:bg-[#5628EE] before:content-['']">
+        <svg viewBox="0 0 86 80" className="size-full">
+          <polygon
+            points="43 8 79 72 7 72"
+            className="animate-pathTriangle fill-none stroke-[#2F3545] stroke-[10]"
+            style={{ strokeDasharray: '145 76 145 76', strokeDashoffset: 0 }}
+            strokeLinejoin="round"
+            strokeLinecap="round"
+          />
+        </svg>
+      </div>
+
+      <div className="relative mx-4 inline-block size-11 before:absolute before:left-[19px] before:top-[37px] before:size-1.5 before:translate-x-[-18px] before:translate-y-[-18px] before:animate-dotRect before:rounded-full before:bg-[#5628EE] before:content-['']">
+        <svg viewBox="0 0 80 80" className="size-full">
+          <rect
+            x="8"
+            y="8"
+            width="64"
+            height="64"
+            className="animate-pathRect fill-none stroke-[#2F3545] stroke-[10]"
+            style={{ strokeDasharray: '192 64 192 64', strokeDashoffset: 0 }}
+            strokeLinejoin="round"
+            strokeLinecap="round"
+          />
+        </svg>
+      </div>
     </div>
   );
 };
