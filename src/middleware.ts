6654ff7820fe6443a441482f8721865b1c481766
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-06 00:12:46 +0900

    :fire: rem: remove x-request-base from requestHeaders

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,9 +6,6 @@ export const middleware = (request: NextRequest) => {
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-request-url', request.url);
 
-  const base = `${request.nextUrl.protocol}//${request.nextUrl.host}`;
-  requestHeaders.set('x-request-base', base);
-
   const res = NextResponse.next({
     request: {
       headers: requestHeaders,
