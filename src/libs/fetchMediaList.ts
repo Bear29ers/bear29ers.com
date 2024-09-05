8d9ac5058b835681b6458386caafd2d5416baf05
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-05 20:48:04 +0900

    :recycle: refa: remove unnecessary code

--- src/libs/fetchMediaList.ts
+++ src/libs/fetchMediaList.ts
@@ -1,40 +1,17 @@
 import { headers } from 'next/headers';
 
-import { convertToCamelCase } from '@/utils/conversion/convertToCamelCase';
+import type { MediaData } from '@/types/media';
 
-import type { MediaEdge, Media, MediaData } from '@/types/media';
-
-import fetchJson from './fetchJson';
-
-const fetchMediaList = async (unixtime: number): Promise<Media> => {
+const fetchMediaList = async (unixtime: number): Promise<MediaData[]> => {
   const baseUrl = headers().get('x-request-base');
   const res = await fetch(`${baseUrl}/api/instagram?since=${unixtime}`);
+  const data = (await res.json()) as MediaData[];
 
   if (!res.ok) {
     throw new Error('Failed to fetch media data');
   }
 
-  let allData: MediaData[] = [];
-  const data = (await res.json()) as Media;
-
-  if (data.media && Array.isArray(data.media.data)) {
-    allData = allData.concat(data.media.data);
-  }
-
-  let nextUrl = data.media.paging?.next || null;
-
-  while (nextUrl) {
-    // eslint-disable-next-line no-await-in-loop
-    const nextData = await fetchJson<MediaEdge>(`${nextUrl}&since=${unixtime}`);
-
-    if (nextData.data && Array.isArray(nextData.data)) {
-      allData = allData.concat(nextData.data);
-    }
-
-    nextUrl = nextData.paging?.next || null;
-  }
-
-  return convertToCamelCase({ media: { data: allData } });
+  return data;
 };
 
 export default fetchMediaList;
