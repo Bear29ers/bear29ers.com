50ceb48992187c74fa8bac23aa791b723b65c5e2
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-07 18:54:52 +0900

    :sparkles: feat: implemented the Thumbnails

--- src/components/ui/gallery/Thumbnails/Thumbnails.tsx
+++ src/components/ui/gallery/Thumbnails/Thumbnails.tsx
@@ -1,9 +1,59 @@
-import type { FC } from 'react';
+import { useEffect, type Dispatch, type FC, type SetStateAction } from 'react';
 
-const Thumbnails: FC = () => {
+import { motion, useMotionTemplate, useSpring } from 'framer-motion';
+
+const COLLAPSED_ASPECT_RATIO = 0.5;
+const FULL_ASPECT_RATIO = 3 / 2;
+const MARGIN = 24;
+const GAP = 2;
+
+interface Props {
+  images: string[];
+  index: number;
+  setIndex: Dispatch<SetStateAction<number>>;
+}
+
+const Thumbnails: FC<Props> = ({ images, index, setIndex }) => {
+  const x = index * 100 * (COLLAPSED_ASPECT_RATIO / FULL_ASPECT_RATIO) + MARGIN + index * GAP;
+  const xSpring = useSpring(x, { bounce: 0 });
+  const xPercentage = useMotionTemplate`-${xSpring}`;
+
+  useEffect(() => {
+    xSpring.set(x);
+  }, [x, xSpring]);
+
   return (
-    <div>
-      <h1>Thumbnails</h1>
+    <div className="mb-6 flex h-12 justify-center overflow-hidden">
+      <motion.div
+        style={{
+          aspectRatio: FULL_ASPECT_RATIO,
+          gap: `${GAP}%`,
+          x: xPercentage,
+        }}
+        className="flex min-w-0">
+        {images.map((image, i) => (
+          <motion.button
+            onClick={() => setIndex(i)}
+            initial={false}
+            animate={i === index ? 'active' : 'inactive'}
+            variants={{
+              active: {
+                aspectRatio: FULL_ASPECT_RATIO,
+                marginLeft: `${MARGIN}%`,
+                marginRight: `${MARGIN}%`,
+              },
+              inactive: {
+                aspectRatio: COLLAPSED_ASPECT_RATIO,
+                marginLeft: 0,
+                marginRight: 0,
+              },
+            }}
+            className="h-full shrink-0"
+            key={image}>
+            <img alt="" src={image} className="h-full object-cover" />
+          </motion.button>
+        ))}
+      </motion.div>
     </div>
   );
 };
