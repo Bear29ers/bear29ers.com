aa07f10615af614d2ddf195d0c5354957d677631
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-07 09:56:07 +0900

    :sparkles: feat: generate a gallery detail page with dynamic routes

---
+++ src/app/gallery/[mediaId]/page.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next';
+
+interface Props {
+  params: {
+    mediaId: string;
+  };
+}
+
+const GalleryDetail: NextPage<Props> = ({ params }) => {
+  console.log('params: ', params.mediaId);
+  return (
+    <div className="relative h-screen w-screen overflow-hidden">
+      <h1>Gallery Detail</h1>
+      <h2>{params.mediaId}</h2>
+    </div>
+  );
+};
+
+export default GalleryDetail;
