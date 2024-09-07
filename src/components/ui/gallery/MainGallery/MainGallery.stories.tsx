c03164f5231c59112a11bdbc19ac36df053568a7
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-07 09:10:06 +0900

    :bug: fix: fix the MainGallery storybook

--- src/components/ui/gallery/MainGallery/MainGallery.stories.tsx
+++ src/components/ui/gallery/MainGallery/MainGallery.stories.tsx
@@ -12,8 +12,8 @@ export default meta;
 type Story = StoryObj<typeof MainGallery>;
 
 export const Default: Story = {
-  args: { imageSrc: '/images/example/image-4.jpg', id: '17924112776916166', maxWidth: 'max-w-72' },
-  render: ({ imageSrc, id, maxWidth }) => {
-    return <MainGallery imageSrc={imageSrc} id={id} maxWidth={maxWidth} />;
+  args: { imageSrc: '/images/example/image-4.jpg', id: '17924112776916166', width: 288, maxWidth: 'max-w-72' },
+  render: ({ imageSrc, id, width, maxWidth }) => {
+    return <MainGallery imageSrc={imageSrc} id={id} width={width} maxWidth={maxWidth} />;
   },
 };
