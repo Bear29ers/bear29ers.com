b64e9471bdf351f1d09408adff0764dd1ed874a3
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-07 18:45:50 +0900

    :construction: wip: implementing the Carousel

--- src/app/gallery/[mediaId]/client.tsx
+++ src/app/gallery/[mediaId]/client.tsx
@@ -2,10 +2,12 @@
 
 import type { FC } from 'react';
 
+import Carousel from '@/components/ui/gallery/Carousel/Carousel';
+
 const GalleryDetailClient: FC = () => {
   return (
     <div className="min-h-screen w-full flex-center">
-      <p>GalleryDetailClient</p>
+      <Carousel />
     </div>
   );
 };
