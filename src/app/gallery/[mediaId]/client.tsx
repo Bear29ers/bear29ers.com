efabd6b62f386d2166bd4fcdcab6d4be07317810
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-07 18:04:04 +0900

    :sparkles: feat: generate a Carousel component

---
+++ src/app/gallery/[mediaId]/client.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import type { FC } from 'react';
+
+const GalleryDetailClient: FC = () => {
+  return (
+    <div className="min-h-screen w-full flex-center">
+      <p>GalleryDetailClient</p>
+    </div>
+  );
+};
+
+export default GalleryDetailClient;
---
+++ src/components/ui/gallery/Carousel/Carousel.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+const Carousel: FC = () => {
+  return (
+    <div>
+      <h1>Carousel</h1>
+    </div>
+  );
+};
+
+export default Carousel;
