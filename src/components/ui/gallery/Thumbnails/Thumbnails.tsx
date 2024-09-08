b207cd50c89954043461720faed400e387d980f7
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-08 11:35:32 +0900

    :lipstick: ui: centering the Thumbnails

--- src/components/ui/gallery/Thumbnails/Thumbnails.tsx
+++ src/components/ui/gallery/Thumbnails/Thumbnails.tsx
@@ -1,9 +1,10 @@
-import { useEffect, type Dispatch, type FC, type SetStateAction } from 'react';
+import { type Dispatch, type FC, type SetStateAction } from 'react';
 
-import { motion, useMotionTemplate, useSpring } from 'framer-motion';
+import { motion } from 'framer-motion';
+// import Image from 'next/image';
 
-const COLLAPSED_ASPECT_RATIO = 0.5;
-const FULL_ASPECT_RATIO = 3 / 2;
+const COLLAPSED_ASPECT_RATIO = 0.4;
+const FULL_ASPECT_RATIO = 4 / 5;
 const MARGIN = 24;
 const GAP = 2;
 
@@ -14,23 +15,14 @@ interface Props {
 }
 
 const Thumbnails: FC<Props> = ({ images, index, setIndex }) => {
-  const x = index * 100 * (COLLAPSED_ASPECT_RATIO / FULL_ASPECT_RATIO) + MARGIN + index * GAP;
-  const xSpring = useSpring(x, { bounce: 0 });
-  const xPercentage = useMotionTemplate`-${xSpring}`;
-
-  useEffect(() => {
-    xSpring.set(x);
-  }, [x, xSpring]);
-
   return (
-    <div className="mb-6 flex h-12 justify-center overflow-hidden">
+    <div className="flex h-40 justify-center">
       <motion.div
         style={{
           aspectRatio: FULL_ASPECT_RATIO,
           gap: `${GAP}%`,
-          x: xPercentage,
         }}
-        className="flex min-w-0">
+        className="flex min-w-0 justify-center">
         {images.map((image, i) => (
           <motion.button
             onClick={() => setIndex(i)}
@@ -51,6 +43,7 @@ const Thumbnails: FC<Props> = ({ images, index, setIndex }) => {
             className="h-full shrink-0"
             key={image}>
             <img alt="" src={image} className="h-full object-cover" />
+            {/* <Image src={image} alt={image} sizes="100vw" width={400} height={500} className="w-full object-cover" /> */}
           </motion.button>
         ))}
       </motion.div>
