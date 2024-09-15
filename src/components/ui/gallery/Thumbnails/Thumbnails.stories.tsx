ff26ce5f5033353f91329eb0c31e5034200338a3
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-15 10:51:22 +0900

    :sparkles: feat: complete the Thumbnails storybook

--- src/components/ui/gallery/Thumbnails/Thumbnails.stories.tsx
+++ src/components/ui/gallery/Thumbnails/Thumbnails.stories.tsx
@@ -4,6 +4,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof Thumbnails> = {
   component: Thumbnails,
+  tags: ['autodocs'],
 };
 
 export default meta;
@@ -11,5 +12,35 @@ export default meta;
 type Story = StoryObj<typeof Thumbnails>;
 
 export const Default: Story = {
-  render: () => <Thumbnails />,
+  args: {
+    images: {
+      data: [
+        {
+          mediaUrl: 'images/example/image-1.jpg',
+          id: '18274358392235025',
+        },
+        {
+          mediaUrl: 'images/example/image-2.jpg',
+          id: '18274358392235025',
+        },
+        {
+          mediaUrl: 'images/example/image-3.jpg',
+          id: '18274358392235025',
+        },
+        {
+          mediaUrl: 'images/example/image-4.jpg',
+          id: '18274358392235025',
+        },
+        {
+          mediaUrl: 'images/example/image-5.jpg',
+          id: '18274358392235025',
+        },
+      ],
+    },
+    index: 0,
+    setIndex: () => {},
+  },
+  render: ({ images, index, setIndex }) => {
+    return <Thumbnails images={images} index={index} setIndex={setIndex} />;
+  },
 };
