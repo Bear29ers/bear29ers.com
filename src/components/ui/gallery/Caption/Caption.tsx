ac62c606babd5535c49287dca2c677cc69543254
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-23 08:47:50 +0900

    :sparkles: feat: add likeCounts and timestamp

--- src/components/ui/gallery/Caption/Caption.tsx
+++ src/components/ui/gallery/Caption/Caption.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 
+import { HeartIcon } from '@heroicons/react/16/solid';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -14,6 +15,17 @@ interface Props {
 }
 
 const Caption: FC<Props> = ({ media, href, alt }) => {
+  const date = new Date(media.timestamp || '');
+  const jstTime = date.toLocaleString('ja-JP', {
+    timeZone: 'Asia/Tokyo',
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  });
+  const formattedDate = jstTime.replace(/\//g, '-');
+
   return (
     <div>
       <div className="mt-4 flex flex-col gap-y-6 rounded-lg bg-darker p-5 text-white">
@@ -28,7 +40,18 @@ const Caption: FC<Props> = ({ media, href, alt }) => {
           />
           <h4 className="text-lg">{media.username || alt}</h4>
         </Link>
-        <p className="whitespace-pre-wrap">{media.caption}</p>
+        <div className="flex flex-col gap-y-4">
+          <p className="whitespace-pre-wrap">{media.caption}</p>
+          <div className="flex items-center justify-between">
+            <div className="flex items-end">
+              <HeartIcon className="size-8 text-pink-500" />
+              <span className="-ml-0.5 mb-1 text-xs">{media.likeCount}</span>
+            </div>
+            <time dateTime={media.timestamp} className="text-gray-500">
+              {formattedDate}
+            </time>
+          </div>
+        </div>
       </div>
     </div>
   );
