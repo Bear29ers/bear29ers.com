ec4a87e9300b6b60ea4109d96b7715d4ddd7cb47
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-23 07:35:31 +0900

    :sparkles: feat: create the Caption

--- src/components/ui/gallery/Caption/Caption.tsx
+++ src/components/ui/gallery/Caption/Caption.tsx
@@ -1,9 +1,33 @@
 import type { FC } from 'react';
 
-const Caption: FC = () => {
+import Image from 'next/image';
+
+import type { Media } from '@/types/media';
+
+import ProfileIcon from '~/images/profile_2.jpg';
+
+interface Props {
+  media: Media;
+  alt: string;
+}
+
+const Caption: FC<Props> = ({ media, alt }) => {
   return (
     <div>
-      <h1>Caption</h1>
+      <div className="mt-4 flex flex-col gap-y-6 rounded-lg bg-darker p-5 text-white">
+        <div className="flex items-center gap-x-4">
+          <Image
+            src={ProfileIcon}
+            alt={media.username || alt}
+            sizes="100vw"
+            width={100}
+            height={100}
+            className="w-12 rounded-full"
+          />
+          <h4 className="text-lg">{media.username || alt}</h4>
+        </div>
+        <p className="w-full max-w-[300px] whitespace-pre-wrap">{media.caption}</p>
+      </div>
     </div>
   );
 };
