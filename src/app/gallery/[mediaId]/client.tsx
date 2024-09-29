496c535bac4888ffa87e1fce495bc7b7c4178d24
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-29 09:48:57 +0900

    :sparkles: feat: add footer

--- src/app/gallery/[mediaId]/client.tsx
+++ src/app/gallery/[mediaId]/client.tsx
@@ -3,6 +3,7 @@
 import { useState, type FC } from 'react';
 
 import AnimatedArrowButton from '@/components/common/AnimatedArrowButton/AnimatedArrowButton';
+import Footer from '@/components/layout/Footer/Footer';
 import Caption from '@/components/ui/gallery/Caption/Caption';
 import Carousel from '@/components/ui/gallery/Carousel/Carousel';
 import Thumbnails from '@/components/ui/gallery/Thumbnails/Thumbnails';
@@ -19,21 +20,25 @@ const GalleryDetailClient: FC<Props> = ({ media }) => {
   const [index, setIndex] = useState<number>(0);
 
   return (
-    <div className="relative min-h-screen w-full flex-col gap-y-10 overflow-y-scroll py-24 flex-center">
-      <div className="absolute left-4 top-4 xsm:left-8 xsm:top-8 lg:left-10">
-        <AnimatedArrowButton url="/gallery?intro=skipped" text="Back to Gallery" />
-      </div>
-      <div className="mt-10 flex flex-col gap-x-8 gap-y-10 lg:mt-0 lg:flex-row">
-        <div className="flex flex-col gap-y-6">
-          <Carousel media={media} index={index} setIndex={setIndex} />
-          {media.children && media.children.data.length && (
-            <Thumbnails images={media.children} index={index} setIndex={setIndex} />
-          )}
+    <div className="relative min-h-screen w-full overflow-y-scroll text-white">
+      <div className="flex-col gap-y-10 pt-24 flex-center">
+        <div className="absolute left-4 top-4 xsm:left-8 xsm:top-8 lg:left-10">
+          <AnimatedArrowButton url="/gallery?intro=skipped" text="Back to Gallery" />
         </div>
-        <div className="mx-auto w-full max-w-xl lg:max-w-[350px]">
-          <Caption media={media} href={CAPTION_INFO.profileHref} alt={CAPTION_INFO.username} />
+        <div className="mt-10 flex flex-col gap-x-8 gap-y-10 lg:mt-0 lg:flex-row">
+          <div className="flex flex-col gap-y-6">
+            <Carousel media={media} index={index} setIndex={setIndex} />
+            {media.children && media.children.data.length && (
+              <Thumbnails images={media.children} index={index} setIndex={setIndex} />
+            )}
+          </div>
+          <div className="mx-auto w-full max-w-xl lg:max-w-[350px]">
+            <Caption media={media} href={CAPTION_INFO.profileHref} alt={CAPTION_INFO.username} />
+          </div>
         </div>
       </div>
+      {/* Footer */}
+      <Footer />
     </div>
   );
 };
