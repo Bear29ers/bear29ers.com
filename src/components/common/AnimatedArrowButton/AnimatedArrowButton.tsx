1c88b0ef09010ec08169e997941beb592a107303
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-29 09:44:19 +0900

    :iphone: resp: smまでのレスポンシブ対応

--- src/components/common/AnimatedArrowButton/AnimatedArrowButton.tsx
+++ src/components/common/AnimatedArrowButton/AnimatedArrowButton.tsx
@@ -15,7 +15,7 @@ const AnimatedArrowButton: FC<Props> = ({ url, text }) => {
       className="group relative flex w-52 justify-end"
       onClick={() => router.push(url, { scroll: false })}>
       <span
-        className="relative m-0 block size-12 rounded-full bg-darkerGray transition-all duration-450 ease-custom-ease-in-out-quart group-hover:w-full md:bg-darker"
+        className="relative m-0 block size-12 rounded-full bg-darkerGray transition-all duration-450 ease-custom-ease-in-out-quart group-hover:w-full txs:bg-darker"
         aria-hidden="true">
         <span className="absolute inset-y-0 right-2.5 m-auto h-0.5 w-4.5 bg-none transition-all duration-450 ease-custom-ease-in-out-quart group-hover:-translate-x-4 group-hover:bg-white">
           <span className="absolute -top-1 left-px size-2.5 -rotate-45 border-l-2 border-t-2 border-white" />
