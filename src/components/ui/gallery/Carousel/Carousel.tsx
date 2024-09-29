e4e460c71573316f8274db490fbb4714b273ee02
Yuta Okuma <y.dream.nest@icloud.com>
2024-09-29 11:02:39 +0900

    :iphone: resp: make the Carousel responsive

--- src/components/ui/gallery/Carousel/Carousel.tsx
+++ src/components/ui/gallery/Carousel/Carousel.tsx
@@ -42,14 +42,14 @@ const Carousel: FC<Props> = ({ media, index, setIndex }) => {
 
   return (
     <MotionConfig transition={{ type: 'spring', bounce: 0 }}>
-      <div className="flex h-full">
-        <div className="relative w-[600px] flex-center">
-          <div className="w-full max-w-[450px] overflow-hidden">
+      <div className="flex h-full px-1 sm:px-0">
+        <div className="relative w-full flex-center sm:w-[600px]">
+          <div className="w-full max-w-72 overflow-hidden xs:max-w-80 sm:max-w-[450px]">
             <motion.div style={{ x: xPercentage }} className="flex">
               {media.children?.data.map((item, i) => (
                 <motion.div
                   key={`carousel-${item.id}-${i}`}
-                  className="relative h-[562.5px] w-[450px] shrink-0"
+                  className="relative h-[360px] w-72 shrink-0 xs:h-[400px] xs:w-80 sm:h-[562.5px] sm:w-[450px]"
                   animate={{ opacity: i === index ? 1 : 0.4 }}>
                   <Image
                     src={item.mediaUrl}
@@ -69,10 +69,10 @@ const Carousel: FC<Props> = ({ media, index, setIndex }) => {
                 animate={{ opacity: 0.7 }}
                 exit={{ opacity: 0, pointerEvents: 'none' }}
                 whileHover={{ opacity: 1 }}
-                className="absolute left-0 top-1/2 flex size-10 -translate-y-1/2 items-center justify-center rounded-full bg-white"
+                className="absolute left-0 top-1/2 hidden size-8 -translate-y-1/2 items-center justify-center rounded-full bg-white xs:flex txs:size-10"
                 aria-label="chevron-left"
                 onClick={() => setIndex(index - 1)}>
-                <ChevronLeftIcon className="size-8 text-black" />
+                <ChevronLeftIcon className="size-7 text-black txs:size-8" />
               </motion.button>
             )}
           </AnimatePresence>
@@ -84,10 +84,10 @@ const Carousel: FC<Props> = ({ media, index, setIndex }) => {
                 animate={{ opacity: 0.7 }}
                 exit={{ opacity: 0, pointerEvents: 'none' }}
                 whileHover={{ opacity: 1 }}
-                className="absolute right-0 top-1/2 flex size-10 -translate-y-1/2 items-center justify-center rounded-full bg-white"
+                className="absolute right-0 top-1/2 hidden size-8 -translate-y-1/2 items-center justify-center rounded-full bg-white xs:flex txs:size-10"
                 aria-label="chevron-right"
                 onClick={() => setIndex(index + 1)}>
-                <ChevronRightIcon className="size-8 text-black" />
+                <ChevronRightIcon className="size-7 text-black txs:size-8" />
               </motion.button>
             )}
           </AnimatePresence>
