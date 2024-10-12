ab9592ed0f5b819c04e4cc0d369b22ce2bb92cec
Yuta Okuma <y.dream.nest@icloud.com>
2024-10-12 17:27:40 +0900

    :sparkles: feat: add x-request-path in the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,10 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export const middleware = (request: NextRequest) => {
-  const { headers, url } = request;
+  const { headers, url, nextUrl } = request;
   const requestHeaders = new Headers(headers);
   requestHeaders.set('x-request-url', url);
+  requestHeaders.set('x-request-path', nextUrl.pathname);
 
   const res = NextResponse.next({
     request: {
