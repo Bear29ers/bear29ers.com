d56b6867bc72e8b86a7e369d1e7a259c63ffb04e
Yuta Okuma <y.dream.nest@icloud.com>
2024-10-14 13:13:33 +0900

    :sparkles: feat: add contents of the gallery modal

--- src/app/@modal/(.)gallery/[mediaId]/client.tsx
+++ src/app/@modal/(.)gallery/[mediaId]/client.tsx
@@ -3,6 +3,11 @@
 import { useState, type FC } from 'react';
 
 import Modal from '@/components/common/Modal/Modal';
+import Caption from '@/components/ui/gallery/Caption/Caption';
+import Carousel from '@/components/ui/gallery/Carousel/Carousel';
+import Thumbnails from '@/components/ui/gallery/Thumbnails/Thumbnails';
+
+import { CAPTION_INFO } from '@/constants/gallery';
 
 import type { Media } from '@/types/media';
 
@@ -12,10 +17,35 @@ interface Props {
 
 const GalleryModalClient: FC<Props> = ({ media }) => {
   const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [index, setIndex] = useState<number>(0);
+  const [touchPosition, setTouchPosition] = useState<number | null>(null);
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <div className="rounded-md bg-white p-4">モーダル表示</div>
+      <div className="rounded-md bg-darkerGray p-4">
+        <div className="flex-col gap-y-10 overflow-y-auto flex-center">
+          <div className="flex flex-col gap-x-8 gap-y-10 px-1.5 sm:px-0 lg:mt-0 lg:flex-row">
+            <div className="flex flex-col gap-y-6">
+              {/* Carousel */}
+              <Carousel
+                media={media}
+                index={index}
+                touchPosition={touchPosition}
+                setIndex={setIndex}
+                setTouchPosition={setTouchPosition}
+              />
+              {/* Thumbnails */}
+              {media.children && media.children.data.length && (
+                <Thumbnails images={media.children} index={index} setIndex={setIndex} />
+              )}
+            </div>
+            <div className="mx-auto w-full max-w-xl lg:max-w-[450px]">
+              {/* Caption */}
+              <Caption media={media} href={CAPTION_INFO.profileHref} alt={CAPTION_INFO.username} index={index} />
+            </div>
+          </div>
+        </div>
+      </div>
     </Modal>
   );
 };
