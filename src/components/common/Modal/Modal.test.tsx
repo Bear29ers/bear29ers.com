10321a3d212c9bd2e162c9aab31432e950f47b01
Yuta Okuma <y.dream.nest@icloud.com>
2024-10-19 19:57:33 +0900

    :white_check_mark: test: complete the Modal test

--- src/components/common/Modal/Modal.test.tsx
+++ src/components/common/Modal/Modal.test.tsx
@@ -1,10 +1,94 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Modal from './Modal';
 
+import type { RenderResult } from '@testing-library/react';
+
+const routerBackMock = jest.fn();
+
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    back: routerBackMock,
+  }),
+}));
+
 describe('src/components/common/Modal/Modal.test.tsx', () => {
-  it('should render the Modal component', () => {
-    const { getByRole } = render(<Modal />);
+  const mockSetIsOpen = jest.fn();
+
+  let renderResult: RenderResult;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterEach(() => {
+    renderResult.unmount();
+  });
+
+  it('should render children when isOpen props is true', () => {
+    renderResult = render(
+      <Modal isOpen setIsOpen={mockSetIsOpen}>
+        <div>Modal Content</div>
+      </Modal>
+    );
+
+    expect(screen.getByText('Modal Content')).toBeInTheDocument();
+  });
+
+  it('should not render children when isOpen props is false', () => {
+    renderResult = render(
+      <Modal isOpen={false} setIsOpen={mockSetIsOpen}>
+        <div>Modal Content</div>
+      </Modal>
+    );
+
+    expect(screen.queryByText('Modal Content')).not.toBeInTheDocument();
+  });
+
+  it('should add overflow-hidden class to body when isOpen props is true', () => {
+    renderResult = render(
+      <Modal isOpen setIsOpen={mockSetIsOpen}>
+        <div>Modal Content</div>
+      </Modal>
+    );
+
+    expect(document.body).toHaveClass('overflow-hidden');
+  });
+
+  it('should remove overflow-hidden class to body when isOpen props is false', () => {
+    renderResult = render(
+      <Modal isOpen={false} setIsOpen={mockSetIsOpen}>
+        <div>Modal Content</div>
+      </Modal>
+    );
+
+    expect(document.body).not.toHaveClass('overflow-hidden');
+  });
+
+  it('should call router.back() and setIsOpen(false) when clicking outside the modal', () => {
+    renderResult = render(
+      <Modal isOpen setIsOpen={mockSetIsOpen}>
+        <div>Modal Content</div>
+      </Modal>
+    );
+
+    fireEvent.mouseDown(renderResult.container.firstChild as Element);
+
+    expect(routerBackMock).toHaveBeenCalled();
+    expect(mockSetIsOpen).toHaveBeenCalledWith(false);
+  });
+
+  it('should not call router.back() and setIsOpen(false) when clicking inside the modal', () => {
+    renderResult = render(
+      <Modal isOpen setIsOpen={mockSetIsOpen}>
+        <div>Modal Content</div>
+      </Modal>
+    );
+
+    fireEvent.mouseDown(screen.getByText('Modal Content'));
+
+    expect(routerBackMock).not.toHaveBeenCalled();
+    expect(mockSetIsOpen).not.toHaveBeenCalledWith(false);
   });
 });
