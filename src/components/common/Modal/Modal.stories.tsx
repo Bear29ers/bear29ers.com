f142e1c474bb9fa48d387034aceec98af143662f
Yuta Okuma <y.dream.nest@icloud.com>
2024-10-19 19:01:32 +0900

    :white_check_mark: test: complete the Modal storybook

--- src/components/common/Modal/Modal.stories.tsx
+++ src/components/common/Modal/Modal.stories.tsx
@@ -4,6 +4,12 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof Modal> = {
   component: Modal,
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  tags: ['autodocs'],
 };
 
 export default meta;
@@ -11,5 +17,14 @@ export default meta;
 type Story = StoryObj<typeof Modal>;
 
 export const Default: Story = {
-  render: () => <Modal />,
+  args: {
+    isOpen: true,
+    setIsOpen: () => {},
+    children: <div className="rounded-md bg-white px-8 py-4">Modal</div>,
+  },
+  render: ({ isOpen, setIsOpen, children }) => (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      {children}
+    </Modal>
+  ),
 };
