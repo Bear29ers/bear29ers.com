d311eab483a33518c409fa2977ee9caf04014350
Yuta Okuma <y.dream.nest@icloud.com>
2024-11-09 21:07:21 +0900

    :sparkles: feat: fix layout

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from 'react';
 
+import { useTranslations } from 'next-intl';
+
 import Bear from '@/components/ui/Bear/Bear';
 import Intro from '@/components/ui/Intro/Intro';
 import PageNavigation from '@/components/ui/PageNavigation/PageNavigation';
@@ -13,9 +15,11 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   const [isClicked, setIsClicked] = useState<boolean>(false);
+  const t = useTranslations('Home');
 
   return (
     <div className="relative h-screen w-screen overflow-hidden">
+      <h1>{t('title')}</h1>
       {/* Intro */}
       <Intro isClicked={isClicked} homeProfileText={HOME_PROFILE_TEXT} />
       {/* Bear */}
