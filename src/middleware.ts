72d5ef1d0a7b6576e2e84be1c32687e2faaab4ae
Yuta Okuma <y.dream.nest@icloud.com>
2024-11-09 10:38:41 +0900

    :sparkles: feat: update middleware temporally for i18n

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,36 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
+//
+// import type { NextRequest } from 'next/server';
+//
+// export const middleware = (request: NextRequest) => {
+//   const { headers, url, nextUrl } = request;
+//   let { pathname } = nextUrl;
+//
+//   // if pathname includes 'gallery', replace it with '/gallery'
+//   if (pathname.includes('gallery')) {
+//     pathname = '/gallery';
+//   }
+//
+//   const requestHeaders = new Headers(headers);
+//   requestHeaders.set('x-request-url', url);
+//   requestHeaders.set('x-request-path', pathname);
+//
+//   const res = NextResponse.next({
+//     request: {
+//       headers: requestHeaders,
+//     },
+//   });
+//
+//   return res;
+// };
 
-import type { NextRequest } from 'next/server';
+import createMiddleware from 'next-intl/middleware';
 
-export const middleware = (request: NextRequest) => {
-  const { headers, url, nextUrl } = request;
-  let { pathname } = nextUrl;
+import { routing } from './i18n/routing';
 
-  // if pathname includes 'gallery', replace it with '/gallery'
-  if (pathname.includes('gallery')) {
-    pathname = '/gallery';
-  }
+export default createMiddleware(routing);
 
-  const requestHeaders = new Headers(headers);
-  requestHeaders.set('x-request-url', url);
-  requestHeaders.set('x-request-path', pathname);
-
-  const res = NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
-
-  return res;
+export const config = {
+  // Match only internationalized pathnames
+  matcher: ['/', '/(ja|en)/:path*', '/((?!_next|_vercel|.*\\..*).*)'],
 };
