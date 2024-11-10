c51aab970167377eea79992a816a4aa39666ca17
Yuta Okuma <y.dream.nest@icloud.com>
2024-11-10 14:31:46 +0900

    :truck: mv: rename InternationalIcons component

--- src/components/icons/InternationalIcons/InternationalIcons.test.tsx
+++ src/components/icons/InternationalIcons/InternationalIcons.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import JapanIcon from './JapanIcon';
+import { JapanIcon } from './InternationalIcons';
 
 describe('src/components/icons/JapanIcon/JapanIcon.test.tsx', () => {
   it('should render the JapanIcon component', () => {
