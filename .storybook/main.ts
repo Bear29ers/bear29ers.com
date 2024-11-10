74ac4ae31177dbbe2e9860c9a7291afb6b2f5b1c
Yuta Okuma <y.dream.nest@icloud.com>
2024-11-10 15:22:36 +0900

    :heavy_plus_sign: dpad: add storybook-next-intl for use i18n in the storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,7 @@ const config: StorybookConfig = {
     '@storybook/addon-interactions',
     '@storybook/addon-controls',
     '@chromatic-com/storybook',
+    'storybook-next-intl',
   ],
 
   framework: {
