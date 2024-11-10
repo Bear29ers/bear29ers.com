5e435a33c427846f539f4f4ed5a53d6ea76deba6
Yuta Okuma <y.dream.nest@icloud.com>
2024-11-10 11:29:31 +0900

    :globe_with_meridians: i18n: integrate default middleware with i18n createMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,39 @@
-// import { NextResponse } from 'next/server';
-//
-// import type { NextRequest } from 'next/server';
-//
-// export const middleware = (request: NextRequest) => {
-//   const { headers, url, nextUrl } = request;
-//   let { pathname } = nextUrl;
-//
-//   // if pathname includes 'gallery', replace it with '/gallery'
-//   if (pathname.includes('gallery')) {
-//     pathname = '/gallery';
-//   }
-//
-//   const requestHeaders = new Headers(headers);
-//   requestHeaders.set('x-request-url', url);
-//   requestHeaders.set('x-request-path', pathname);
-//
-//   const res = NextResponse.next({
-//     request: {
-//       headers: requestHeaders,
-//     },
-//   });
-//
-//   return res;
-// };
-
 import createMiddleware from 'next-intl/middleware';
 
 import { routing } from './i18n/routing';
+
+import type { NextRequest, NextResponse } from 'next/server';
+
+export const middleware = (request: NextRequest): NextResponse<unknown> => {
+  const { headers, url, nextUrl } = request;
+  let { pathname } = nextUrl;
+
+  // if pathname includes 'gallery', replace it with '/gallery'
+  if (pathname.includes('gallery')) {
+    pathname = '/gallery';
+  }
+
+  const defaultLocale = headers.get('x-request-locale') || 'en';
+
+  const handleI18nRouting = createMiddleware(routing);
+  const response = handleI18nRouting(request);
+
+  response.headers.set('x-request-locale', defaultLocale);
+  response.headers.set('x-request-url', url);
+  response.headers.set('x-request-path', pathname);
+
+  // const requestHeaders = new Headers(headers);
+  // requestHeaders.set('x-request-url', url);
+  // requestHeaders.set('x-request-path', pathname);
+
+  // const res = NextResponse.next({
+  //   request: {
+  //     headers: requestHeaders,
+  //   },
+  // });
+
+  return response;
+};
 
 export default createMiddleware(routing);
 
