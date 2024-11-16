2cd265ad69300552807da13a6a7269d6e1582144
Yuta Okuma <y.dream.nest@icloud.com>
2024-11-16 23:40:28 +0900

    :lipstick: ui: add letter-spacing tw class

--- src/components/common/SectionTitle/SectionTitle.tsx
+++ src/components/common/SectionTitle/SectionTitle.tsx
@@ -9,8 +9,8 @@ interface Props {
 const SectionTitle: FC<Props> = ({ title }) => {
   return (
     <div className={`${damion.variable} relative w-fit`}>
-      <h2 className="font-damion text-[20vw] text-darkGray lg:text-[200px]">{title}</h2>
-      <h4 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-damion text-[10vw] text-hitGray lg:text-8xl">
+      <h2 className="font-damion text-[20vw] tracking-normal text-darkGray lg:text-[200px]">{title}</h2>
+      <h4 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-damion text-[10vw] tracking-normal text-hitGray lg:text-8xl">
         {title}
       </h4>
     </div>
