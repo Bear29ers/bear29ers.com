a914a496f1743196a0a2ba0df14897a623ddfc36
Yuta Okuma <y.dream.nest@icloud.com>
2024-11-17 22:37:04 +0900

    :construction: wip: applying i18n for biography paragraphs

--- src/components/ui/Biography/Biography.tsx
+++ src/components/ui/Biography/Biography.tsx
@@ -1,21 +1,25 @@
 import type { FC } from 'react';
 
+import { useTranslations } from 'next-intl';
+
 import { WavingHand } from '@/components/icons/ProfileIcons/ProfileIcons';
 
 import { PROFILE_TEXT } from '@/constants/about';
 
 const Biography: FC = () => {
+  const t = useTranslations('about.biography');
+
   return (
     <div className="flex w-full max-w-5xl flex-col gap-y-6">
       <div className="flex items-end gap-x-3">
-        <h4 className="text-lg font-bold sm:text-xl">{PROFILE_TEXT.shortGreeting}</h4>
+        <h4 className="text-lg font-bold sm:text-xl">{t('greeting')}</h4>
         <WavingHand />
       </div>
-      <h3 className="text-xl font-bold sm:text-2xl">{PROFILE_TEXT.title}</h3>
+      <h3 className="text-xl font-bold sm:text-2xl">{t('occupation')}</h3>
       <div className="mt-5 flex flex-col gap-y-5">
-        {PROFILE_TEXT.biographyList.map((biography) => (
-          <p key={biography.id} className="sm:text-lg">
-            {biography.text}
+        {PROFILE_TEXT.paragraphList.map((paragraph) => (
+          <p key={paragraph.id} className="sm:text-lg">
+            {t(`paragraph.${paragraph.text}`)}
           </p>
         ))}
       </div>
