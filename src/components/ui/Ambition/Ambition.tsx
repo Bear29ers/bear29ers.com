581a9901918139de81fad104c25526d819544947
Yuta Okuma <y.dream.nest@icloud.com>
2024-11-22 00:03:14 +0900

    :globe_with_meridians: i18n: i18n for ambitions

--- src/components/ui/Ambition/Ambition.tsx
+++ src/components/ui/Ambition/Ambition.tsx
@@ -1,15 +1,19 @@
 import type { FC } from 'react';
 
+import { useTranslations } from 'next-intl';
+
 import { AMBITIONS } from '@/constants/about';
 
 import type { Ambition as AmbitionType } from '@/types/about';
 
 const Ambition: FC = () => {
+  const t = useTranslations('about.ambitions');
+
   return (
     <ul className="space-y-3">
       {AMBITIONS.map((ambition: AmbitionType) => (
         <li key={ambition.id} className="xxs:text-lg sm:text-xl">
-          {ambition.text}
+          {t(ambition.text)}
         </li>
       ))}
     </ul>
