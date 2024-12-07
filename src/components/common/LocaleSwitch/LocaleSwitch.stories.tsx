d4dc6e613aee59e14ff2cb18e51790130b3cbb64
Yuta Okuma <y.dream.nest@icloud.com>
2024-12-07 17:56:16 +0900

    :white_check_mark: test: complete the LocaleSwitch storybook

--- src/components/common/LocaleSwitch/LocaleSwitch.stories.tsx
+++ src/components/common/LocaleSwitch/LocaleSwitch.stories.tsx
@@ -1,9 +1,12 @@
+import { USIcon } from '@/components/icons/InternationalIcons/InternationalIcons';
+
 import LocaleSwitch from './LocaleSwitch';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof LocaleSwitch> = {
   component: LocaleSwitch,
+  tags: ['autodocs'],
 };
 
 export default meta;
@@ -11,9 +14,16 @@ export default meta;
 type Story = StoryObj<typeof LocaleSwitch>;
 
 export const Default: Story = {
-  render: () => (
+  args: {
+    globalLocale: 'en',
+    locale: 'en',
+    children: <USIcon />,
+  },
+  render: ({ globalLocale, locale, children }) => (
     <div className="h-screen w-screen bg-dark p-4">
-      <LocaleSwitch />
+      <LocaleSwitch globalLocale={globalLocale} locale={locale}>
+        {children}
+      </LocaleSwitch>
     </div>
   ),
 };
