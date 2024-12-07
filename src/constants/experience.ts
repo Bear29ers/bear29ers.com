276ac82b4c26afb7d111b0013b1eae5a3646b4b7
Yuta Okuma <y.dream.nest@icloud.com>
2024-12-07 11:58:57 +0900

    :globe_with_meridians: i18n: i18n for experience in english

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -1,5 +1,7 @@
+import type { Experience } from '@/types/experience';
+
 // 経歴情報
-export const EXPERIENCE_INFO = [
+export const EXPERIENCE_INFO: Experience[] = [
   {
     id: 3,
     startAt: '2023',
@@ -29,57 +31,40 @@ export const EXPERIENCE_INFO = [
       },
     ],
   },
-  // {
-  //   id: 2,
-  //   startAt: '2020',
-  //   endAt: '2023',
-  //   projects: [
-  //     {
-  //       id: 1,
-  //       startAt: '2020.6',
-  //       endAt: '2023: 4',
-  //       role: 'webdev_designer',
-  //       company: 'emoshu',
-  //       location: 'tokyo',
-  //       tagList: ['PHP', 'Laravel', 'MySQL', 'HTML', 'CSS', 'JavaScript', 'TypeScript', 'React', 'XD', 'UI'],
-  //       description: [
-  //         'When I joined the company, there were no web-related jobs, so he started by helping with iOS development directly under the CTO. At that time, there were two directors and one intern, and the office was a room in an old apartment.',
-  //         "While working on client projects, I also contributed greatly to the company's internal business, and experienced the creation of a new blog site, the development of internal tools, and the launch and release of a new application. In addition to my work as an engineer, I have experience in a wide range of tasks, including the development of internal communication tools and documents, planning events and hosting study sessions, supporting interns, and managing internal projects.",
-  //         "The company culture was such that each employee's opinion is easily heard, so I often gave my own opinion on improvements to the company's website or new feature of the application, and after discussion, it was actually decided by the company, and I was able to proceed with tasks with a strong sense of ownership in any situation.",
-  //       ],
-  //       highlightList: [
-  //         'Using Laravel to create a CMS',
-  //         'Learning GSAP to animate elements',
-  //         'Creating UI design with Adobe XD',
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: 1,
-  //   startAt: '2016',
-  //   endAt: '2019',
-  //   projects: [
-  //     {
-  //       id: 1,
-  //       startAt: '2016.4',
-  //       endAt: '2019.3',
-  //       role: 'mentor',
-  //       company: 'lifeIsTech',
-  //       location: 'osaka',
-  //       tagList: ['AfterEffects', 'PremierePro', 'Lightroom', 'Photoshop', 'Illustrator', 'HTML', 'CSS'],
-  //       description: [
-  //         'Sympathizing with the desire to create new opportunities for junior and senior high school students, I participated as a mentor and was mainly in charge of the video editing course.',
-  //         'In order to provide junior and senior high school students with an opportunity to experience IT technology, I participated in IT camp events held at universities all over Japan as a mentor in various places such as Tokyo and Fukuoka, in addition to camps in Kansai, where I am based.',
-  //         'In addition to being a mentor, I also participated in some of the events as a photographer, taking pictures of the students having fun and livening up the entire events.',
-  //         'Because each student has a different personality, enthusiasm, and skill set, I learned a lot during my internship about how to put together a curriculum and communicate with each student.',
-  //       ],
-  //       highlightList: [
-  //         'Improving video editing skills',
-  //         'Designing & Photography',
-  //         'Creating materials with Ps and Ai',
-  //       ],
-  //     },
-  //   ],
-  // },
+  {
+    id: 2,
+    startAt: '2020',
+    endAt: '2023',
+    projects: [
+      {
+        id: 1,
+        startAt: '2020.6',
+        endAt: '2023: 4',
+        role: 'webdev_designer',
+        company: 'emoshu',
+        location: 'tokyo',
+        tagList: ['PHP', 'Laravel', 'MySQL', 'HTML', 'CSS', 'JavaScript', 'TypeScript', 'React', 'XD', 'UI'],
+        description: [0, 1, 2],
+        highlightList: [0, 1, 2],
+      },
+    ],
+  },
+  {
+    id: 1,
+    startAt: '2016',
+    endAt: '2019',
+    projects: [
+      {
+        id: 1,
+        startAt: '2016.4',
+        endAt: '2019.3',
+        role: 'mentor',
+        company: 'lifeIsTech',
+        location: 'osaka',
+        tagList: ['AfterEffects', 'PremierePro', 'Lightroom', 'Photoshop', 'Illustrator', 'HTML', 'CSS'],
+        description: [0, 1, 2, 3],
+        highlightList: [0, 1, 2],
+      },
+    ],
+  },
 ];
