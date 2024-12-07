1f0740f43514edb6096564b9ef1811eb8e4ebf79
Yuta Okuma <y.dream.nest@icloud.com>
2024-12-07 18:28:38 +0900

    :white_check_mark: test: update the Project component storybook

--- src/components/ui/Project/Project.stories.tsx
+++ src/components/ui/Project/Project.stories.tsx
@@ -15,15 +15,12 @@ const projectData = {
   id: 1,
   startAt: '2024.5',
   endAt: 'current',
-  role: 'Frontend Developer',
-  company: 'Gakken LEAP Co.,Ltd',
-  location: 'Tokyo, Japan',
+  role: 'frontend',
+  company: 'gakkenLeap',
+  location: 'tokyo',
   tagList: ['HTML', '(S)CSS', 'Tailwind', 'JavaScript', 'TypeScript', 'React', 'NextJS', 'Figma', 'UI', 'UX'],
-  description: [
-    'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident.',
-    'Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.',
-  ],
-  highlightList: ['tennant-theming for an existing component library', 'Introduced Designed Tokens'],
+  description: [0, 1],
+  highlightList: [0, 1],
 };
 
 export const Default: Story = {
