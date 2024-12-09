93aa73abfc4862f8f736ba77aee30763a3d946f4
Yuta Okuma <y.dream.nest@icloud.com>
2024-12-09 20:36:51 +0900

    :construction: wip: creating the LocaleSwitch component test

--- src/components/common/LocaleSwitch/LocaleSwitch.test.tsx
+++ src/components/common/LocaleSwitch/LocaleSwitch.test.tsx
@@ -1,10 +1,51 @@
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { NextIntlClientProvider } from 'next-intl';
+
+import messages from '../../../../messages/en.json';
 
 import LocaleSwitch from './LocaleSwitch';
 
+import type { RenderResult } from '@testing-library/react';
+
+jest.mock('next/navigation', () => ({
+  usePathname: () => '/',
+  useRouter: () => ({
+    back: jest.fn(),
+    forward: jest.fn(),
+    refresh: jest.fn(),
+    push: jest.fn(),
+    prefetch: jest.fn(),
+    replace: jest.fn(),
+  }),
+  useParams: () => ({ locale: 'en' }),
+  useSelectedLayoutSegment: () => ({ locale: 'en' }),
+}));
+
 describe('src/components/common/LocaleSwitch/LocaleSwitch.test.tsx', () => {
-  it('should render the LocaleSwitch component', () => {
-    const { getByRole } = render(<LocaleSwitch />);
+  let renderResult: RenderResult;
+  const locale = 'en';
+
+  describe('when globalLocale props is different from locale props', () => {
+    beforeEach(() => {
+      renderResult = render(
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <LocaleSwitch globalLocale={locale} locale={locale}>
+            LocaleSwitch
+          </LocaleSwitch>
+        </NextIntlClientProvider>
+      );
+    });
+
+    afterEach(() => {
+      renderResult.unmount();
+    });
+
+    it('should render the LocaleSwitch component', () => {
+      expect(screen.getByRole('button')).toBeInTheDocument();
+    });
+
+    it('should not apply opacity tailwind class', () => {
+      expect(screen.getByRole('button')).not.toHaveClass('opacity-40');
+    });
   });
 });
