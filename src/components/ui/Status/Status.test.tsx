6b15f534ec603cc0810bee61eb01c5f00429de7f
Yuta Okuma <y.dream.nest@icloud.com>
2024-12-11 21:23:39 +0900

    :white_check_mark: test: update the Status component test

--- src/components/ui/Status/Status.test.tsx
+++ src/components/ui/Status/Status.test.tsx
@@ -1,46 +1,54 @@
 import { render, screen } from '@testing-library/react';
+import { NextIntlClientProvider } from 'next-intl';
 
 import { PROFILE_TEXT } from '@/constants/about';
+
+import messages from '../../../../messages/en.json';
 
 import Status from './Status';
 
 import type { RenderResult } from '@testing-library/react';
 
-const profileDetails = {
-  organization: {
-    icon: 'OrganizationIcon',
-    text: 'Gakken LEAP Co.,Ltd',
-  },
-  location: {
-    icon: 'LocationIcon',
-    text: 'Tokyo, Japan',
-  },
-  website: {
-    icon: 'WebsiteIcon',
-    text: 'https://bear29ers.com',
-  },
-};
-
-const teams = {
-  dodgers: {
-    name: 'Los Angeles Dodgers',
-  },
-  yankees: {
-    name: 'New York Yankees',
-  },
-  jays: {
-    name: 'Toronto Blue Jays',
-  },
-  giants: {
-    name: 'Tokyo Yomiuri Giants',
-  },
-};
-
 describe('src/components/ui/Status/Status.test.tsx', () => {
+  const fullName = 'Yuta Okuma';
+  const profileDetails = {
+    organization: {
+      icon: 'OrganizationIcon',
+      text: 'Gakken LEAP Co.,Ltd',
+    },
+    location: {
+      icon: 'LocationIcon',
+      text: 'Tokyo, Japan',
+    },
+    website: {
+      icon: 'WebsiteIcon',
+      text: 'https://bear29ers.com',
+    },
+  };
+
+  const teams = {
+    dodgers: {
+      name: 'Los Angeles Dodgers',
+    },
+    yankees: {
+      name: 'New York Yankees',
+    },
+    jays: {
+      name: 'Toronto Blue Jays',
+    },
+    giants: {
+      name: 'Tokyo Yomiuri Giants',
+    },
+  };
+
   let renderResult: RenderResult;
 
   beforeEach(() => {
-    renderResult = render(<Status />);
+    renderResult = render(
+      <NextIntlClientProvider locale="en" messages={messages}>
+        <Status />
+      </NextIntlClientProvider>
+    );
   });
 
   afterEach(() => {
@@ -52,7 +60,7 @@ describe('src/components/ui/Status/Status.test.tsx', () => {
   });
 
   it('should render the fullname', () => {
-    expect(screen.getByRole('heading', { level: 2, name: PROFILE_TEXT.fullName })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { level: 2, name: fullName })).toBeInTheDocument();
   });
 
   it('should render the username', () => {
