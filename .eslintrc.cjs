92aa1147c72d527ec9f9e8d02164504c7c4985d1
Yuta Okuma <y.dream.nest@icloud.com>
2024-12-22 19:51:43 +0900

    :sparkles: feat: apply theme color to the experience page

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -185,7 +185,7 @@ const config = {
       {
         config: 'tailwind.config.ts',
         cssFiles: ['**/*.scss', '**/*.css', '!**/node_modules', '!**/.*', '!**/dist', '!**/build'],
-        whitelist: ['gallery-intro-title', 'bg-custom', 'text-custom', 'border-custom', '-500'],
+        whitelist: ['gallery-intro-title', 'bg-custom', 'text-custom', 'border-custom', '-200', '-400', '-500', '-900'],
       },
     ],
     'tailwindcss/classnames-order': 'off',
