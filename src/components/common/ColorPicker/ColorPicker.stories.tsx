d0878d2494e9c7a16aa87ffc602e0661045c8b0d
Yuta Okuma <y.dream.nest@icloud.com>
2025-01-07 20:52:38 +0900

    :construction: wip: update some storybook files

--- src/components/common/ColorPicker/ColorPicker.stories.tsx
+++ src/components/common/ColorPicker/ColorPicker.stories.tsx
@@ -4,6 +4,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof ColorPicker> = {
   component: ColorPicker,
+  tags: ['autodocs'],
 };
 
 export default meta;
@@ -11,5 +12,8 @@ export default meta;
 type Story = StoryObj<typeof ColorPicker>;
 
 export const Default: Story = {
-  render: () => <ColorPicker />,
+  args: {
+    pathname: '/experience',
+  },
+  render: ({ pathname }) => <ColorPicker pathname={pathname} />,
 };
