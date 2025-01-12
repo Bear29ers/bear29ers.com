aa3da99767b102cf1b1589e7e8855c454f5e9ed7
Yuta Okuma <y.dream.nest@icloud.com>
2025-01-12 10:30:31 +0900

    :recycle: refa: fix the ProjectHighlight component

--- src/components/ui/projects/ProjectHighlight/ProjectHighlight.tsx
+++ src/components/ui/projects/ProjectHighlight/ProjectHighlight.tsx
@@ -2,6 +2,8 @@ import type { FC } from 'react';
 
 import { useTranslations } from 'next-intl';
 
+import getCustomColorClass from '@/utils/getCustomColorClass';
+
 import type { Color } from '@/types/color';
 
 interface Props {
@@ -13,10 +15,11 @@ interface Props {
 
 const ProjectHighlight: FC<Props> = ({ id, company, index, themeColor }) => {
   const t = useTranslations('experience.highlightList');
+  const customBgColorClass = getCustomColorClass('bg', themeColor, 900);
 
   return (
     <span
-      className={`flex w-fit cursor-default items-start gap-x-2 rounded-lg px-3 py-1.5 text-sm xs:rounded-xl md:px-4 md:py-2 md:text-base bg-custom${themeColor}-900`}>
+      className={`flex w-fit cursor-default items-start gap-x-2 rounded-lg px-3 py-1.5 text-sm xs:rounded-xl md:px-4 md:py-2 md:text-base ${customBgColorClass}`}>
       <span>&#127775;</span>
       <span className="font-medium">{t(`${company}.${id}.${index}`)}</span>
     </span>
