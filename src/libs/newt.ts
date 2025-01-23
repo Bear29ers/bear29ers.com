283645fcb8268aea1d42c24c86c30e3cfdb3be96
Yuta Okuma <y.dream.nest@icloud.com>
2025-01-23 20:29:38 +0900

    :alien: api: add getArticles

--- src/libs/newt.ts
+++ src/libs/newt.ts
@@ -1,7 +1,23 @@
+import { cache } from 'react';
+
 import { createClient } from 'newt-client-js';
+
+import { Article } from '@/types/article';
 
 const client = createClient({
   spaceUid: process.env.NEWT_SPACE_UID + '',
   token: process.env.NEWT_CDN_API_TOKEN + '',
   apiType: 'cdn',
+});
+
+export const getArticles = cache(async () => {
+  const { items } = await client.getContents<Article>({
+    appUid: 'blog',
+    modelUid: 'article',
+    query: {
+      select: ['_id', 'title', 'slug', 'body'],
+    },
+  });
+
+  return items;
 });
