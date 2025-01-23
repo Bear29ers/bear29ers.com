520787d28910e74cb695b0b278661a324876349b
Yuta Okuma <y.dream.nest@icloud.com>
2025-01-23 20:50:27 +0900

    :sparkles: feat: display blog articles

---
+++ src/app/[locale]/blog/page.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from 'next';
+import Link from 'next/link';
+
+import { getArticles } from '@/libs/newt';
+
+const Blog: NextPage = async () => {
+  const articles = await getArticles();
+  return (
+    <main>
+      <ul>
+        {articles.map((article) => {
+          return (
+            <li key={article._id} className="text-white">
+              <Link href={`blog/${article.slug}`}>{article.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
+};
+
+export default Blog;
