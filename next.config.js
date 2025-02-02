885239a3ee9725ba2a8b56a7095a1b5055a46ff0
Yuta Okuma <y.dream.nest@icloud.com>
2025-02-02 17:47:21 +0900

    :wrench: conf: update next.config

--- next.config.js
+++ next.config.js
@@ -13,17 +13,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    // Enable polling based on env variable being set
-    if (process.env.NEXT_WEBPACK_USEPOLLING) {
-      config.webpackOptions = {
-        poll: 1200,
-        aggregateTimeout: 500,
-        ignored: ['node_modules'],
-      };
-    }
-    return config;
-  },
   compiler: {
     reactRemoveProperties: process.env.NODE_ENV === 'production' ? { properties: ['^data-testid$'] } : false,
   },
