66dd1d5b6686ba8ad926db6f4fb57400f29ef721
Yuta Okuma <y.dream.nest@icloud.com>
2025-02-02 18:39:02 +0900

    :sparkles: feat: implement the ArticleDate

--- src/app/[locale]/articles/[slug]/page.tsx
+++ src/app/[locale]/articles/[slug]/page.tsx
@@ -1,6 +1,10 @@
 import { Metadata } from 'next/types';
 
+import ArticleDate from '@/components/ui/articles/ArticleDate/ArticleDate';
+
 import { getArticleBySlug, getArticles } from '@/libs/newt';
+
+import getFormattedDate from '@/utils/getFormattedDate';
 
 type Params = Promise<{ slug: string }>;
 
@@ -27,13 +31,13 @@ const Article = async ({ params }: { params: Params }) => {
   const article = await getArticleBySlug(slug);
   if (!article) return;
 
-  const createdAtData = new Date(article._sys.createdAt);
-  const formattedCreatedAt = `${createdAtData.getFullYear()}年${createdAtData.getMonth() + 1}月${createdAtData.getDate()}日`;
-  console.log(formattedCreatedAt);
+  const formattedCreatedAt = getFormattedDate(new Date(article._sys.createdAt));
+  const formattedUpdatedAt = getFormattedDate(new Date(article._sys.updatedAt));
 
   return (
     <main className="relative size-full overscroll-y-none bg-dark bg-auto bg-center bg-repeat font-murecho tracking-wider flex-center txs:bg-noise-pattern">
-      <div className="mt-20 w-full max-w-5xl rounded-2xl bg-white px-24 py-20 shadow-2xl">
+      <div className="mt-20 w-full max-w-6xl rounded-2xl bg-white px-32 py-20 shadow-2xl">
+        <ArticleDate createdAt={formattedCreatedAt} updatedAt={formattedUpdatedAt} />
         <h1>{article.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: article.body }}></div>
       </div>
--- src/components/ui/articles/ArticleDate/ArticleDate.tsx
+++ src/components/ui/articles/ArticleDate/ArticleDate.tsx
@@ -1,9 +1,23 @@
 import type { FC } from 'react';
 
-const ArticleDate: FC = () => {
+import { ArrowPathIcon, PencilSquareIcon } from '@heroicons/react/24/outline';
+
+interface Props {
+  createdAt: string;
+  updatedAt: string;
+}
+
+const ArticleDate: FC<Props> = ({ createdAt, updatedAt }) => {
   return (
-    <div>
-      <h1>ArticleDate</h1>
+    <div className="flex items-center gap-x-4">
+      <div className="flex items-center gap-x-0.5 text-sm text-hitGray">
+        <PencilSquareIcon className="size-4" />
+        {createdAt}
+      </div>
+      <div className="flex items-center gap-x-0.5 text-sm text-hitGray">
+        <ArrowPathIcon className="size-4" />
+        {updatedAt}
+      </div>
     </div>
   );
 };
