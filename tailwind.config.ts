9259a41c1e17299959724b9f48955030495db7bc
Yuta Okuma <y.dream.nest@icloud.com>
2025-02-08 21:23:41 +0900

    :wrench: conf: add custom borderWidth config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,15 @@ const config: Config = {
       txs: '450px',
       xs: '400px',
     },
+    borderWidth: {
+      'DEFAULT': '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+    },
     extend: {
       width: {
         '4.5': '1.125rem',
