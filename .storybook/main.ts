b60aa378f89ae3ff658f40974089b80f724de577
Yuta Okuma <y.dream.nest@icloud.com>
2025-02-11 16:42:53 +0900

    :heavy_plus_sign: dpad: add storybook addon packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,43 @@
+import path from 'path';
+
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
-    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
+    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    '@storybook/addon-controls',
+    '@storybook/addon-styling-webpack',
+    '@chromatic-com/storybook',
+    'storybook-next-intl',
   ],
+
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
+
+  docs: {},
+
   staticDirs: ['../public'],
+
+  webpackFinal: async (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      '@': path.resolve(__dirname, '../src'),
+      '~': path.resolve(__dirname, '../public'),
+    };
+    return config;
+  },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
 };
+
 export default config;
