8085ea670eaa2fcfa52c7fffbea5c8a2d8806597
Yuta Okuma <y.dream.nest@icloud.com>
2025-02-13 20:26:15 +0900

    :sparkles: feat: add constants

---
+++ src/constants/home.ts
@@ -0,0 +1,8 @@
+import type { HomeText } from '@/types/home';
+
+// プロフィールテキスト
+export const HOME_PROFILE_TEXT: HomeText = {
+  userName: 'Bear29ers',
+  occupation: 'Frontend Developer & Designer',
+  profileAlt: 'bear29ers',
+};
