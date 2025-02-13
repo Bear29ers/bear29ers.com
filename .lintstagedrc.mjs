6381df2bfe98e1a603c0059f3202923f61369ced
Yuta Okuma <y.dream.nest@icloud.com>
2025-02-13 20:21:20 +0900

    :wrench: conf: update lintstagedrc

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -3,13 +3,11 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const tsCheckCommand = () => `tsc --noEmit`;
-
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(__dirname, f)).join(' --file ')}`;
 
 export default {
-  '**/*.{js,jsx,ts,tsx}': [tsCheckCommand, buildEslintCommand],
+  '**/*.{js,jsx,ts,tsx}': [buildEslintCommand],
   '**/*.{js,jsx,ts,tsx,json}': 'prettier --write',
   '**/*.{html,jsx,tsx}': 'markuplint --fix',
   '**/*.{css,scss,sass}': 'stylelint --fix',
