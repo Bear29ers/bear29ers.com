ee5acb4a980b0108f0f7716f204c1a7742e38961
Yuta Okuma <y.dream.nest@icloud.com>
2025-02-13 20:24:24 +0900

    :bento: asst: add styles/fonts

---
+++ src/styles/fonts.ts
@@ -0,0 +1,20 @@
+import { Montserrat, Damion, Murecho } from 'next/font/google';
+
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-mont',
+  display: 'swap',
+});
+
+export const murecho = Murecho({
+  subsets: ['latin'],
+  variable: '--font-murecho',
+  display: 'swap',
+});
+
+export const damion = Damion({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: '--font-damion',
+  display: 'swap',
+});
