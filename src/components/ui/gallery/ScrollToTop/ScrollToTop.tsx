16f1a2a5f78286fca5f164b1cf8cef62514cd82f
Yuta Okuma <y.dream.nest@icloud.com>
2025-06-08 14:14:36 +0900

    :iphone: resp: make the ScrollToTop responsive

--- src/components/ui/gallery/ScrollToTop/ScrollToTop.tsx
+++ src/components/ui/gallery/ScrollToTop/ScrollToTop.tsx
@@ -39,30 +39,32 @@ const ScrollToTop: FC<Props> = ({ themeColor }) => {
   return (
     <AnimatePresence>
       {scrollPosition > 100 && (
-        <motion.button
-          type="button"
-          className={`size-14 rounded-full transition-colors duration-300 flex-center ${bgColorVariants[themeColor]}`}
-          onClick={scrollToTop}
-          initial={{ y: 100, opacity: 0 }}
-          animate={{ y: 0, opacity: 1, transition: { duration: 0.6 } }}
-          exit={{ opacity: 0, transition: { duration: 0.4 } }}
-          whileHover={{ scale: 1.1, transition: { type: 'spring', stiffness: 400, damping: 10 } }}
-          whileTap={{ scale: 0.9, transition: { type: 'spring', stiffness: 400, damping: 10 } }}>
-          <svg
-            className="size-8"
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="chevron-up"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512">
-            <path
-              fill="currentColor"
-              d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"
-            />
-          </svg>
-        </motion.button>
+        <div className="fixed bottom-6 right-4 xsm:right-8 lg:right-10">
+          <motion.button
+            type="button"
+            className={`size-9 rounded-full transition-colors duration-300 flex-center txs:size-14 ${bgColorVariants[themeColor]}`}
+            onClick={scrollToTop}
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1, transition: { duration: 0.6 } }}
+            exit={{ opacity: 0, transition: { duration: 0.4 } }}
+            whileHover={{ scale: 1.1, transition: { type: 'spring', stiffness: 400, damping: 10 } }}
+            whileTap={{ scale: 0.9, transition: { type: 'spring', stiffness: 400, damping: 10 } }}>
+            <svg
+              className="size-6 txs:size-8"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="chevron-up"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512">
+              <path
+                fill="currentColor"
+                d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"
+              />
+            </svg>
+          </motion.button>
+        </div>
       )}
     </AnimatePresence>
   );
