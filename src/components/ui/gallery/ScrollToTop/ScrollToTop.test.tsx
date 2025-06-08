8cc6ae713bdb3034ff09a9ca4b6a7a203e559061
Yuta Okuma <y.dream.nest@icloud.com>
2025-06-08 16:08:54 +0900

    :white_check_mark: test: fix the ScrollToTop storybook and test

--- src/components/ui/gallery/ScrollToTop/ScrollToTop.test.tsx
+++ src/components/ui/gallery/ScrollToTop/ScrollToTop.test.tsx
@@ -22,7 +22,7 @@ describe('src/components/ui/gallery/ScrollToTop/ScrollToTop.test.tsx', () => {
   let renderResult: RenderResult;
 
   beforeEach(() => {
-    renderResult = render(<ScrollToTop themeColor="Red" />);
+    renderResult = render(<ScrollToTop />);
 
     // 各テスト前にscrollYをリセット
     Object.defineProperty(window, 'scrollY', { value: 0, writable: true });
