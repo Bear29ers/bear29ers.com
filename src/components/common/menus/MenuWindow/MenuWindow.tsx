9ef71b8e96ed015c8de235c649d21b59458f66ca
Yuta Okuma <y.dream.nest@icloud.com>
2025-06-08 13:44:06 +0900

    :recycle: refa: add onClick in the MenuList Link

--- src/components/common/menus/MenuWindow/MenuWindow.tsx
+++ src/components/common/menus/MenuWindow/MenuWindow.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react';
+import type { Dispatch, FC, SetStateAction } from 'react';
 
 import { AnimatePresence, motion } from 'motion/react';
 
@@ -7,6 +7,7 @@ import type { Locale } from '@/types/locale';
 import MenuList from '../MenuList/MenuList';
 
 const getVariants = (isMobile: boolean) => {
+  const easing = [0.76, 0, 0.24, 1];
   const variants = isMobile
     ? {
         opened: {
@@ -14,14 +15,14 @@ const getVariants = (isMobile: boolean) => {
           height: '500px',
           top: '-10px',
           right: '-8px',
-          transition: { duration: 0.75, type: 'tween', ease: [0.76, 0, 0.24, 1] },
+          transition: { duration: 0.75, type: 'tween', ease: easing },
         },
         closed: {
           width: '48px',
           height: '48px',
           top: '0px',
           right: '0px',
-          transition: { duration: 0.75, delay: 0.35, type: 'tween', ease: [0.76, 0, 0.24, 1] },
+          transition: { duration: 0.75, delay: 0.35, type: 'tween', ease: easing },
         },
       }
     : {
@@ -30,14 +31,14 @@ const getVariants = (isMobile: boolean) => {
           height: '650px',
           top: '-15px',
           right: '-15px',
-          transition: { duration: 0.75, type: 'tween', ease: [0.76, 0, 0.24, 1] },
+          transition: { duration: 0.75, type: 'tween', ease: easing },
         },
         closed: {
           width: '56px',
           height: '56px',
           top: '0px',
           right: '0px',
-          transition: { duration: 0.75, delay: 0.35, type: 'tween', ease: [0.76, 0, 0.24, 1] },
+          transition: { duration: 0.75, delay: 0.35, type: 'tween', ease: easing },
         },
       };
 
@@ -49,9 +50,10 @@ interface Props {
   isMobile: boolean;
   pathname: string;
   locale: Locale;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-const MenuWindow: FC<Props> = ({ isOpen, isMobile, pathname, locale }) => {
+const MenuWindow: FC<Props> = ({ isOpen, isMobile, pathname, locale, setIsOpen }) => {
   return (
     <motion.div
       layout
@@ -61,7 +63,9 @@ const MenuWindow: FC<Props> = ({ isOpen, isMobile, pathname, locale }) => {
       style={{ borderRadius: 30 }}
       className="relative h-[500px] w-[310px] bg-darkerGray xsm:h-[650px] xsm:w-[500px] md:bg-darker"
       data-testid="menu-window">
-      <AnimatePresence>{isOpen && <MenuList pathname={pathname} locale={locale} />}</AnimatePresence>
+      <AnimatePresence>
+        {isOpen && <MenuList pathname={pathname} locale={locale} setIsOpen={setIsOpen} />}
+      </AnimatePresence>
     </motion.div>
   );
 };
